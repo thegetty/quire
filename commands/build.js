5cb5de65aa19735393f68885620988342c1a932e
Eric Gardner <gardner.ec@gmail.com>
2017-03-22 13:16:07 -0700

    Add placeholders, command-exists module

--- commands/build.js
+++ commands/build.js
@@ -1,5 +1,11 @@
 // Build command
 //
+const exists = require('command-exists')
+const exec = require('child_process').exec
+
 module.exports = function() {
   console.log("Building!")
+  // If 'hugo' command not found, throw an error, tell user to install hugo
+  // If not in a project directory, throw an error
+  // If in a project directory, run `hugo build` and pass any options
 }
--- commands/new.js
+++ commands/new.js
@@ -1,8 +1,14 @@
 // New command
 //
+// Accepts a projectName argument, required
+//
 const GIT_REPO = "https://github.com/gettypubs/quire-catalogue"
 
 module.exports = function(projectName) {
+
+  // If 'git' command not found, throw an error, tell user to install git
+  // If not able to connect with git repo, throw an error
+  // Run `git clone GIT_REPO projectName` and when finished, run a setup script
+
   console.log(`Cloning from ${GIT_REPO} into ${projectName}`)
-  // console.log(`Generating new project: ${projectName}`)
 }
