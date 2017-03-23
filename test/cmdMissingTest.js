26517066814d831e632eae8676ac87cf5c601e7e
Eric Gardner <gardner.ec@gmail.com>
2017-03-23 12:01:27 -0700

    Update tests

--- test/cmdMissingTest.js
+++ test/cmdMissingTest.js
@@ -1,12 +1,14 @@
 const assert = require('assert')
-const commandMissing = require('../util/cmd_missing')
+const util = require('../util/util')
 
-describe('check command util', function() {
-  it('should return false when a command exists', function() {
-    assert.equal(commandMissing('node'), false)
-  })
+describe('Utility functions', function() {
+  describe('commandMissing()', function() {
+    it('should return false when a command exists', function() {
+      assert.equal(util.commandMissing('node'), false)
+    })
 
-  it('should return true when a command does not exist', function() {
-    assert.equal(commandMissing('foo'), true)
+    it('should return true when a command does not exist', function() {
+      assert.equal(util.commandMissing('foo'), true)
+    })
   })
 })
