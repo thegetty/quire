d1a16f4415970973b1e78fd5ce5f957eb00e1aaf
Eric Gardner <gardner.ec@gmail.com>
2017-06-28 11:49:52 -0700

    Add Documentation with JSDoc
    Run `npm run docs` to generate HTML documentation.

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
-# quire-cli
+## quire-cli
+
 Command line interface for building and managing Quire projects
+More info TK.
 
-More info TK
+### Installation
 
-## Installation
-To have a globally available `quire` command, run `npm install -g` inside the project folder after cloning this repo. 
-To create a symbolic link (useful if you are developing locally) run `npm link` in the project directory.
+To have a globally available `quire` command, run `npm install -g` inside the
+project folder after cloning this repo.
+To create a symbolic link (useful if you are developing locally) run `npm link`
+in the project directory.
+
+#### Dependencies
+
+- Git 
+- [Hugo static site generator](http://gohugo.io)
+- Node.js (version?) and NPM
+- Prince PDF generator (optional but required for PDF generation)
+
+Quire has a few dependencies which should be installed separately. They include
+the [Hugo](http://gohugo.io) static site generator, and (optional, but required
+for PDF generation), the [Prince](http://www.princexml.com/) command-line tool.
+
+Git is also required. Mac users can run `xcode-select --install` in the Terminal
+app. PC users should look at the Git Bash Shell. PC support for Quire is
+strictly experimental at this point.
+
+Mac users using Homebrew can install Hugo by running `brew install hugo`
+
+Hugo is free and open-source software; Prince is proprietary (but you can use a
+free version which watermarks the first page of the PDF output).
+
+In the future other PDF generators like WKHTML2PDF may be supported.
+
--- index.js
+++ index.js
@@ -4,26 +4,46 @@
  * @fileOverview Quire CLI
  * @author Eric Gardner / Getty Publications
  * @license MIT
- *
  */
 
+/**
+ * Program
+ *
+ * @description CLI using the Commander library. Quire supports the following
+ * commands:
+ *
+ * ### new <projectName>
+ * Creates a new Quire project in the named directory. Uses `git clone` to grab
+ * the Quire starter kit and starter theme from GitHub. Also uses `npm install`
+ * to install theme dependencies.
+ *
+ * ### preview
+ * Runs the `hugo server` and `webpack --watch` commands in their appropriate
+ * directories at the same time, supporting live-reloading for both content
+ * and theme files.
+ *
+ * ### build
+ * Builds the theme using Webpack and then builds the website using Hugo.
+ *
+ * ### pdf
+ * Runs the Hugo preview server, and then uses [Prince](http://www.princexml.com/)
+ * to hit all pages that do not have `pdf: false` specified in their frontmatter.
+ *
+ * ### epub
+ * Builds an epub version of the site. To be implemented.
+ *
+ */
 const program = require('commander')
+
 const QuireCLI = require('./lib/quire')
 const cli = new QuireCLI()
 
 // Shut down child processes on program exit
 process.on('SIGINT', function() { cli.emit('shutdown') })
 
-// Version
-//
 program
   .version('0.1.0')
 
-// quire new
-//
-// Create a new Quire project in the current directory
-// Should run necessary setup scripts after init
-//
 program
   .command('new <projectName>')
   .description('Create a new Quire project in the current directory.')
@@ -31,11 +51,6 @@ program
     cli.emit('new', projectName)
   })
 
-// quire preview
-//
-// run the preview server in the current directory
-// Pass options to hugo?
-//
 program
   .command('preview [options]')
   .description('Run the preview server in the current directory')
