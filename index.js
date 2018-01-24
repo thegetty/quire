f002a34c407d7a7199b03a584002f2881f0ba40b
Eric Gardner <gardner.ec@gmail.com>
2018-01-23 16:19:37 -0800

    Version bump

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const cli = new CLI()
 process.on('SIGINT', function() { cli.emit('shutdown') })
 
 program
-  .version('0.1.0.alpha.3')
+  .version('0.1.0.alpha.4')
 
 program
   .command('new <projectName>')
