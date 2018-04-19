0e1ce278262ab35ca51db978d6ee4332b7bc2863
Fred Chasen <fchasen@gmail.com>
2018-04-18 17:35:51 -0700

    Update Cover url for Epub

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ program
   .command('new <projectName>')
   .description('Create a new Quire project in the current directory.')
   .action(function(projectName) {
+    cli.verbose = program.verbose
     cli.emit('new', projectName)
   })
 
@@ -64,6 +65,7 @@ program
   .command('install')
   .description('Install this project\'s theme dependencies')
   .action(function() {
+    cli.verbose = program.verbose
     cli.emit('install')
   })
 
@@ -77,6 +79,7 @@ program
   .alias('build')
   .description('Run the build command in the current directory')
   .action(function(env) {
+    cli.verbose = program.verbose
     cli.emit('site', env)
   })
 
@@ -89,6 +92,7 @@ program
   .command('pdf [env]')
   .description('Generate a PDF version of the current project')
   .action(function(env) {
+    cli.verbose = program.verbose
     cli.emit('pdf', env)
   })
 
@@ -101,6 +105,7 @@ program
   .command('epub [env]')
   .description('Generate an EPUB version of the current project')
   .action(function(env) {
+    cli.verbose = program.verbose
     cli.emit('epub', env)
   })
 
@@ -110,6 +115,7 @@ program
   .command('debug')
   .description('Development use only - log info about current project')
   .action(function() {
+    cli.verbose = program.verbose
     cli.emit('debug')
   })
 
