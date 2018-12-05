6d05f0ad49f6324559a572f3ac3cc82b61c0d88c
Nick Hulea <naeluh@gmail.com>
2018-12-04 16:30:48 -0800

    updated css and markup in figure shortcodes to add enlarge links (this does not add fig badge need clarification on a couple of items)

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,58 +1,73 @@
-import DeepZoom from './deepzoom.js'
-import Map from './map.js'
-import 'magnific-popup/dist/magnific-popup.css'
-require('magnific-popup')
-
+import DeepZoom from './deepzoom.js';
+import Map from './map.js';
+import 'magnific-popup/dist/magnific-popup.css';
+require('magnific-popup');
 
 export default function (gallerySelector) {
-
   const findSoundCloudWidgetsAndStopThem = () => {
-    const items = [...document.querySelectorAll(`.quire-figure`)]
+    const items = [...document.querySelectorAll(`.quire-figure`)];
     items.filter((item) => {
-      return [...item.children][0].src !== undefined && [...item.children][0].src.indexOf(`soundcloud`) ? [...item.children][0] : ``
+      return [...item.children][0].src !== undefined && [...item.children][0].src.indexOf(`soundcloud`) ? [...item.children][0] : ``;
     }).map((item) => {
-      let iframeElementID = [...item.children][0].id
-      let widget = SC.Widget(iframeElementID)
-      return widget
+      let iframeElementID = [...item.children][0].id;
+      let widget = SC.Widget(iframeElementID);
+      return widget;
     }).forEach((item) => {
-      return item.pause()
-    })
-  }
+      return item.pause();
+    });
+  };
 
   const captionUpdate = (self) => {
     if ($('.quire-caption-container')) {
-      $('.quire-caption-container').remove()
+      $('.quire-caption-container').remove();
     }
     switch (self.currItem.type) {
       case 'inline':
-        self.caption = self.content.attr('title')
+        self.caption = self.content.attr('title');
         if (self.caption !== undefined) {
-          self.captionCont = `<div class="quire-caption-container"><span class="caption">${self.caption}</span></div>`
-          $('.mfp-wrap').prepend(self.captionCont)
+          self.captionCont = `<div class="quire-caption-container"><span class="caption">${self.caption}</span></div>`;
+          $('.mfp-wrap').prepend(self.captionCont);
         }
-        break
+        break;
       case 'iframe':
-        self.caption = $(self.currItem.el).attr('title')
+        self.caption = $(self.currItem.el).attr('title');
         if (self.caption !== undefined) {
-          self.captionCont = `<div class="quire-caption-container"><span class="caption">${self.caption}</span></div>`
-          $('.mfp-wrap').prepend(self.captionCont)
+          self.captionCont = `<div class="quire-caption-container"><span class="caption">${self.caption}</span></div>`;
+          $('.mfp-wrap').prepend(self.captionCont);
         }
-        break
+        break;
       case 'image':
-        $('.mfp-title').hide()
-        self.caption = $(self.currItem.el).attr('title')
+        $('.mfp-title').hide();
+        self.caption = $(self.currItem.el).attr('title');
         if (self.caption !== undefined) {
-          self.captionCont = `<div class="quire-caption-container"><span class="caption">${self.caption}</span></div>`
-          $('.mfp-wrap').prepend(self.captionCont)
+          self.captionCont = `<div class="quire-caption-container"><span class="caption">${self.caption}</span></div>`;
+          $('.mfp-wrap').prepend(self.captionCont);
         }
-        break
+        break;
       default:
         if ($('.quire-caption-container')) {
-          $('.quire-caption-container').remove()
+          $('.quire-caption-container').remove();
         }
-        break
+        break;
     }
-  }
+  };
+
+  const updateViewSlidesLink = () => {
+    const items = [...document.querySelectorAll(`.quire-figure--group`)];
+    // const child =
+    items.filter((item) => {
+      console.log(item.children[0]);
+      // return [...item.children][0].src !== undefined && [...item.children][0].src.indexOf(`soundcloud`) ? [...item.children][0] : ``;
+    }).map((item) => {
+      // let iframeElementID = [...item.children][0].id;
+      // let widget = SC.Widget(iframeElementID);
+      // return widget;
+    }).forEach((item) => {
+      // return item.pause();
+    });
+  };
+
+  updateViewSlidesLink();
 
   $(gallerySelector).magnificPopup({
     delegate: 'a.popup',
@@ -75,81 +90,78 @@ export default function (gallerySelector) {
     },
     callbacks: {
       beforeOpen: function () {
-        findSoundCloudWidgetsAndStopThem()
-        $('body').addClass('android-fixed')
-        this.current = this.index + 1
-        this.total = this.items.length - 1
-        this.counter = `<span class="counter">${this.current} of ${this.items.length}</span>`
-        this.cont = `<div class="quire-counter-container">${this.counter}</div>`
+        findSoundCloudWidgetsAndStopThem();
+        $('body').addClass('android-fixed');
+        this.current = this.index + 1;
+        this.total = this.items.length - 1;
+        this.counter = `<span class="counter">${this.current} of ${this.items.length}</span>`;
+        this.cont = `<div class="quire-counter-container">${this.counter}</div>`;
       },
       elementParse: function (item) {
         if (item.el[0].getAttribute('data-type') === 'video') {
+          // eslint-disable-next-line no-unused-expressions
           item.type = 'iframe',
-            item.iframe = {
-              patterns: {
-                youtube: {
-                  index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+          item.iframe = {
+            patterns: {
+              youtube: {
+                index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
 
-                  id: 'v=', // String that splits URL in a two parts, second part should be %id%
-                  // Or null - full URL will be returned
-                  // Or a function that should return %id%, for example:
-                  // id: function(url) { return 'parsed id'; } 
-
-                  src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe. 
-                },
-                vimeo: {
-                  index: 'vimeo.com/',
-                  id: '/',
-                  src: '//player.vimeo.com/video/%id%?autoplay=1'
-                },
-                gmaps: {
-                  index: '//maps.google.',
-                  src: '%id%&output=embed'
-                }
+                id: 'v=', // String that splits URL in a two parts, second part should be %id%
+                // Or null - full URL will be returned
+                // Or a function that should return %id%, for example:
+                // id: function(url) { return 'parsed id'; }
+                src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
+              },
+              vimeo: {
+                index: 'vimeo.com/',
+                id: '/',
+                src: '//player.vimeo.com/video/%id%?autoplay=1'
+              },
+              gmaps: {
+                index: '//maps.google.',
+                src: '%id%&output=embed'
               }
             }
+          };
         } else if (item.el[0].getAttribute('data-type') === 'inline') {
-          item.type = 'inline'
+          item.type = 'inline';
         } else {
+          // eslint-disable-next-line no-unused-expressions
           item.type = 'image',
-            item.tLoading = 'Loading image #%curr%...',
-            item.mainClass = 'mfp-img-mobile',
-            item.image = {
-              tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-            }
+          item.tLoading = 'Loading image #%curr%...',
+          item.mainClass = 'mfp-img-mobile',
+          item.image = {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+          };
         }
-
       },
       change: function () {
-        this.current = this.index + 1
+        this.current = this.index + 1;
         if (document.querySelector('.counter')) {
-          document.querySelector('.counter').innerHTML = `${this.current} of ${this.items.length}`
+          document.querySelector('.counter').innerHTML = `${this.current} of ${this.items.length}`;
         }
 
-        captionUpdate(this)
+        captionUpdate(this);
 
-        let id = this.content.children()[0].id
-        let waitForDOMUpdate = 100
+        let id = this.content.children()[0].id;
+        let waitForDOMUpdate = 100;
         if (id !== '' || id !== undefined) {
           if (id.indexOf('map') !== -1) {
             setTimeout(() => {
-              new Map(id)
-            }, waitForDOMUpdate)
+              new Map(id);
+            }, waitForDOMUpdate);
           }
           if (id.indexOf('deepzoom') !== -1) {
-
             setTimeout(() => {
-              new DeepZoom(id)
-            }, waitForDOMUpdate)
+              new DeepZoom(id);
+            }, waitForDOMUpdate);
           }
           if (id.indexOf('iiif') !== -1) {
             setTimeout(() => {
-              new DeepZoom(id)
-            }, waitForDOMUpdate)
+              new DeepZoom(id);
+            }, waitForDOMUpdate);
           }
-
         }
-
       },
       resize: function () {
         // console.log('Popup resized ' + $(window).innerHeight());
@@ -157,15 +169,15 @@ export default function (gallerySelector) {
       },
       open: function () {
         // console.log('Popup open');
-        captionUpdate(this)
-        $('.mfp-wrap').prepend(this.cont)
+        captionUpdate(this);
+        $('.mfp-wrap').prepend(this.cont);
       },
 
       beforeClose: function () {
         // Callback available since v0.9.0
         // console.log('Popup close has been initiated');
-        $('body').removeClass('android-fixed')
-        $('.quire-counter-container, .quire-caption-container').remove()
+        $('body').removeClass('android-fixed');
+        $('.quire-counter-container, .quire-caption-container').remove();
       },
       close: function () {
         // console.log('Popup removal initiated (after removalDelay timer finished)');
