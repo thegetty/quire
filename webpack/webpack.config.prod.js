09e57120a86d79b7d1f6093508d19772fe00d66e
Nick Hulea <naeluh@gmail.com>
2019-01-10 17:11:18 -0800

    fix for pathing webpack for now

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -87,7 +87,7 @@ module.exports = {
           loader: 'file-loader',
           options: {
             name: '[name].[ext]',
-            outputPath: '/img/'
+            outputPath: '../img'
           }
         }]
       },
@@ -97,7 +97,7 @@ module.exports = {
           loader: 'file-loader',
           options: {
             name: '[name].[ext]',
-            outputPath: '/fonts/'
+            outputPath: '../fonts'
           }
         }]
       }
