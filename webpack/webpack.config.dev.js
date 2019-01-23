f36e7f24f677577dbc716c4cbdb8720541c22dc8
Nick Hulea <naeluh@gmail.com>
2019-01-23 13:10:56 -0800

    merge fix

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,9 +1,11 @@
+// @ts-check
+
 const path = require('path')
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const autoprefixer = require("autoprefixer")
 const ImageminPlugin = require("imagemin-webpack");
- 
+
 // Before importing imagemin plugin make sure you add it in `package.json` (`dependencies`) and install
 const imageminGifsicle = require("imagemin-gifsicle");
 const imageminJpegtran = require("imagemin-jpegtran");
@@ -32,67 +34,83 @@ module.exports = {
   },
   module: {
     rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: ['@babel/preset-env']
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: [/node_modules/, path.join(PATHS.build, 'css', 'epub.scss')],
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader',
+          {
+            loader: "postcss-loader",
+            options: {
+              autoprefixer: {
+                browsers: ["last 2 versions"]
+              },
+              plugins: () => [
+                autoprefixer
+              ]
+            },
+          }, 'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader',
+          {
+            loader: "postcss-loader",
+            options: {
+              autoprefixer: {
+                browsers: ["last 3 versions"]
+              },
+              plugins: () => [
+                autoprefixer
+              ]
+            },
+          }, 'sass-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img/'
+          }
+        }]
+      }
+    ]
+  },
+  optimization: {
+    runtimeChunk: false,
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor_app',
+          chunks: 'all',
+          minChunks: 2
         }
       }
-    },
-    {
-      test: /\.scss$/,
-      exclude: [/node_modules/, path.join(PATHS.build, 'css', 'epub.scss')],
-      use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader',
-        {
-          loader: "postcss-loader",
-          options: {
-            autoprefixer: {
-              browsers: ["last 2 versions"]
-            },
-            plugins: () => [
-              autoprefixer
-            ]
-          },
-        }, 'sass-loader']
-    },
-    {
-      test: /\.css$/,
-      use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader',
-        {
-          loader: "postcss-loader",
-          options: {
-            autoprefixer: {
-              browsers: ["last 3 versions"]
-            },
-            plugins: () => [
-              autoprefixer
-            ]
-          },
-        }, 'sass-loader']
-    },
-    {
-      test: /\.(woff|woff2|eot|ttf|otf)$/, 
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'fonts/'
-        }
-      }]
-    },
-    {
-      test: /\.(jpg|png|gif|svg)$/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'img/'
-        }
-      }]
     }
-    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
@@ -104,8 +122,8 @@ module.exports = {
       jQuery: 'jquery',
       'window.jQuery': 'jquery'
     }),
-     // Make sure that the plugin is after any plugins that add images, example `CopyWebpackPlugin`
-     new ImageminPlugin({
+    // Make sure that the plugin is after any plugins that add images, example `CopyWebpackPlugin`
+    new ImageminPlugin({
       bail: false, // Ignore errors on corrupted images
       cache: false,
       imageminOptions: {
