0cda11759ea9f3a6f6532748d5dffc11c8ae0034
naeluh <naeluh@gmail.com>
2019-08-19 15:48:51 -0700

    pdf test working

--- test/cli.test.js
+++ test/cli.test.js
@@ -3,42 +3,132 @@ const path = require("path");
 const tmp = require("tmp");
 const fs = require("fs");
 const CLI = require(path.join("..", "lib", "cli"));
-const validProjectDir = path.join(
-  process.cwd(),
-  "test",
-  "fixtures",
-  "quire-starter"
-);
 
 describe("CLI", () => {
   let quire = new CLI();
-
-  test("should successfully create a starter project", async done => {
-    let sandboxDir = tmp.dirSync();
-    let projectName = "testProject";
-    let projectThemePath = path.join(
-      projectName,
-      "themes",
-      "quire-starter-theme"
-    );
-    process.chdir(sandboxDir.name);
-    // console.log("sandboxDir: ", sandboxDir);
-    let result = await quire.create(projectName);
-    // console.log(result);
-    if (result) {
-      let fullPath = path.join(sandboxDir.name, projectThemePath);
-      // console.log(fs.existsSync(fullPath));
-      assert.equal(fs.existsSync(fullPath), true);
-      // quire.shutdown(true);
-      done();
-    } else {
-      // quire.shutdown(true);
-      done();
-    }
-  }, 25000);
+  let sandboxDir = tmp.dirSync();
+  let projectName = "testProject";
+  let projectThemePath = path.join(
+    projectName,
+    "themes",
+    "quire-starter-theme"
+  );
 
   test("prints the CLI object", () => {
     console.log(`prints the CLI object`);
     console.log(quire);
   }, 2000);
+
+  test(
+    "should successfully create a starter project",
+    async done => {
+      process.chdir(sandboxDir.name);
+      let result = await quire.create(projectName);
+      if (result) {
+        let fullPath = path.join(sandboxDir.name, projectThemePath);
+        assert.equal(fs.existsSync(fullPath), true);
+        done();
+      } else {
+        done();
+      }
+    },
+    25000 * 1
+  );
+
+  test(
+    "should successfully install node modules in a starter project theme",
+    async done => {
+      let result = await quire.install();
+      if (result) {
+        let nodeModulesPath = path.join(
+          sandboxDir.name,
+          projectThemePath,
+          "node_modules"
+        );
+        assert.equal(fs.existsSync(nodeModulesPath), true);
+        done();
+      } else {
+        done();
+      }
+    },
+    25000 * 2
+  );
+
+  test(
+    "should successfully build a static site",
+    async done => {
+      let result = await quire.site();
+      if (result) {
+        let sitePath = path.join(sandboxDir.name, projectName, "site");
+        assert.equal(fs.existsSync(sitePath), true);
+        done();
+      } else {
+        done();
+      }
+    },
+    25000 * 3
+  );
+
+  test(
+    "should successfully build a pdf",
+    async done => {
+      let result = await quire.pdf();
+      if (result) {
+        let pdfPath = path.join(
+          sandboxDir.name,
+          projectName,
+          "static",
+          "downloads",
+          "output.pdf"
+        );
+        assert.equal(fs.existsSync(pdfPath), true);
+        done();
+      } else {
+        done();
+      }
+    },
+    25000 * 4
+  );
+
+  test(
+    "should successfully build a epub",
+    async done => {
+      let result = await quire.epub();
+      if (result) {
+        let epubPath = path.join(
+          sandboxDir.name,
+          projectName,
+          "static",
+          "downloads",
+          "output.epub"
+        );
+        assert.equal(fs.existsSync(epubPath), true);
+        done();
+      } else {
+        done();
+      }
+    },
+    25000 * 5
+  );
+
+  test(
+    "should successfully build a mobi",
+    async done => {
+      let result = await quire.mobi();
+      if (result) {
+        let mobiPath = path.join(
+          sandboxDir.name,
+          projectName,
+          "static",
+          "downloads",
+          "output.mobi"
+        );
+        assert.equal(fs.existsSync(mobiPath), true);
+        done();
+      } else {
+        done();
+      }
+    },
+    25000 * 6
+  );
 });
