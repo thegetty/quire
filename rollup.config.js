89d8aa42874c163282dbe029545346ce7766cb88
naeluh <naeluh@gmail.com>
2019-08-26 15:49:44 -0700

    modifications to babel config, exports for pandoc template, rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,44 @@
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import { uglify } from "rollup-plugin-uglify";
+import multiEntry from "rollup-plugin-multi-entry";
+import replace from "rollup-plugin-replace";
+import json from "rollup-plugin-json";
+
 export default {
   input: {
-    include: ["index.js", "lib/*.js"]
+    include: ["lib/*.js"]
   },
   output: {
     file: "./bin/index.js",
     format: "iife",
-    name: "bundle"
+    banner: "#!/usr/bin/env node"
   },
-  plugins: [multiEntry()]
+  plugins: [
+    commonjs({
+      include: "node_modules/**"
+    }),
+    babel({
+      babelrc: false,
+      runtimeHelpers: true,
+      plugins: [
+        "@babel/plugin-transform-async-to-generator",
+        "@babel/plugin-transform-runtime"
+      ],
+      presets: ["@babel/preset-env"]
+    }),
+    multiEntry(),
+    replace({
+      delimiters: ["", ""],
+      "#!/usr/bin/env node": ""
+    }),
+    resolve({
+      include: /node_modules/,
+      preferBuiltins: false,
+      browser: true
+    }),
+    uglify(),
+    json()
+  ]
 };
