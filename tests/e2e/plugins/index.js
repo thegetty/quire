0d08463ea9d3505035f94d19df8d2690ec7ecc63
naeluh <naeluh@gmail.com>
2019-10-11 14:54:57 -0700

    set quire preview process to background true
    fix cache path
    
    change some paths
    
    move artifacts
    
    move tests directory again

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -17,8 +17,8 @@ module.exports = (on, config) => {
   return Object.assign({}, config, {
     fixturesFolder: "tests/e2e/fixtures",
     integrationFolder: "tests/e2e/specs",
-    screenshotsFolder: "tests/e2e/screenshots",
-    videosFolder: "tests/e2e/videos",
+    screenshotsFolder: "test-results/e2e/screenshots",
+    videosFolder: "test-results/e2e/videos",
     supportFile: "tests/e2e/support/index.js"
   });
 };
