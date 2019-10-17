a1449b06a8f66131a445d83acdc2497b2adabd3e
naeluh <naeluh@gmail.com>
2019-10-17 16:00:02 -0700

    entry tests for click done, scripts working

--- tests/e2e/specs/quire-entry.spec.js
+++ tests/e2e/specs/quire-entry.spec.js
@@ -18,11 +18,33 @@ describe("Quire Entry", () => {
   });
 
   it("should download the image", () => {
-    cy.get(".quire-image-control--download").click();
+    cy.get(".quire-image-control--download").then(anchor => {
+      let href = anchor.attr("href");
+      let url = new URL(href, window.location.origin).href;
+      cy.request(url).then(response => {
+        expect(response.status).to.eq(200);
+      });
+    });
   });
 
+  /**
+   *
+   * This mostly works. However in some browsers for
+   * cypress, It will error with "Failed to execute
+   * 'requestFullscreen' on 'Element': API can only be initiated by
+   * a user gesture." This test passes becasue it does not
+   * detect that fullscreen API has been initialized, but
+   * it looks that the "fullscreen" class has been added to
+   * style the button.
+   * Issues on Github => https://github.com/cypress-io/cypress/issues/311,
+   * https://github.com/cypress-io/cypress/issues/1213
+   *
+   */
   it("should fullscreen the image", () => {
     cy.get("#toggleFullscreen").click();
-    cy.get("#toggleFullscreen").should("has.class", "fullscreen");
+    cy.get("#toggleFullscreen").then(element => {
+      let c = element.attr("class");
+      expect(c.indexOf("fullscreen") !== -1).to.eq(true);
+    });
   });
 });
