799607c79bd8d6fb5a11735648fffa2adaa18fce
Hannah Balenda <hbalenda@getty.edu>
2021-04-28 17:15:55 -0700

    Move themes/default/tests to tests/e2e

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const dir = path.join(process.cwd(), "test-results", "jest");
+
+module.exports = {
+  moduleNameMapper: {
+    "^@src(.*)$": "<rootDir>/packages/cli/lib/$1"
+  },
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        suiteName: "Tests for Quire CLI",
+        outputDirectory: dir,
+        outputName: "results.xml"
+      }
+    ]
+  ],
+  testEnvironment: "node",
+  transform: {
+    ".*": "<rootDir>/node_modules/babel-jest"
+  }
+};
