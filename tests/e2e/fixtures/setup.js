d726e676358a00444a93b7a48ae9a4e5a944cc57
Hannah Balenda <hbalenda@getty.edu>
2021-05-03 09:56:19 -0700

    Use async exec so that command works across operating systems

--- tests/e2e/fixtures/setup.js
+++ tests/e2e/fixtures/setup.js
@@ -2,14 +2,17 @@
  * Creates a test project for end to end tests
  */
 
-const spawnSync = require("child_process").spawnSync;
+const exec = require("child_process").exec;
 const path = require("path");
 const CONFIG = require("./project-config");
 
-console.log("Generating test project, this may take a while...");
-console.log(`Running: quire new ${path.join(CONFIG.FIXTURES_DIR, CONFIG.PROJECT_FOLDER)}`)
-spawnSync("quire", [
-  "new",
-  path.join(CONFIG.FIXTURES_DIR, CONFIG.PROJECT_FOLDER),
-]);
-console.log("Done creating test project.");
+
+console.log("Creating test project, this may take a while...");
+const childProcess = exec(`quire new ${CONFIG.PROJECT_FOLDER}`, { cwd: CONFIG.FIXTURES_DIR });
+childProcess.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+childProcess.on('close', () => {
+  console.log(`Done creating test project.`)
+});
+
