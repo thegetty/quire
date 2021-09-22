9f1fbe55cae031da090f1d302b421ebad979cbce
matthew p hrudka <mhrudka@getty.edu>
2021-09-22 14:12:05 -0700

    Rewrite twitter-card as a JavaScript template

--- packages/11ty/_includes/page/twitter-card.11ty.js
+++ packages/11ty/_includes/page/twitter-card.11ty.js
@@ -1,20 +1,55 @@
-{% liquid
-if type != 'essay'
-  assign siteUrl = site.baseURL
-  assign title = site.title
-  assign description = publication.description.one_line or publication.description.full
-  assign image = publication.promo_image
-else
-  assign siteUrl = permalink
-  assign title = page.title
-  assign description = page.abstract or publication.description.one_line or publication.description.full
-  assign image = page.cover or publication.promo_image
-endif %}
+const path = require('path')
 
-{% assign imagePath = site.baseURL | site.imageDir | site.figureSubDir | image | join: '/' %}
+/**
+ * Renders <head> <meta> data tags for Open Graph protocol data
+ *
+ * @param      {Object}  data    data
+ * @return     {String}  HTML meta and link elements
+ */
+module.exports = class OpenGraph {
+  data() {
+    const { description, promo_image } = publication
+    const pageType = this.page.type
 
-<meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:site" content="{{ siteUrl }}">
-<meta name="twitter:title" content="{{ title }}">
-<meta name="twitter:description" content="{{ description | truncate 160 }}">
-<meta property="twitter:image" content="{{ imagePath }}" />
+    const imagePath = () => {
+      if (pageType !== 'essay' ) {
+        return path.join(site.baseURL, site.imageDir, promo_image)
+      } else {
+        const image = page.cover || promo_image
+        path.join(site.baseURL, site.imageDir, site.figureSubDir, image)
+      }
+    }
+
+    const meta: [
+      {
+        name: 'twitter:card',
+        content: summary_large_image
+      },
+      {
+        name: 'twitter:site',
+        content: pageType !== 'essay' ? site.baseURL : permalink
+      },
+      {
+        name: 'twitter:title',
+        content: pageType !== 'essay' ? site.title : page.title
+      },
+      {
+        name: 'twitter:description',
+        content: pageType !== 'essay'
+          ? description.one_line || description.full
+          : page.abstract || description.one_line || description.full
+      },
+      {
+        name: 'twitter:image',
+        content: imagePath()
+      }
+    ]
+  }
+
+  render({ link, meta }) {
+    const metaTags = meta.map(({ name, content }) => (
+      `<meta name="${name}" content="${content}">`
+    ))
+    return `${metaTags.join('\n')}`
+  }
+}
