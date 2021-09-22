03584a1f78873e58ab93120fee5c856c2dfe42d4
Hannah Balenda <hbalenda@getty.edu>
2021-09-22 13:00:31 -0700

    Include all shortcodes in shortcodes/index and comment out

--- packages/11ty/plugins/shortcodes/index.js
+++ packages/11ty/plugins/shortcodes/index.js
@@ -1,7 +1,12 @@
-const figure = require ('./figure')
-const styleClass = require ('./styleClass')
+const bibliography = require ('./bibliography.11ty.js')
+const cite = require ('./cite.11ty.js')
+const contributor = require ('./contributor.11ty.js')
+const figure = require ('./figure.11ty.js')
+const figureGroup = require ('./figureGroup.11ty.js')
+const figureZoom = require ('./figureZoom.11ty.js')
+const styleClass = require ('./styleClass.11ty.js')
 
 module.exports = function(eleventyConfig, options) {
-  eleventyConfig.addPairedShortcode('qclass', styleClass)
-  eleventyConfig.addShortcode('qfigure', figure)
+  eleventyConfig.addPairedShortcode('class', styleClass)
+  eleventyConfig.addShortcode('figure', figure)
 }
