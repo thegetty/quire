40b528fa06674040bcc1888765899ff486b9c880
matthew p hrudka <mhrudka@getty.edu>
2021-11-08 11:28:49 -0800

    Refactor shortcodes for access to global data
    Pass specific global data objects to shortcode functions;
    currently neither the context nor global data are available to universal
    shortcodes without wrapping them in a closure that loads the relavent
    data file and passes it as an argument to the shortcode function.

--- packages/11ty/plugins/shortcodes/index.js
+++ packages/11ty/plugins/shortcodes/index.js
@@ -1,15 +1,32 @@
-const backmatter = require ('./backmatter.11ty.js')
-const cite = require ('./cite.11ty.js')
-const contributor = require ('./contributor.11ty.js')
-const figure = require ('./figure/index.11ty.js')
-const figureGroup = require ('./figureGroup.11ty.js')
+const backmatter = require('./backmatter.11ty.js')
+const cite = require('./cite.11ty.js')
+const contributor = require('./contributor.11ty.js')
+const figure = require('./figure/index.11ty.js')
+const figureGroup = require('./figureGroup.11ty.js')
+
+const fs   = require('fs')
+// const path = require('path')
+const yaml = require('js-yaml')
 
 module.exports = function(eleventyConfig, options) {
-  const config = eleventyConfig
-
   eleventyConfig.addPairedShortcode('backmatter', (data) => backmatter(data))
-  eleventyConfig.addShortcode('cite', (data) => cite(config, data))
-  eleventyConfig.addShortcode('contributor', (data) => contributor(config, data))
-  eleventyConfig.addShortcode('figure', (data) => figure(config, data))
-  eleventyConfig.addShortcode('figuregroup', (data) => figureGroup(config, data))
+
+  const { entries } = yaml.load(fs.readFileSync('./src/_data/references.yaml'))
+  const references = Object.fromEntries(
+    entries.map(({ id, full }) => [id, full])
+  )
+  eleventyConfig.addShortcode('cite', (data) => cite(eleventyConfig, references, data))
+
+  const publication = yaml.load(fs.readFileSync('./src/_data/publication.yaml'))
+  eleventyConfig.addShortcode('contributor', (data) => {
+    return contributor(eleventyConfig, publication, data)
+  })
+
+  const { figure_list } = yaml.load(fs.readFileSync('./src/_data/figures.yaml'))
+  const figures = Object.fromEntries(figure_list.map((figure) => {
+    const { caption, credit, id, src } = figure
+    return [ id, { caption, credit, src }]
+  }))
+  eleventyConfig.addShortcode('figure', (data) => figure(eleventyConfig, figures, data))
+  eleventyConfig.addShortcode('figuregroup', (data) => figureGroup(eleventyConfig, figures, data))
 }
