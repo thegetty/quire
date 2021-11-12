ce5407203d6845b468fcd37bb53055edc16edf95
Anders Pollack <pollack.anders@gmail.com>
2021-11-11 19:53:07 -0800

    Hack computed imageDir for now

--- packages/11ty/src/_data/eleventyComputed.js
+++ packages/11ty/src/_data/eleventyComputed.js
@@ -5,7 +5,8 @@ const path = require('path')
 module.exports = {
   canonicalURL: ({ config, page }) => path.join(config.baseURL, page.url),
   data: (data) => data,
-  imageDir: ({ config }) => path.join(config.baseURL, config.params.imageDir),
+  // imageDir: ({ config }) => path.join(config.baseURL, config.params.imageDir),
+  imageDir: '/_assets/img/',
   navigation: {
     id: (data) => data.title,
     parent: (data) => data.parent
