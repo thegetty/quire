140d64eefd9a4dfb694376aa34281a266f888b71
matthew p hrudka <mhrudka@getty.edu>
2021-11-15 14:08:54 -0800

    Use single quotes

--- packages/11ty/_layouts/contents.11ty.js
+++ packages/11ty/_layouts/contents.11ty.js
@@ -5,12 +5,12 @@
  * It is scoped to show the contents of the full site. 
  * Pages can be removed from the TOC indivudally by setting toc to `false` in the page yaml.
  */
-const contentsItemToc = require("../_includes/contentsItemToc.11ty.js");
+const contentsItemToc = require('../_includes/contentsItemToc.11ty.js');
 
 module.exports = class Contents {
   data() {
     return {
-      layout: "base.11ty.js"
+      layout: 'base'
     }
   }
 
@@ -32,11 +32,11 @@ module.exports = class Contents {
           </div>
         </div>
         `
-      : "";
-    const containerClass = className === "grid" ? "is-fullhd" : "";
-    const contentsListClass = ["abstract", "brief", "grid"].includes(className)
+      : '';
+    const containerClass = className === 'grid' ? 'is-fullhd' : '';
+    const contentsListClass = ['abstract', 'brief', 'grid'].includes(className)
       ? className
-      : "list";
+      : 'list';
 
     /**
      * @todo get pages from global data
@@ -89,13 +89,13 @@ module.exports = class Contents {
             });
           listItem += `<ul>${subListItems.join('')}</ul>`;
         }
-        listItem += "</li>";
+        listItem += '</li>';
         return listItem;
       }
     });
 
     return this.renderTemplate(
-      `<div class="{% render "page/class" %} quire-contents" id="main" role="main">
+      `<div class="{% render 'page/class' %} quire-contents" id="main" role="main">
         {% render "page/header" %}
         <section class="section quire-page__content" id="content">
           ${contentElement}
@@ -107,14 +107,14 @@ module.exports = class Contents {
                 </ul>
               </div>
               <div class="content">
-                {% render "page/bibliography" %}
+                {% render 'page/bibliography' %}
               </div>
             </div>
-            {% render "footer-buttons" %}
+            {% render 'footer-buttons' %}
           </div>
         </section>
       </div>`,
-      "liquid",
+      'liquid',
       data.argData
     );
   }
