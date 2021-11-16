382a6017b54581e3d9870607ed7751c8ae6507dc
Hannah Balenda <hbalenda@getty.edu>
2021-11-15 16:53:03 -0800

    Add getFigure filter

--- packages/11ty/plugins/filters/index.js
+++ packages/11ty/plugins/filters/index.js
@@ -1,3 +1,7 @@
+const globalData = require('../globalData')
+const getFigure = require('./getFigure')
+
 module.exports = function(eleventyConfig, options) {
   // @see https://www.11ty.dev/docs/filters/#universal-filters
+  eleventyConfig.addFilter('getFigure', (id) => getFigure(eleventyConfig, globalData, id))
 }
