2cc674fc3b5277b63ce2f8760a9040786fd983d4
matthew p hrudka <mhrudka@getty.edu>
2021-11-18 14:33:35 -0800

    Add plugin to configure eleventy linters

---
+++ packages/11ty/plugins/linting/index.js
@@ -0,0 +1,8 @@
+/**
+ * @see {@link https://www.11ty.dev/docs/config/#linters}
+ */
+ module.exports = function(eleventyConfig, options) {
+  // eleventyConfig.addLinter('html', function(content) {
+  //   console.log(`writing ${this.inputPath} to ${this.outputPath}`)
+  // })
+}
