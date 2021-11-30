5ae2ef963272af14565472a3647b06f7a6e4fd9d
matthew p hrudka <mhrudka@getty.edu>
2021-11-30 11:55:50 -0800

    Create module for navigation plugin

---
+++ packages/11ty/plugins/navigation/index.js
@@ -0,0 +1,11 @@
+/**
+ * An Eleventy plugin for Quire navigation
+ *
+ * @param      {Object}  eleventyConfig  eleventy configuration
+ * @param      {Object}  [options]       navigation options
+ */
+module.exports = function(eleventyConfig, options) {
+
+  // eleventyConfig.addFilter('menu', function() {})
+  // eleventyConfig.addFilter('toc', function() {})
+}
