52b0e121d7bb29c0029ce5cc82961f38e37efd71
Anders Pollack <pollack.anders@gmail.com>
2022-01-19 14:34:48 -0800

    Move `content/js`, `content/css` into `content/_assets/javascript` and `content/_assets/styles`
    - Move all application JS into `/content/_assets/javascript/application` subdirectory
    - rename `application.js` to `index.js` to clarify entrypoint

--- packages/11ty/_includes/components/scripts.11ty.js
+++ packages/11ty/_includes/components/scripts.11ty.js
@@ -1,7 +1,7 @@
 module.exports = function({ config }) {
   return `
     <script type="text/javascript"> var figureModal = ${config.params.figureModal}</script>
-    <script type="module" src="/js/application.js"></script>
-    <script src="/js/custom.js"></script>
+    <script type="module" src="/_assets/javascript/application/index.js"></script>
+    <script type="module" src="/_assets/javascript/custom.js"></script>
   `
 }
--- packages/11ty/content/_assets/javascript/application/index.js
+++ packages/11ty/content/_assets/javascript/application/index.js
@@ -10,7 +10,7 @@
 // Stylesheets
 import "intersection-observer";
 import "leaflet-fullscreen/dist/leaflet.fullscreen.css";
-import "../css/application.scss";
+import "../../styles/application.scss";
 import "leaflet/dist/leaflet.css";
 import quicklink from "quicklink";
 
@@ -22,7 +22,7 @@ import "./soundcloud-api";
 
 // Modules (feel free to define your own and import here)
 import { preloadImages, stopVideo, toggleFullscreen } from "./helper";
-import Search from "../plugins/search/search.js";
+import Search from "../../../../plugins/search/search.js";
 import Navigation from "./navigation";
 import Popup from "./popup";
 import DeepZoom from "./deepzoom";
