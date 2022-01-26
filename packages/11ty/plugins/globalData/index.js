b051e3d0972f9f3125cbfcc4234b184713526a9f
matthew p hrudka <mhrudka@getty.edu>
2022-01-26 13:11:12 -0800

    Load all supported files in _data as globalData

--- packages/11ty/plugins/globalData/index.js
+++ packages/11ty/plugins/globalData/index.js
@@ -1,16 +1,40 @@
 const fs = require('fs')
+const json5 = require('json5')
 const path = require('path')
+const toml = require('toml')
 const yaml = require('js-yaml')
 
-const loadData = (fileName) => {
-  const filePath = path.join('content', '_data', fileName)
-  return yaml.load(fs.readFileSync(filePath))
+/**
+ * Loads _data files in order to make them available to shortcodes
+ */
+
+const dataDir = path.join('content', '_data')
+const globalData = {}
+
+function loadData = (filePath) => {
+  const { ext } = path.parse(filePath)
+  const data = fs.readFileSync(filePath)
+  switch (true) {
+    case ext === 'geojson':
+      return JSON.parse(data)
+    case ext === 'json5':
+      return json5.load(data)
+    case ext === 'toml':
+      return toml.load(data)
+    case ext === 'yaml':
+      return yaml.load(data)
+    default:
+      console.warn(`Unsupported data file type ${filePath}`)
+      return
+  }
 }
 
-const config = loadData('config.yaml')
-const figures = loadData('figures.yaml')
-const objects = loadData('objects.yaml')
-const publication = loadData('publication.yaml')
-const references = loadData('references.yaml')
+for (let file of fs.readdirSync(dataDir)) {
+  const filePath = path.join(dataDir, file)
+  const { name } = path.parse(filePath)
+  if (name !== 'eleventyComputed') {
+    globalData[name] = loadData(filePath)
+  }
+}
 
-module.exports = { config, figures, objects, publication, references }
+module.exports = globalData
