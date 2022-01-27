55402455140f569237b5404c41adf704760d4bf5
Hannah Balenda <hbalenda@getty.edu>
2022-01-27 12:48:25 -0800

    Refactor site-title and script component signature

--- packages/11ty/_includes/components/scripts.js
+++ packages/11ty/_includes/components/scripts.js
@@ -1,4 +1,10 @@
-module.exports = function(eleventyConfig, { config }) {
+/**
+ * @param  {Object} context
+ * 
+ * @return {String} script tags
+ */
+module.exports = function({ globalData }) {
+  const { config } = globalData
   return `
     <script type="text/javascript"> var figureModal = ${config.params.figureModal}</script>
     <script type="module" src="/_assets/javascript/application/index.js"></script>
--- packages/11ty/_includes/components/site-title.js
+++ packages/11ty/_includes/components/site-title.js
@@ -1,9 +1,13 @@
 /**
  * Concatenates the site title and subtitle, using a colon, or if the title ends with a ! or ?, no colon is included.
  * See also page/title.liquid
+ *
+ * @param  {Object} context
+ * 
+ * @return  {String} Site title
  */
-
-module.exports = function(eleventyConfig, { publication }) {
+module.exports = function({ globalData }) {
+  const { publication } = globalData
   const { reading_line: readingLine, subtitle, title } = publication
   const lastLetter = title.slice(-1)
   const endPunctuation = '!?'.includes(lastLetter) ? '' : ':'
