7630c6c014f74cadbb6bfd9d26556c7c16c0cfee
matthew p hrudka <mhrudka@getty.edu>
2022-01-27 10:36:58 -0800

    Rewrite expressions

--- packages/11ty/_includes/components/contributor/title.js
+++ packages/11ty/_includes/components/contributor/title.js
@@ -1,5 +1,11 @@
 module.exports = function(eleventyConfig, globalData, { affiliation, title }) {
-  const affiliationElement = affiliation ? `<span class="quire-contributor__affiliation">${ affiliation }</span>` : ''
-  const titleElement = title ? `<span class="quire-contributor__title">${ title }</span>` : ''
+  affiliation = affiliation
+    ? `<span class="quire-contributor__affiliation">${ affiliation }</span>`
+    : ''
+
+  title = title
+    ? `<span class="quire-contributor__title">${ title }</span>`
+    : ''
+
   return [title, affiliation].join(', ')
 }
