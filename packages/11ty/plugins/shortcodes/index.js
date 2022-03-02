119b1909df70a6dc6f190ce6a6cf4dc8ac5fa9c7
Hannah Balenda <hbalenda@getty.edu>
2022-03-02 15:21:39 -0800

    Remove this.page from shortcodes that don't need it and add todos to refactor qcite to work without providing the page to the context

--- packages/11ty/plugins/shortcodes/index.js
+++ packages/11ty/plugins/shortcodes/index.js
@@ -14,52 +14,55 @@ const tombstone = require('./tombstone.js')
 
 module.exports = function(eleventyConfig, options) {
   eleventyConfig.addPairedShortcode('backmatter', function(content, ...args) {
-    const context = { eleventyConfig, globalData, page: this.page }
+    const context = { eleventyConfig, globalData }
     return backmatter(context, content, ...args)
   })
 
   eleventyConfig.addPairedShortcode('class', function(content, ...args) {
-    const context = { eleventyConfig, globalData, page: this.page }
+    const context = { eleventyConfig, globalData }
     return div(context, content, ...args)
   })
 
+  /**
+   * @todo remove this.page from context
+   */
   eleventyConfig.addShortcode('cite', function(...args) {
     const context = { eleventyConfig, globalData, page: this.page }
     return cite(context, ...args)
   })
 
   eleventyConfig.addShortcode('contributor', function(...args) {
-    const context = { eleventyConfig, globalData, page: this.page }
+    const context = { eleventyConfig, globalData }
     return contributor(context, ...args)
   })
 
   eleventyConfig.addShortcode('icon', function(...args) {
-    const context = { eleventyConfig, globalData, page: this.page }
+    const context = { eleventyConfig, globalData }
     return icon(context, ...args)
   })
 
   eleventyConfig.addShortcode('figure', function(...args) {
-    const context = { eleventyConfig, globalData, page: this.page }
+    const context = { eleventyConfig, globalData }
     return figure(context, ...args)
   })
 
   eleventyConfig.addShortcode('figuregroup', function(...args) {
-    const context = { eleventyConfig, globalData, page: this.page }
+    const context = { eleventyConfig, globalData }
     return figureGroup(context, ...args)
   })
 
   eleventyConfig.addShortcode('ref', function(ids) {
-    const context = { eleventyConfig, globalData, page: this.page }
+    const context = { eleventyConfig, globalData }
     return ref(context, ids)
   })
 
   eleventyConfig.addShortcode('title', function() {
-    const context = { eleventyConfig, globalData, page: this.page }
+    const context = { eleventyConfig, globalData }
     return title(context)
   })
 
   eleventyConfig.addShortcode('tombstone', function(...args) {
-    const context = { eleventyConfig, globalData, page: this.page }
+    const context = { eleventyConfig, globalData }
     return tombstone(context, ...args)
   })
 
@@ -69,7 +72,7 @@ module.exports = function(eleventyConfig, options) {
   eleventyConfig.namespace('qfigure', () => {
     Object.keys(figureComponents).forEach((name) => 
       eleventyConfig.addShortcode(name, function(...args) {
-        const context = { eleventyConfig, globalData, page: this.page }
+        const context = { eleventyConfig, globalData }
         return figureComponents[name](context, ...args)
       })
     )
