bcd9fb8a10a5f29a0aaed4fbe133241252c77d25
matthew p hrudka <mhrudka@getty.edu>
2022-03-03 13:33:00 -0800

    Add custom Liquid tags for shortcodes
    Implement Liquid template tags using the liquid-args parser to allow
    Nunjucks style keyword arguments.

--- packages/11ty/plugins/components/index.js
+++ packages/11ty/plugins/components/index.js
@@ -1,3 +1,5 @@
+const liquidArgs = require('liquid-args')
+// const { Liquid, Hash } = require('liquidjs')
 const components = require('../../_includes/components')
 
 /**
@@ -8,8 +10,30 @@ const components = require('../../_includes/components')
  */
 module.exports = function(eleventyConfig, options) {
   for (const component in components) {
-    eleventyConfig.addShortcode(component, function(...args) {
+    eleventyConfig.addJavaScriptFunction(component, function(...args) {
       return components[component](eleventyConfig, ...args)
+    })
+
+    const renderComponent = function(...args) {
+      const kwargs = args.find((arg) => arg.__keywords)
+      return components[component](eleventyConfig, kwargs)
+    }
+
+    eleventyConfig.addLiquidTag(component, function(liquidEngine) {
+      return {
+        parse: function(tagToken) {
+          this.args = tagToken.args //new Hash(tagToken.args)
+        },
+        render: async function(scope) {
+          const evalValue = (arg) => liquidEngine.evalValue(arg, scope)
+          const args = await Promise.all(liquidArgs(this.args, evalValue))
+          return renderComponent(...args)
+        }
+        // * render(context, emitter) {
+        //   const args = yield this.args.render(context)
+        //   emitter.write(component[component](eleventyConfig, ...args))
+        // }
+      }
     })
   }
 }
