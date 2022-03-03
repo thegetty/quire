b92c28fa55b575a3d20c48ed1d5db990fbbd49ef
matthew p hrudka <mhrudka@getty.edu>
2022-03-03 13:33:00 -0800

    Add custom Liquid tags for shortcodes
    Implement Liquid template tags using the liquid-args parser to allow
    Nunjucks style keyword arguments.

--- packages/11ty/plugins/shortcodes/index.js
+++ packages/11ty/plugins/shortcodes/index.js
@@ -7,10 +7,11 @@ const div = require('./div.js')
 const figure = require('./figure/index.js')
 const figureComponents = require('./figure/components')
 const figureGroup = require('./figureGroup.js')
-const icon = require('./icon.js')
 const ref = require('./figureRef.js')
 const title = require('./title.js')
 const tombstone = require('./tombstone.js')
+
+const liquidArgs = require('liquid-args')
 
 module.exports = function(eleventyConfig, options) {
   eleventyConfig.addPairedShortcode('backmatter', function(content, ...args) {
@@ -23,29 +24,120 @@ module.exports = function(eleventyConfig, options) {
     return div(context, content, ...args)
   })
 
-  eleventyConfig.addShortcode('cite', function(...args) {
+  eleventyConfig.addJavaScriptFunction('cite', function(...args) {
     const context = { eleventyConfig, globalData, page: this.page }
     return cite(context, ...args)
   })
 
-  eleventyConfig.addShortcode('contributor', function(...args) {
+  eleventyConfig.addLiquidTag('cite', function(liquidEngine) {
+    const context = { eleventyConfig, globalData, page: this.page }
+    const renderComponent = function(...args) {
+      const kwargs = args.find((arg) => arg.__keywords)
+      return cite(context, kwargs)
+    }
+    return {
+      parse: function(tagToken) {
+        this.args = tagToken.args
+      },
+      render: async function(scope) {
+        const evalValue = (arg) => liquidEngine.evalValue(arg, scope)
+        const args = await Promise.all(liquidArgs(this.args, evalValue))
+        return renderComponent(...args)
+      }
+    }
+  })
+
+
+  eleventyConfig.addJavaScriptFunction('contributor', function(...args) {
     const context = { eleventyConfig, globalData, page: this.page }
     return contributor(context, ...args)
   })
 
-  eleventyConfig.addShortcode('icon', function(...args) {
+  eleventyConfig.addLiquidTag('contributor', function(liquidEngine) {
+    const context = { eleventyConfig, globalData, page: this.page }
+    const renderComponent = function(...args) {
+      const kwargs = args.find((arg) => arg.__keywords)
+      return contributor(context, kwargs)
+    }
+    return {
+      parse: function(tagToken) {
+        this.args = tagToken.args
+      },
+      render: async function(scope) {
+        const evalValue = (arg) => liquidEngine.evalValue(arg, scope)
+        const args = await Promise.all(liquidArgs(this.args, evalValue))
+        return renderComponent(...args)
+      }
+    }
+  })
+
+  eleventyConfig.addJavaScriptFunction('qicon', function(...args) {
     const context = { eleventyConfig, globalData, page: this.page }
     return icon(context, ...args)
   })
 
-  eleventyConfig.addShortcode('figure', function(...args) {
+  eleventyConfig.addLiquidTag('qicon', function(liquidEngine) {
+    const context = { eleventyConfig, globalData, page: this.page }
+    const renderComponent = function(...args) {
+      const kwargs = args.find((arg) => arg.__keywords)
+      return icon(context, kwargs)
+    }
+    return {
+      parse: function(tagToken) {
+        this.args = tagToken.args
+      },
+      render: async function(scope) {
+        const evalValue = (arg) => liquidEngine.evalValue(arg, scope)
+        const args = await Promise.all(liquidArgs(this.args, evalValue))
+        return renderComponent(...args)
+      }
+    }
+  })
+
+  eleventyConfig.addJavaScriptFunction('figure', function(...args) {
     const context = { eleventyConfig, globalData, page: this.page }
     return figure(context, ...args)
   })
 
-  eleventyConfig.addShortcode('figuregroup', function(...args) {
+  eleventyConfig.addLiquidTag('figure', function(liquidEngine) {
+    const context = { eleventyConfig, globalData, page: this.page }
+    const renderComponent = function(...args) {
+      const kwargs = args.find((arg) => arg.__keywords)
+      return figure(context, kwargs)
+    }
+    return {
+      parse: function(tagToken) {
+        this.args = tagToken.args
+      },
+      render: async function(scope) {
+        const evalValue = (arg) => liquidEngine.evalValue(arg, scope)
+        const args = await Promise.all(liquidArgs(this.args, evalValue))
+        return renderComponent(...args)
+      }
+    }
+  })
+
+  eleventyConfig.addJavaScriptFunction('figuregroup', function(...args) {
     const context = { eleventyConfig, globalData, page: this.page }
     return figureGroup(context, ...args)
+  })
+
+  eleventyConfig.addLiquidTag('figuregroup', function(liquidEngine) {
+    const context = { eleventyConfig, globalData, page: this.page }
+    const renderComponent = function(...args) {
+      const kwargs = args.find((arg) => arg.__keywords)
+      return figureGroup(context, kwargs)
+    }
+    return {
+      parse: function(tagToken) {
+        this.args = tagToken.args
+      },
+      render: async function(scope) {
+        const evalValue = (arg) => liquidEngine.evalValue(arg, scope)
+        const args = await Promise.all(liquidArgs(this.args, evalValue))
+        return renderComponent(...args)
+      }
+    }
   })
 
   eleventyConfig.addShortcode('ref', function(ids) {
@@ -68,7 +160,7 @@ module.exports = function(eleventyConfig, options) {
    */
   eleventyConfig.namespace('qfigure', () => {
     Object.keys(figureComponents).forEach((name) => 
-      eleventyConfig.addShortcode(name, function(...args) {
+      eleventyConfig.addJavaScriptFunction(name, function(...args) {
         const context = { eleventyConfig, globalData, page: this.page }
         return figureComponents[name](context, ...args)
       })
