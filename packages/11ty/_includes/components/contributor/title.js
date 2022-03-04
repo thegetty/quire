5b284f777b2e315d02a11c3eec7ea390b0610ab6
Hannah Balenda <hbalenda@getty.edu>
2022-03-03 17:20:44 -0800

    Fix expected params structure

--- packages/11ty/_includes/components/contributor/title.js
+++ packages/11ty/_includes/components/contributor/title.js
@@ -1,6 +1,7 @@
 /**
- * @param  {Object} context
- * @param  {Object} contributor
+ * @param  {Object} eleventyConfig
+ * @param  {Object} globalData
+ * @param  {Object} params
  * @property  {String} affiliation
  * @property  {String} title
  * 
@@ -8,7 +9,7 @@
  */
 module.exports = function(eleventyConfig, globalData) {
   return function (params) {
-    const { affiliation, title } = contributor
+    const { affiliation, title } = params
     affiliation = affiliation ? `<span class="quire-contributor__affiliation">${ affiliation }</span>` : ''
     title = title ? `<span class="quire-contributor__title">${ title }</span>` : ''
     return [title, affiliation].join(', ')
