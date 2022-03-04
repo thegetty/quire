8eb784596edc2c31b8e8128fdedef05edcff9e6f
matthew p hrudka <mhrudka@getty.edu>
2022-03-03 17:52:28 -0800

    Update contributor shortcode module to use closure

--- packages/11ty/plugins/shortcodes/contributor.js
+++ packages/11ty/plugins/shortcodes/contributor.js
@@ -18,7 +18,7 @@ const path = require('path')
  * 
  * @return {String} contributor markup
  */
-module.exports = function ({ eleventyConfig }, { contributor, format, entryType }) {
+module.exports = function (eleventyConfig, globalData) {
   const fullname = eleventyConfig.getFilter('fullname')
   const getContributor = eleventyConfig.getFilter('getContributor')
   const icon = eleventyConfig.getFilter('icon')
@@ -26,35 +26,40 @@ module.exports = function ({ eleventyConfig }, { contributor, format, entryType 
   const pageTitle = eleventyConfig.getFilter('pageTitle')
   const slugify = eleventyConfig.getFilter('slugify')
 
-  const { bio, id, imagePath, pages=[], url } = contributor
+  return function(params) {
+    const { contributor, format, entryType } = params
+    const { bio, id, imagePath, pages=[], url } = contributor
 
-  const name = fullname(contributor)
+    const name = fullname(contributor)
 
-  const contributorPages = pages.map(({ page, url }) => {
-    return `${link({
-      classes: ['quire-contributor__page-link'],
-      name: pageTitle({ page, withLabel: true }),
-      url,
-    })}`
-  })
-  return oneLine`
-    <ul class="quire-contributors-list bio">
-      <li class="quire-contributor" id="${slugify(name)}">
-        <div class="title is-5">
-          <span class="quire-contributor__name">${name}</span> 
-          ${link({ classes: ["quire-contributor__url"], name: icon({ type: 'link', description:'' }), url })}
-        </div>
-        <div class="media">
-          <div class="quire-contributor__details media-content">
-            <div class="media-left">
-              <img class="image quire-contributor__pic" src="${imagePath}" alt="Picture of ${name}">
-            </div>
-            <div class="quire-contributor__bio">
-              ${bio}
-            </div>
-            ${contributorPages}
+    const contributorPages = pages.map(({ page, url }) => {
+      return `${link({
+        classes: ['quire-contributor__page-link'],
+        name: pageTitle({ page, withLabel: true }),
+        url,
+      })}`
+    })
+
+    return oneLine`
+      <ul class="quire-contributors-list bio">
+        <li class="quire-contributor" id="${slugify(name)}">
+          <div class="title is-5">
+            <span class="quire-contributor__name">${name}</span>
+            ${link({ classes: ["quire-contributor__url"], name: icon({ type: 'link', description:'' }), url })}
           </div>
-        </div>
-      </li>
-    </ul>`
+          <div class="media">
+            <div class="quire-contributor__details media-content">
+              <div class="media-left">
+                <img class="image quire-contributor__pic" src="${imagePath}" alt="Picture of ${name}">
+              </div>
+              <div class="quire-contributor__bio">
+                ${bio}
+              </div>
+              ${contributorPages}
+            </div>
+          </div>
+        </li>
+      </ul>
+    `
+  }
 }
