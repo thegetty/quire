e6a63434a56ea94e44b5bed4c9c27d65a5c4a9d3
matthew p hrudka <mhrudka@getty.edu>
2022-03-03 16:50:18 -0800

    Refactor _includes/components to use closure

--- packages/11ty/_includes/components/head.js
+++ packages/11ty/_includes/components/head.js
@@ -4,70 +4,72 @@
  * @param      {Object}  eleventyConfig
  * @param      {Object}  data
  */
-module.exports = function(eleventyConfig, data) {
+module.exports = function(eleventyConfig, globalData) {
   const analytics = eleventyConfig.getFilter('analytics')
   const dublinCore = eleventyConfig.getFilter('dublinCore')
   const jsonld = eleventyConfig.getFilter('jsonld')
   const opengraph = eleventyConfig.getFilter('opengraph')
   const twitterCard = eleventyConfig.getFilter('twitterCard')
 
-  const { canonicalURL, config, page, publication } = data
+  const { canonicalURL, config, page, publication } = globalData
 
-  const title = page.title
-    ? `${page.title} | ${publication.title}`
-    : publication.title
+  return function (params) {
+    const title = page.title
+      ? `${page.title} | ${publication.title}`
+      : publication.title
 
-  const description = publication.description.full || publication.description.one_line
+    const description = publication.description.full || publication.description.one_line
 
-  const publisherLinks = publication.publisher
-    .filter(({ url }) => url)
-    .map(({ url }) => `<link rel="publisher" href="${ url }">`)
-    .join('\n')
+    const publisherLinks = publication.publisher
+      .filter(({ url }) => url)
+      .map(({ url }) => `<link rel="publisher" href="${ url }">`)
+      .join('\n')
 
-  const contributorLinks = publication.contributor
-    .filter(({ url }) => url)
-    .map(({ url }) => `<link rel="author" href="${ url }">`)
-    .join('\n')
+    const contributorLinks = publication.contributor
+      .filter(({ url }) => url)
+      .map(({ url }) => `<link rel="author" href="${ url }">`)
+      .join('\n')
 
-  const keywords = publication.subject
-    .filter(({ type }) => type === "keyword")
-    .map(({ name }) => name)
-    .join(', ')
+    const keywords = publication.subject
+      .filter(({ type }) => type === "keyword")
+      .map(({ name }) => name)
+      .join(', ')
 
-  return `
-    <head>
-      <meta charset="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <meta name="robots" content="noindex, nofollow"/>
+    return `
+      <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="robots" content="noindex, nofollow"/>
 
-      <title>${title}</title>
+        <title>${title}</title>
 
-      <meta name="description" content="${description}">
-      <meta name="keywords" content="${keywords}">
+        <meta name="description" content="${description}">
+        <meta name="keywords" content="${keywords}">
 
-      <link rel="canonical" href="${canonicalURL}">
-      <link rel="version-history" href="${publication.repositoryUrl}">
+        <link rel="canonical" href="${canonicalURL}">
+        <link rel="version-history" href="${publication.repositoryUrl}">
 
-      ${publisherLinks}
+        ${publisherLinks}
 
-      ${contributorLinks}
+        ${contributorLinks}
 
-      ${dublinCore(data)}
+        ${dublinCore(data)}
 
-      ${opengraph(data)}
+        ${opengraph(data)}
 
-      ${twitterCard(data)}
+        ${twitterCard(data)}
 
-      <script type="application/ld+json">${jsonld(data)}</script>
+        <script type="application/ld+json">${jsonld(data)}</script>
 
-      <link rel="icon" href="/_assets/img/icons/favicon.ico" />
-      <link rel="stylesheet" href="/_assets/styles/custom.css" />
-      <link rel="stylesheet" href="/_assets/styles/application.css" />
+        <link rel="icon" href="/_assets/img/icons/favicon.ico" />
+        <link rel="stylesheet" href="/_assets/styles/custom.css" />
+        <link rel="stylesheet" href="/_assets/styles/application.css" />
 
-      <!-- {% render 'polyfills/template.html' %} -->
+        <!-- {% render 'polyfills/template.html' %} -->
 
-      ${analytics(data)}
-    </head>
-  `
+        ${analytics(data)}
+      </head>
+    `
+  }
 }
--- packages/11ty/_includes/components/link.js
+++ packages/11ty/_includes/components/link.js
@@ -11,10 +11,15 @@ const { oneLine } = require('common-tags');
  * @param  {Array<String>} classes
  * @return {String}                anchor tag
  */
-module.exports = function (eleventyConfig, { classes = [], link_relation, media_type, name, url }) {
-  return oneLine`
-    <a href="${url}" class="${classes.join(' ')}" target="_blank" rel="${link_relation}" type="${media_type}">
-      ${name}
-    </a>
-  `
+module.exports = function(eleventyConfig, globalData) {
+  const link = eleventyConfig.getFilter('link')
+
+  return function (params) {
+    const { classes = [], link_relation, media_type, name, url } = params
+    return oneLine`
+      <a href="${url}" class="${classes.join(' ')}" target="_blank" rel="${link_relation}" type="${media_type}">
+        ${name}
+      </a>
+    `
+  }
 }
