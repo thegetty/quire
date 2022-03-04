c8e0781fb34642e3a999f4af2c35050c13a7770b
matthew p hrudka <mhrudka@getty.edu>
2022-03-03 17:05:25 -0800

    Refactor to use closure for component functions

--- packages/11ty/_includes/components/copyright/index.js
+++ packages/11ty/_includes/components/copyright/index.js
@@ -6,63 +6,66 @@ const path = require('path')
  * 
  * @return {String}
  */
-module.exports = function(eleventyConfig, data) {
-  const { config, publication } = data
+module.exports = function(eleventyConfig, globalData) {
   const copyrightLicensing = eleventyConfig.getFilter('copyrightLicensing')
   const markdownify = eleventyConfig.getFilter('markdownify')
 
-  const publisherImages = publication.publisher.filter((item) => item.logo).map(({ logo }) => {
-    return `<img src="${ path.join('/_assets', config.params.imageDir, logo) }" class="quire-copyright__icon__logo" alt="logo" />`
-  }).join('')
+  return function (params) {
+    const { config, publication } = globalData
 
-  const copyright = publication.copyright ? `<p>${markdownify(publication.copyright)}</p>` : ''
+    const publisherImages = publication.publisher.filter((item) => item.logo).map(({ logo }) => {
+      return `<img src="${ path.join('/_assets', config.params.imageDir, logo) }" class="quire-copyright__icon__logo" alt="logo" />`
+    }).join('')
 
-  const { license } = publication
+    const copyright = publication.copyright ? `<p>${markdownify(publication.copyright)}</p>` : ''
 
-  let licenseIcons = ''
-  if (license && config.params.licenseIcons) {
-    const licenseAbbreviations = license.abbreviation.split(' ')
-    licenseIcons += `<a rel="license" class="quire-copyright__icon__link" href="${ license.url }" target="_blank">`
-    licenseIcons += `<svg class="quire-copyright__icon">`
-    for (abbr of licenseAbbreviations) {
-      let licenseIcon = eleventyConfig.getFilter(abbr)
-      licenseIcons+=licenseIcon()
+    const { license } = publication
+
+    let licenseIcons = ''
+    if (license && config.params.licenseIcons) {
+      const licenseAbbreviations = license.abbreviation.split(' ')
+      licenseIcons += `<a rel="license" class="quire-copyright__icon__link" href="${ license.url }" target="_blank">`
+      licenseIcons += `<svg class="quire-copyright__icon">`
+      for (abbr of licenseAbbreviations) {
+        let licenseIcon = eleventyConfig.getFilter(abbr)
+        licenseIcons+=licenseIcon()
+      }
+      licenseIcons += `</svg></a>`
     }
-    licenseIcons += `</svg></a>`
+
+    let licenseText = ''
+
+    if (license.online_text) {
+      licenseText+= `
+        <div class="is-screen-only">
+          ${ markdownify(license.online_text) }
+        </div>
+      `
+    } else {
+      licenseText+=copyrightLicensing()
+    }
+
+    if (license.pdf_ebook_text) {
+      licenseText += `
+        <div class="is-print-only">
+          ${ markdownify(license.pdf_ebook_text) }
+        </div>
+      `
+    } else {
+      licenseText += `
+        <div class="is-print-only">
+          ${copyrightLicensing()}
+        </div>
+      `
+    }
+
+    return `
+    <div class="quire-copyright">
+      ${publisherImages}
+      ${copyright}
+      ${licenseIcons}
+      ${licenseText}
+    </div>
+  `
   }
-
-  let licenseText = ''
-
-  if (license.online_text) {
-    licenseText+= `
-      <div class="is-screen-only">
-        ${ markdownify(license.online_text) }
-      </div>
-    `
-  } else {
-    licenseText+=copyrightLicensing()
-  }
-
-  if (license.pdf_ebook_text) {
-    licenseText += `
-      <div class="is-print-only">
-        ${ markdownify(license.pdf_ebook_text) }
-      </div>
-    `
-  } else {
-    licenseText += `
-      <div class="is-print-only">
-        ${copyrightLicensing()}
-      </div>
-    `
-  }
-
-  return `
-  <div class="quire-copyright">
-    ${publisherImages}
-    ${copyright}
-    ${licenseIcons}
-    ${licenseText}
-  </div>
-`
 }
--- packages/11ty/_includes/components/license-icons/BY.js
+++ packages/11ty/_includes/components/license-icons/BY.js
@@ -1,13 +1,15 @@
 const path = require('path')
 
-module.exports = function(eleventyConfig, data) {
-  const { config } = data
-  return `
-    <switch>
-      <use xlink:href="#cc-by"></use>
-      <foreignObject width="135" height="30">
-          <img src="${ path.join(config.params.imageDir, 'icons', 'cc-by.png') }" alt="CC-BY" />
-      </foreignObject>
-    </switch>
-  `
+module.exports = function(eleventyConfig, globalData) {
+  const imageDir = globalData.config.params.imageDir
+  return function(params) {
+    return `
+      <switch>
+        <use xlink:href="#cc-by"></use>
+        <foreignObject width="135" height="30">
+            <img src="${path.join(imageDir, 'icons', 'cc-by.png')}" alt="CC-BY" />
+        </foreignObject>
+      </switch>
+    `
+  }
 }
