10923bfda63ab103e332d4a7cc7265e5b767ab9a
Hannah Balenda <hbalenda@getty.edu>
2022-03-08 16:32:04 -0800

    Update contents layout name to table-of-contents

--- packages/11ty/_includes/components/menu/list.js
+++ packages/11ty/_includes/components/menu/list.js
@@ -19,17 +19,17 @@ module.exports = function(eleventyConfig, globalData) {
       .filter(({ data }) => data.menu !== false)
       .map((page) => {
         let listItem = ''
-        if (page.data.layout !== 'contents' && !page.data.section) {
+        if (page.data.layout !== 'table-of-contents' && !page.data.section) {
           return `<li class="page-item">${menuItem({ page: page.data })}</li>`
         } else if (
-          page.data.layout === 'contents' &&
+          page.data.layout === 'table-of-contents' &&
           page.data.section !== renderedSection
         ) {
           renderedSection = page.data.section
           listItem += `<li class="section-item">${menuItem({ page: page.data })}`
           if (config.params.tocType === 'full') {
             subListItems = pages
-              .filter((item) => item.data.section === page.data.section && item.data.layout !== 'contents')
+              .filter((item) => item.data.section === page.data.section && item.data.layout !== 'table-of-contents')
               .map((item) => {
                 if (page.fileSlug !== item.fileSlug)
                   return `<li class="page-item">${menuItem({ page: item.data })}</li>`
