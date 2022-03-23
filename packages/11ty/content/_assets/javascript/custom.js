e40f87c27a0cdc3bc0fe03dc5fe7fc0f94338178
Hannah Balenda <hbalenda@getty.edu>
2022-03-23 10:36:07 -0700

    Comment out client-side handling of choices

--- packages/11ty/content/_assets/javascript/custom.js
+++ packages/11ty/content/_assets/javascript/custom.js
@@ -3,23 +3,26 @@
 // A number of JavaScript functions and libraries are included with Quire,
 // to see which ones, check the files in themes/quire-starter-theme/source/js // and the list of dependencies in themes/quire-starter-theme/package.json
 
-window.onload = function () {
-  const canvasPanels = document.getElementsByTagName('canvas-panel');
+/**
+ * @todo Commented out for now, would prefer to handle choices on the server
+ */
+// window.onload = function () {
+//   const canvasPanels = document.getElementsByTagName('canvas-panel');
 
-  console.log('canvas panels', canvasPanels)
+//   console.log('canvas panels', canvasPanels)
 
-  const loadChoices = async function () {
-    for (const canvasPanel of canvasPanels) {
-      const manifesetId = canvasPanel.getAttribute('manifest-id')
-      await canvasPanel.vault.loadManifest(manifesetId);
+//   const loadChoices = async function () {
+//     for (const canvasPanel of canvasPanels) {
+//       const manifesetId = canvasPanel.getAttribute('manifest-id')
+//       await canvasPanel.vault.loadManifest(manifesetId);
 
-      canvasPanel.addEventListener('choice', (event) => {
-        const choicesEl = document.createElement('span');
-        choicesEl.innerHTML ='This manifest has choices';
-        event.target.parentNode.insertBefore(choicesEl, event.target);
-      }, { once: true });
-    }
-  };
+//       canvasPanel.addEventListener('choice', (event) => {
+//         const choicesEl = document.createElement('span');
+//         choicesEl.innerHTML ='This manifest has choices';
+//         event.target.parentNode.insertBefore(choicesEl, event.target);
+//       }, { once: true });
+//     }
+//   };
 
-  loadChoices();
-};
+//   loadChoices();
+// };
