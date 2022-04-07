cb4cb2c9a5534777905c43674b4ba89c46111290
Hannah Balenda <hbalenda@getty.edu>
2022-04-07 11:26:50 -0700

    Add processImages method to _plugins/iiif/lib

--- packages/11ty/_plugins/iiif/index.js
+++ packages/11ty/_plugins/iiif/index.js
@@ -1,25 +1,8 @@
-const fs = require('fs-extra')
-const path = require('path')
-const { build: biiif } = require('biiif');
+const processImages = require('./lib/processImages')
 
-module.exports = function(eleventyConfig, options) {
+module.exports = function (eleventyConfig, options = {}) {
   eleventyConfig.on('beforeBuild', () => {
-    console.warn(`[eleventy:beforeBuild:iiif] Checking for new images...`)
-    const iiifDirectory = path.join('content', '_assets', 'images', 'iiif')
-    const filenames = fs.readdirSync(iiifDirectory)
-    filenames.forEach(filename => {
-      const filePath = path.join(iiifDirectory, filename)
-      if (fs.lstatSync(filePath).isDirectory()) {
-        const manifest = path.join(filePath, 'index.json')
-        if (!fs.pathExistsSync(manifest)) {
-          console.warn(`[eleventy:beforeBuild:iiif] Processing "${filename}"`)
-          biiif(filePath, path.join('http://localhost:3000', 'images', 'iiif'))  
-        } else {
-          console.warn(`[eleventy:beforeBuild:iiif] Image "${filename}" has already been processed, skipping.`)
-        }
-      }
-    })
-    console.warn('[eleventy:beforeBuild:iiif] Done')
+    processImages(options.processImages)
   })
   // eleventyConfig.on('afterBuild', () => {})
 }
