1085484aea88dedf9c7977968aabab2851ce5325
Hannah Balenda <hbalenda@getty.edu>
2022-04-26 11:21:02 -0700

    Fix typo and add jsdoc todo

--- packages/11ty/_plugins/shortcodes/canvasPanel.js
+++ packages/11ty/_plugins/shortcodes/canvasPanel.js
@@ -46,11 +46,14 @@ module.exports = function(eleventyConfig, globalData) {
       case !!manifestId && !!canvasId:
         break;
       case !!id:
+        /**
+         * @todo replace with directory defined in IIIF config
+         */
         canvasId = new URL(['_assets', 'images', '_iiif', id, 'canvas'].join('/'), process.env.URL)
         manifestId = new URL(['_assets', 'images', '_iiif', id, 'manifest.json'].join('/'), process.env.URL)
         break;
       default:
-        console.warn(`Error in CanvasPanel shortode: Missing params canvasId or manifestId. Fig.id: `, id)
+        console.warn(`Error in CanvasPanel shortcode: Missing params canvasId or manifestId. Fig.id: `, id)
         return;
     }
     
