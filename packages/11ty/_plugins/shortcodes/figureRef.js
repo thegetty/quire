2c8b675ad2f36e09a41c050184e24161ac957f95
matthew p hrudka <mhrudka@getty.edu>
2022-05-10 13:34:26 -0700

    Refactor figureRef shortcode

--- packages/11ty/_plugins/shortcodes/figureRef.js
+++ packages/11ty/_plugins/shortcodes/figureRef.js
@@ -1,28 +1,39 @@
-const { oneLineCommaLists } = require('common-tags')
+const { oneLineCommaListsAnd } = require('common-tags')
 
 /**
  * Generate markdown for an inline list of links to figures on the page.
  *
- * @example.liquid
- *  ({% ref 'fig-4', 'fig-5', 'fig-6', 'fig-7' %})
- * renders the following markdown
- *  (fig. [4](#fig-4), [5](#fig-5), [6](#fig-6), [7](#fig-7))
+ * @example reference a single figure
+ *   {% ref 'fig-4' %}
+ *  renders the following markdown string
+ *   [fig. 4](#fig-4)
+ *
+ * @example reference two figures
+ *   {% ref 'fig-4', 'fig-5' %}
+ *  renders the following markdown string
+ *   [figs. 4](#fig-4) and [5](#fig-5)
+ *
+ * @example reference three or more figures
+ *   {% ref 'fig-4', 'fig-5', 'fig-6', 'fig-7' %}
+ *  renders the following markdown string
+ *   [figs. 4](#fig-4), [5](#fig-5), [6](#fig-6), and [7](#fig-7)
  */
 module.exports = function(eleventyConfig) {
-  const fig = 'fig.'
-
   return function (ids) {
+    if (!ids.length) {
+      console.warn(`Error: NoId: Figure 'ref' shortcode must include one or more values corresponding to the 'id' of a figure on the page. @example {% ref 'fig-1', 'fig-7', 'fig-11' %}`)
+    }
+
+    const label = ids.length > 1 ? 'figs.' : 'fig.'
+
     // transform the array of figure ids into and array of markdown links
-    const links = ids.split(',').map((id) => {
+    const links = ids.split(',').map((index, id) => {
       id = id.trim()
-      const text = id.replace('fig-', '')
+      let text = id.replace('/^fig-/i', '')
+      if (index === 0) text = `${label} ${text}`
       return `[${text}](#${id})`
     })
 
-    if (!ids.length) {
-      console.warn(`Error: NoId: the q-ref shortcode must include one or more 'id' values that correspond to the 'id' of a figure on the page. @example {% q-ref 'fig-1', 'fig-7', 'fig-11' %}`)
-    }
-
-    return oneLineCommaLists`${fig} ${links}`
+    return oneLineCommaListsAnd`${links}`
   }
 }
