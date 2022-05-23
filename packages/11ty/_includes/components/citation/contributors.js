385ab1261e0786f8adf7e4c91ac29a87f448c8e9
Hannah Balenda <hbalenda@getty.edu>
2022-05-23 10:26:49 -0700

    Fix citation contributors string for more than max number of contributors

--- packages/11ty/_includes/components/citation/contributors.js
+++ packages/11ty/_includes/components/citation/contributors.js
@@ -1,46 +1,44 @@
 /**
  * Renders a list of contributors with options
  * Used across MLA and Chicago formats
- * 
+ *
  * @param     {Object}  eleventyConfig
  * @param     {Object}  params
  * @property  {Array}   contributors
  * @param     {Object}  options
  * @property  {Boolean} reverse If true, returns family name before surname
- * @property  {Number}  max The maximum number of contributors who will be listed by name. 
+ * @property  {Number}  max The maximum number of contributors who will be listed by name.
  * If there are more than the max, ", et al" will be appended.
  * @property  {String}  separator The separator that will be used to join names
- * 
+ *
  * @return {String}
  */
-module.exports = function(eleventyConfig) {
-  const fullname = eleventyConfig.getFilter('fullname')
-  const getContributor = eleventyConfig.getFilter('getContributor')
+module.exports = function (eleventyConfig) {
+  const fullname = eleventyConfig.getFilter('fullname');
+  const getContributor = eleventyConfig.getFilter('getContributor');
 
   return function (params) {
-    const { contributors, max, reverse, separator } = params
-    if (!Array.isArray(contributors)) return ''
+    const { contributors, max, reverse, separator } = params;
+    if (!Array.isArray(contributors)) return '';
 
-    const contributorObjects = contributors.map((item) => getContributor(item))
+    const contributorObjects = contributors.map((item) => getContributor(item));
 
-    let pageContributors = []
+    let pageContributors = [];
 
     for (const [i, contributor] of contributorObjects.entries()) {
-      if (i <= max) {
-        pageContributors.push(fullname(contributor, { reverse: reverse }))
+      const lastContributor = i + 1 === contributors.length || i + 1 === max;
+
+      pageContributors.push(
+        fullname(contributor, { reverse }).replace(/\.$/, '')
+      );
+      if (lastContributor) {
+        pageContributors = pageContributors.join(separator);
+        break;
       }
-      if (contributors.length === 1) {
-        pageContributors = pageContributors.join('')
-      }
-      if (i === 1) {
-        pageContributors = pageContributors.join(separator)
-      }
-      if (contributors.length > max) {
-        pageContributors += ', et al'
-      }
-      if (i === max) break
     }
 
-    return pageContributors.replace(/\.$/, '')
-  }
-}
+    return contributors.length > max
+      ? (pageContributors += ', et al')
+      : pageContributors;
+  };
+};
