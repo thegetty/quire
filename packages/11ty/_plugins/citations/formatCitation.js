2a90d43a548daeb4f12110df604e59a4473a4406
Hannah Balenda <hbalenda@getty.edu>
2022-06-02 17:28:53 -0700

    Add error message for unrecognized styles

--- packages/11ty/_plugins/citations/formatCitation.js
+++ packages/11ty/_plugins/citations/formatCitation.js
@@ -4,10 +4,18 @@ const styles = require('./styles')
 
 module.exports = function(item, params) {
   const { type } = params
+
+  const style = styles[type]
+
+  if (!style) {
+    console.error(`Citation style "${type}" is not supported. You may need to add it to _plugins/citations/styles.`)
+    return;
+  }
+
   const processor = new Processor({
     items: [item],
     locale,
-    style: styles[type]
+    style
   })
   const citation = processor.cite({ citationItems: [{ id: item.id }] })
   return processor.bibliography().value
