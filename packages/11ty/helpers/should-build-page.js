a22e42193811bb0b473b318c796e225229bf034f
Hannah Balenda <hbalenda@getty.edu>
2022-06-07 12:20:18 -0700

    Rename .env.OUTPUT -> .env.QUIRE_OUTPUT

--- packages/11ty/helpers/should-build-page.js
+++ packages/11ty/helpers/should-build-page.js
@@ -1,6 +1,6 @@
 /**
  * Returns true if page should be built
- * for the current output type defined in `env.OUTPUT`
+ * for the current output type defined in `env.QUIRE_OUTPUT`
  * @param  {Object} outputs Page frontmatter property `outputs`
  * @return {Boolean}
  */
@@ -8,6 +8,6 @@ module.exports = function (outputs) {
   return outputs !== "none" &&
     outputs !== false &&
     (!outputs ||
-      Array.isArray(outputs) && outputs.includes(process.env.OUTPUT) ||
-      outputs === process.env.OUTPUT)
+      Array.isArray(outputs) && outputs.includes(process.env.QUIRE_OUTPUT) ||
+      outputs === process.env.QUIRE_OUTPUT)
 }
