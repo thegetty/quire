5a6a9275db78b97901df577517bfc3b889adf354
Hannah Balenda <hbalenda@getty.edu>
2022-06-07 12:24:34 -0700

    Pass anonymous fns to filter

--- packages/11ty/_plugins/collections/index.js
+++ packages/11ty/_plugins/collections/index.js
@@ -9,27 +9,26 @@ module.exports = function (eleventyConfig, options = {}) {
    * Collection of pages for the current output (epub, html, or pdf)
    */
   eleventyConfig.addCollection('current', function (collectionApi) {
-    return collectionApi.getAll().filter(function (page) {
-      const { outputs } = page.data
-      return shouldBuildPage(outputs)
-    });
-  });
+    return collectionApi
+      .getAll()
+      .filter((page) => shouldBuildPage(page.data.outputs))
+  })
   /**
    * Collection of pages to display in the menu
    */
   eleventyConfig.addCollection('menu', function (collectionApi) {
-    return collectionApi.getAll('current').filter(function (page) {
+    return collectionApi.getAll('current').filter((page) => {
       const { menu, outputs, type } = page.data
-      return shouldBuildPage(outputs) && menu !== false && type !== 'data';
-    });
-  });
+      return shouldBuildPage(outputs) && menu !== false && type !== 'data'
+    })
+  })
   /**
    * Collection of pages to display in Table of Contents
    */
   eleventyConfig.addCollection('tableOfContents', function (collectionApi) {
-    return collectionApi.getAll('current').filter(function (page) {
+    return collectionApi.getAll('current').filter((page) => {
       const { outputs, toc, type } = page.data
-      return shouldBuildPage(outputs) && toc !== false && type !== 'data';
-    });
-  });
-};
+      return shouldBuildPage(outputs) && toc !== false && type !== 'data'
+    })
+  })
+}
