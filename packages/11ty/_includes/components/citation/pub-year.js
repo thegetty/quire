46d315c38346377bbdc756b99b3f520ceec4d2ea
Hannah Balenda <hbalenda@getty.edu>
2022-06-13 11:58:15 -0700

    Use citation-styles for citations

--- packages/11ty/_includes/components/citation/pub-year.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(eleventyConfig) {
-  const pubDate = eleventyConfig.globalData.publication.pub_date
-  return function (params) {
-    return new Date(pubDate).getFullYear()
-  }
-}
