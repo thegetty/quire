df35017cfc758d83f74e1386ac44fd274e5fc2e1
matthew p hrudka <mhrudka@getty.edu>
2022-06-15 16:26:14 -0700

    Call htmlForPdf tranform with Eleventy app context

--- packages/11ty/_plugins/transforms/index.js
+++ packages/11ty/_plugins/transforms/index.js
@@ -11,6 +11,10 @@ const htmlForPdf = require('./html-for-pdf')
 module.exports = function(eleventyConfig, collections, options = {}) {
   // eleventyConfig.addTransform('format', format)
   eleventyConfig.addTransform('htmlForPdf', function(content) {
-    return htmlForPdf(collections, content)
+    /**
+     * Nota bene:
+     * call transform with `this` context to ensure we have `this.outputPath`
+     */
+    return htmlForPdf.call(this, collections, content)
   })
 }
