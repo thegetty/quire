550b1d780e30515d62463aed56cb78276afb5e78
matthew p hrudka <mphstudios@users.noreply.github.com>
2022-06-15 10:17:17 -0700

    Feature: Add tranform to format output using Prettier

---
+++ packages/11ty/_plugins/transforms/index.js
@@ -0,0 +1,12 @@
+const format = require('./format')
+
+/**
+ * An Eleventy plugin to configure output transforms
+ *
+ * @param      {Object}  eleventyConfig  eleventy configuration
+ * @param      {Object}  options
+ */
+
+module.exports = function(eleventyConfig, options) {
+  eleventyConfig.addTransform('format', format)
+}
