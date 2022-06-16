5c931b5d98b8f453e364052307491c3e7849b1a1
matthew p hrudka <mhrudka@getty.edu>
2022-06-15 20:09:43 -0700

    Rename directory temp to _temp

--- packages/11ty/_plugins/transforms/html-for-pdf/write.js
+++ packages/11ty/_plugins/transforms/html-for-pdf/write.js
@@ -1,24 +1,27 @@
 const fs = require('fs-extra')
 const jsdom = require('jsdom')
-const { JSDOM } = jsdom
 const path = require('path')
 
 /**
  * Write each page section in the PDF collection to a single HTML file
  * @param  {Object} collection collections.pdf with `sectionContent` property
  */
-const outputPath = path.join('temp', 'pdf.html')
 const layoutPath = path.join('_plugins', 'transforms', 'html-for-pdf', 'layout.html')
+const outputPath = path.join('_temp', 'pdf.html')
 
 module.exports = function(collection) {
   const layout = fs.readFileSync(layoutPath)
-  const { document: doc } = new JSDOM(layout).window
+
+  const { JSDOM } = jsdom
+  const { document } = new JSDOM(layout).window
+
   collection.forEach(({ sectionContent }) => {
-    doc.body.appendChild(sectionContent)
+    document.body.appendChild(sectionContent)
   })
+
   try {
     fs.ensureDirSync(path.parse(outputPath).dir)
-    fs.writeFileSync(outputPath, doc.documentElement.outerHTML)
+    fs.writeFileSync(outputPath, document.documentElement.outerHTML)
   } catch(error) {
     console.error(`Eleventy transform error writing combined HTML output for PDF. Error message: `, error)
   }
