e8f1ead26b3e5abfa2a924bc187dcf2bf0881ff1
matthew p hrudka <mhrudka@getty.edu>
2022-06-16 17:33:26 -0700

    Refactor html-for-pdf plugin
    Check that output path is in the collection of PDF pages before parsing
    content.
    
    Transform relative links to in-page anchor links when building output
    for PDF.

--- packages/11ty/_plugins/transforms/html-for-pdf/index.js
+++ packages/11ty/_plugins/transforms/html-for-pdf/index.js
@@ -7,30 +7,52 @@ const writePDF = require('./write')
  * @param  {String} content Page HTML
  * @return {Object}
  */
-const getPageSection = function(content) {
-  const { document: doc } = new JSDOM(content).window
-  return doc.querySelector('section[data-output-path]')
+const getSectionElement = (content) => {
+  const { document } = new JSDOM(content).window
+  return document.querySelector('section[data-output-path]')
+}
+
+/**
+ * Transform relative links to anchor links
+ *
+ * @param      {HTMLElement}  element
+ */
+const transformRelativeLinks = (element) => {
+  const nodes = element.querySelectorAll('a')
+  nodes.forEach((a) => {
+    const url = a.getAttribute('href')
+    a.setAttribute('href', `#${url}`)
+  })
+  return element
 }
 
 module.exports = function(collections, content) {
-  const htmlPaths = collections.html.map(({ outputPath }) => outputPath)
+  const htmlPages = collections.html.map(({ outputPath }) => outputPath)
+  const pdfPages = collections.pdf.map(({ outputPath }) => outputPath)
 
-  const section = getPageSection(content)
+  if (pdfPages.includes(this.outputPath)) {
+    const pageIndex = pdfPages.findIndex((path) => path === this.outputPath)
+    const sectionElement = getSectionElement(content)
 
-  if (section) {
-    const sectionOutputPath = section.getAttribute('data-output-path')
-    const pdfIndex = collections.pdf.findIndex(({ outputPath }) => {
-      return sectionOutputPath === outputPath
-    })
+    if (sectionElement) {
+      const identifier = sectionElement.getAttribute('data-output-path')
 
-    if (pdfIndex !== -1) {
-      collections.pdf[pdfIndex].sectionContent = section
-    }
+      if (pageIndex !== -1) {
+        sectionElement.removeAttribute('data-output-path')
+        sectionElement.setAttribute('id', collections.pdf[pageIndex].url)
+        transformRelativeLinks(sectionElement)
+        collections.pdf[pageIndex].sectionElement = sectionElement
+      }
 
-    if (collections.pdf.every(({ sectionContent }) => !!sectionContent)) {
-      writePDF(collections.pdf)
+      /**
+       * Once this transform has been called for each PDF page
+       * every item in the collection will have `sectionConent`
+       */
+      if (collections.pdf.every(({ sectionElement }) => !!sectionElement)) {
+        writePDF(collections.pdf)
+      }
     }
   }
 
-  return htmlPaths.includes(this.outputPath) ? content : undefined
+  return htmlPages.includes(this.outputPath) ? content : undefined
 }
