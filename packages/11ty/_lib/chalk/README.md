709483b83811e4ba9cde6f13e73de24d4e92c806
matthew p hrudka <mhrudka@getty.edu>
2022-06-23 11:10:58 -0700

    WIP façade module for the chalk library

---
+++ packages/11ty/_lib/chalk/README.md
@@ -0,0 +1,3 @@
+## A façade module for the `chalk` library
+
+Nota bene: `chalk` v5 is an ESM only library, Eleventy uses v4. For more information see the [chalk v4 documentation](https://github.com/chalk/chalk/tree/v4.1.2).
---
+++ packages/11ty/_lib/chalk/index.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk')
+
+/**
+ * chalk themes
+ * @see https://github.com/chalk/chalk/tree/v4.1.2#usage
+ */
+const info = chalk.magenta
+const error = chalk.bold.red
+const warn = chalk.bold.keyword('yellow')
