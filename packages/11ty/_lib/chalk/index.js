87fe557e500867142bd227c5f829b52cd7f734f4
Hannah Balenda <hbalenda@getty.edu>
2022-06-23 17:27:49 -0700

    Style console methods

--- packages/11ty/_lib/chalk/index.js
+++ packages/11ty/_lib/chalk/index.js
@@ -1,9 +1,22 @@
 const chalk = require('chalk')
 
-/**
- * chalk themes
- * @see https://github.com/chalk/chalk/tree/v4.1.2#usage
- */
-const info = chalk.magenta
-const error = chalk.bold.red
-const warn = chalk.bold.keyword('yellow')
+module.exports = function(context) {
+  /**
+   * chalk themes
+   * @see https://github.com/chalk/chalk/tree/v4.1.2#usage
+   */
+  const styles = {
+    info: chalk.magenta,
+    error: chalk.bold.red,
+    warn: chalk.bold.keyword('yellow')
+  }
+  const logFn = (type) => {
+    const log = console[type]
+    const style = styles[type]
+    return (message) => log(style(chalk.bold(context), message))
+  }
+  return Object.keys(styles).reduce((logFns, type) => {
+    logFns[type] = logFn(type)
+    return logFns
+  }, {})
+}
