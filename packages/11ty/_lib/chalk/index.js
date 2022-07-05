74ea5a25dc3f14d063c37beddd8a5c16b10df622
matthew p hrudka <mhrudka@getty.edu>
2022-07-05 16:44:01 -0700

    Edits to _lib/chalk JSDoc comments

--- packages/11ty/_lib/chalk/index.js
+++ packages/11ty/_lib/chalk/index.js
@@ -1,27 +1,30 @@
 const chalk = require('chalk')
 
 /**
- * @param  {String} prefix A prefix for messages in terminal output,
- *                         likely the name of the module where the logging occurs
- * @return {Function}      Factory function that returns `info`, `error`, and `warn` log methods
+ * A factory function for custom logging methods using the chalk library
+ *
+ * @param  {String} prefix  A prefix for messages output to the terminal
+ * @return {Function}  An object with custom log methods `info`, `error`, `warn`
  */
-module.exports = function(prefix='') {
+module.exports = function (prefix='') {
   /**
    * chalk themes
    * @see https://github.com/chalk/chalk/tree/v4.1.2#usage
    */
   const styles = {
-    info: chalk.magenta,
     error: chalk.bold.red,
+    info: chalk.magenta,
     warn: chalk.bold.keyword('yellow')
   }
+
   const logFn = (type) => {
     const log = console[type]
     const style = styles[type]
     return (message) => log(style(chalk.bold(prefix), message))
   }
+
   /**
-   * @return {Object} `info`, `error`, and `warn` methods
+   * @return {Object} Customized logging methods `info`, `error`, `warn`
    */
   return Object.keys(styles).reduce((logFns, type) => {
     logFns[type] = logFn(type)
