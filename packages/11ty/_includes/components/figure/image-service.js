00118cf88033a17e650361fe2fb133e767499475
Hannah Balenda <hbalenda@getty.edu>
2022-07-14 14:55:52 -0700

    Set default preset to 'responsive'

--- packages/11ty/_includes/components/figure/image-service.js
+++ packages/11ty/_includes/components/figure/image-service.js
@@ -9,7 +9,7 @@ const { html } = require('~lib/common-tags')
  * @return     {String}  An <image-service> element
  */
 module.exports = function(eleventyConfig) {
-  return function({ alt='', height='', preset='', region='', iiif, virtualSizes='', width='' }) {
+  return function({ alt='', height='', preset='responsive', region='', iiif, virtualSizes='', width='' }) {
     return html`
       <image-service 
         alt="${alt}"
