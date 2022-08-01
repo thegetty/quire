8ae7a23786570ed8bc6969de1de7b5ac3f6a446e
Anders Pollack <pollack.anders@gmail.com>
2022-08-01 16:53:57 -0700

    Delete `youtube` figure shortcode

--- packages/11ty/_includes/components/figure/youtube/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(eleventyConfig) {
-  const renderOutputs = eleventyConfig.getFilter('renderOutputs')
-  return function(params) {
-    return renderOutputs(__dirname, params)
-  }
-}
