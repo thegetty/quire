7b5696ae5f8c2fea37dfc2dcb3830944b8bd343e
Anders Pollack <pollack.anders@gmail.com>
2022-08-01 16:50:22 -0700

    Implement `print.js` fallback for video; Rename video figure `src` key to `poster` in `figures.yaml`

---
+++ packages/11ty/_includes/components/figure/video/print.js
@@ -0,0 +1,28 @@
+const { html } = require('~lib/common-tags')
+const chalkFactory = require('~lib/chalk')
+
+/**
+ * Renders an image instead of a video player
+ *
+ * @param      {Object}  eleventyConfig  eleventy configuration
+ * @param      {Object}  figure          The figure
+ * @return     {String}  An HTML
+ */
+module.exports = function(eleventyConfig) {
+  const figurecaption = eleventyConfig.getFilter('figurecaption')
+  const figurelabel = eleventyConfig.getFilter('figurelabel')
+
+  const { figureLabelLocation } = eleventyConfig.globalData.config.params
+
+  return function({ aspect_ratio: aspectRatio, caption, credit, id, label, poster}) {
+    const isEmbed = mediaType === 'vimeo' || mediaType === 'youtube'
+
+    return html`
+      <div class="q-figure__media-wrapper--${ aspectRatio || 'widescreen' }">
+        <img src="${poster}" />
+      </div>
+      ${label && figureLabelLocation === 'on-top' ? figurelabel({ caption, id, label }) : ''}
+      ${figurecaption({ caption, credit })}
+    `
+  }
+}
