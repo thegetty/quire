35024c74a045f367128142eba128183d05a9e044
Anders Pollack <pollack.anders@gmail.com>
2022-08-04 15:53:20 -0700

    Render video poster images in print output with relative path including assets directory

--- packages/11ty/_includes/components/figure/video/print.js
+++ packages/11ty/_includes/components/figure/video/print.js
@@ -1,5 +1,6 @@
 const { html } = require('~lib/common-tags')
 const chalkFactory = require('~lib/chalk')
+const path = require('path')
 
 /**
  * Renders an image instead of a video player
@@ -14,12 +15,16 @@ module.exports = function(eleventyConfig) {
 
   const { figureLabelLocation } = eleventyConfig.globalData.config.params
 
+  const imageDir = eleventyConfig.globalData.config.params.imageDir.slice(1)
+
   return function({ aspect_ratio: aspectRatio, caption, credit, id, label, mediaType, poster}) {
     const isEmbed = mediaType === 'vimeo' || mediaType === 'youtube'
 
+    const posterSrc = path.join(imageDir, poster)
+
     return html`
       <div class="q-figure__media-wrapper--${ aspectRatio || 'widescreen' }">
-        <img src="${poster}" />
+        <img src="${posterSrc}" />
       </div>
       ${label && figureLabelLocation === 'on-top' ? figurelabel({ caption, id, label }) : ''}
       ${figurecaption({ caption, credit })}
