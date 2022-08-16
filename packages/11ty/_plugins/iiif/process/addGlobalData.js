bd096afe254966983c3ade6b60dec711e809bbfe
Hannah Balenda <hbalenda@getty.edu>
2022-08-16 14:40:18 -0700

    Return undefined from get-image-service if image is not an image service

--- packages/11ty/_plugins/iiif/process/addGlobalData.js
+++ packages/11ty/_plugins/iiif/process/addGlobalData.js
@@ -24,7 +24,7 @@ module.exports = async (eleventyConfig) => {
     const info = getImageService(eleventyConfig, figure)
     Object.assign(eleventyConfig.globalData.figures.figure_list[index], {
       isCanvas: !!iiif.canvas,
-      isImageService: info || false,
+      isImageService: !!info,
       iiif: {
         ...iiif,
         info
