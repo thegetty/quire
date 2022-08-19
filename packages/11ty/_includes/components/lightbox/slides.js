ef5f0e17660b9e94a81d16c6d12a533b198c266e
Anders Pollack <pollack.anders@gmail.com>
2022-08-19 12:18:28 -0700

    Refactor `components/lightbox/slides.js`

--- packages/11ty/_includes/components/lightbox/slides.js
+++ packages/11ty/_includes/components/lightbox/slides.js
@@ -15,52 +15,53 @@ module.exports = function(eleventyConfig) {
   return function(figures) {
     if (!figures) return ''
 
-    const slides = figures
-      .reduce((slides, figure, index) => {
-        const {
-          caption,
-          credit,
-          id,
-          iiif,
-          label,
-          media_type: mediaType,
-          preset,
-          src
-        } = figure
+    const slideElement = (fgure) => {
+      const {
+        caption,
+        credit,
+        id,
+        iiif,
+        label,
+        media_type: mediaType,
+        preset,
+        src
+      } = figure
 
-        if (mediaType === 'video' || mediaType === 'audio') return slides;
+      if (mediaType === 'video' || mediaType === 'audio') return slides;
 
-        const labelSpan = label
-          ? html`<span class="q-lightbox-slides__caption-label">${label}</span>`
-          : ''
-        const captionAndCreditSpan = caption || credit
-          ? html`<span class="q-lightbox-slides__caption-content">${caption ? markdownify(caption) : ''} ${credit ? credit : ''}</span>`
-          : ''
-        const captionElement = labelSpan.length || captionAndCreditSpan.length
-          ? html`
-            <div class="q-lightbox-slides__caption">
-              ${labelSpan}
-              ${captionAndCreditSpan}
-            </div>
-          `
-          : ''
-        const elementId = `lightbox-image-${index}`
-
-        slides.push(html`
-          <div
-            class="q-lightbox-slides__slide"
-            data-lightbox-slide
-            data-lightbox-id="${id}"
-          >
-            <div class="q-lightbox-slides__image">
-              ${figureImageElement(figure)}
-            </div>
-            ${captionElement}
+      const labelSpan = label
+        ? html`<span class="q-lightbox-slides__caption-label">${label}</span>`
+        : ''
+      const captionAndCreditSpan = caption || credit
+        ? html`<span class="q-lightbox-slides__caption-content">${caption ? markdownify(caption) : ''} ${credit ? credit : ''}</span>`
+        : ''
+      const captionElement = labelSpan.length || captionAndCreditSpan.length
+        ? html`
+          <div class="q-lightbox-slides__caption">
+            ${labelSpan}
+            ${captionAndCreditSpan}
           </div>
-        `)
+        `
+        : ''
 
-        return slides
-      }, []).join('')
+      return html`
+        <div
+          class="q-lightbox-slides__slide"
+          data-lightbox-slide
+          data-lightbox-id="${id}"
+        >
+          <div class="q-lightbox-slides__image">
+            ${figureImageElement(figure)}
+          </div>
+          ${captionElement}
+        </div>
+      `
+    }
+
+    let slides = ''
+    for (figure of figures) {
+      slides += slideElement(figure)
+    }
 
     return html`
       <div class="q-lightbox-slides">
