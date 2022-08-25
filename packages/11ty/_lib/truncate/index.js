e56f40971cf879056710f4dd5d4b887979f5b4ac
Hannah Balenda <hbalenda@getty.edu>
2022-08-25 11:12:45 -0700

    Update JSDOC

--- packages/11ty/_lib/truncate/index.js
+++ packages/11ty/_lib/truncate/index.js
@@ -1,6 +1,6 @@
 /**
  * Truncate a string
  * @param  {String} text  The string to truncate
- * @param  {String} limit The maximum number of characters to return
+ * @param  {Number} limit The maximum number of characters to return
  */
 module.exports = (text, limit) => text?.slice(0, limit)
