f84870bfb68b4c39ee4c924f120df0bb9a90abd4
Hannah Balenda <hbalenda@getty.edu>
2022-08-31 12:21:41 -0700

    Remove helper get-image-service

--- packages/11ty/_plugins/figures/helpers/index.js
+++ packages/11ty/_plugins/figures/helpers/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-  getImageService: require('./get-image-service'),
   getPrintImage: require('./get-print-image'),
   isCanvas: require('./is-canvas'),
   isImageService: require('./is-image-service')
