d139947eab347033003d44a8cf64bb6aa5eefcaa
Hannah Balenda <hbalenda@getty.edu>
2022-09-02 17:18:06 -0700

    Toggle image annotations using opacity

--- packages/11ty/content/_assets/javascript/application/canvas-panel.js
+++ packages/11ty/content/_assets/javascript/application/canvas-panel.js
@@ -10,21 +10,31 @@ window.onload = function () {
     if (!figure) return
     const annotationInputs = figure.querySelectorAll('.annotations-ui .annotations-ui__input')
     for (const annotationInput of annotationInputs) {
-      annotationInput.addEventListener('click', (event) => {
-        const id = event.target.getAttribute('id')
-        const type = annotationInput.getAttribute('data-annotation-type')
-        switch (type) {
-          case 'choice':
+      const id = annotationInput.getAttribute('id')
+      const type = annotationInput.getAttribute('data-annotation-type')
+      switch (type) {
+        case 'choice':
+          annotationInput.addEventListener('click', (event) => {
             canvasPanel.makeChoice(id)
-            break
-          case 'annotation':
-            // @todo select annotation
-            break
-          default:
-            console.warn(`Invalid annotation type ${type}`)
-            break
-        }
-      })
+          })
+          break
+        case 'annotation':
+          const toggleAnnotation = (input) => {
+            if (input.checked) {
+              canvasPanel.applyStyles(id, { opacity: 1 })
+            } else {
+              canvasPanel.applyStyles(id, { opacity: 0 })
+            }
+          }
+          toggleAnnotation(annotationInput)
+          annotationInput.addEventListener('click', ({ target }) => {
+            toggleAnnotation(target)
+          })
+          break
+        default:
+          console.warn(`Invalid annotation type ${type}`)
+          break
+      }
     }
   }
 }
