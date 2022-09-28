f3a3aa27fa8800a4615492eccce1737ebddf369c
Hannah Balenda <hbalenda@getty.edu>
2022-09-28 08:44:28 -0700

    Change DOMContentLoaded to load

--- packages/11ty/content/_assets/javascript/application/index.js
+++ packages/11ty/content/_assets/javascript/application/index.js
@@ -362,7 +362,7 @@ function parseQueryParams() {
 globalSetup();
 
 // Run when DOM content has loaded
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
   pageSetup()
   scrollToHash(window.location.hash, 75, 'swing')
   const params = parseQueryParams()
