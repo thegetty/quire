3b236daaba0f6a489aed7369fbae79c0af621b2c
Hannah Balenda <hbalenda@getty.edu>
2022-10-05 16:49:34 -0700

    Set annotation targets on manifest json directly

--- packages/11ty/_plugins/figures/iiif/manifest/index.js
+++ packages/11ty/_plugins/figures/iiif/manifest/index.js
@@ -145,6 +145,15 @@ module.exports = class Manifest {
     })
     try {
       this.json = builder.toPresentation3(manifest)
+      /**
+       * Note: Builder can't currently create annotations with target regions,
+       * so set the target on each annotation directly
+       */
+      const manifestAnnotations = this.json.items[0]?.items[0]?.items || []
+      manifestAnnotations.forEach((item) => {
+        const annotation = this.annotations.find((a) => a.id === item.id)
+        if (annotation) item.target = annotation.target
+      })
       info(`Generated manifest for figure "${this.figure.id}"`)
       return { success: true }
     } catch(errorMessage) {
