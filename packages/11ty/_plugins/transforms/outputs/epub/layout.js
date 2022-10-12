8d3be6f78c85cca776391ffc40af26d0be7a447a
Hannah Balenda <hbalenda@getty.edu>
2022-10-12 08:20:26 -0700

    Use common-tags html method to return layout

--- packages/11ty/_plugins/transforms/outputs/epub/layout.js
+++ packages/11ty/_plugins/transforms/outputs/epub/layout.js
@@ -1,10 +1,11 @@
+const { html } = require('~lib/common-tags')
 module.exports = ({ body, language, title }) => {
   /**
    * @todo include styles, i.e.:
    * <link rel="stylesheet" type="text/css" href="epub.css" />
    */
   const stylesheets = ''
-  return `
+  return html`
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE html>
     <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" xml:lang="${language}">
