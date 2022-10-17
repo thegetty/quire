56e73fdccfb966148bce60cf9b99d491d8ba8029
mph <mhrudka@getty.edu>
2022-10-16 20:54:29 -0700

    Resolve paths for lib/11ty to configure Eleventy

--- packages/cli/src/lib/11ty/eleventy.js
+++ packages/cli/src/lib/11ty/eleventy.js
@@ -1,17 +1,32 @@
+import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 import Eleventy from '@11ty/eleventy'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 /**
  * A wrapper module for Eleventy
  * @see https://www.11ty.dev/docs/programmatic/
+ * @todo read paths from the project configuration
  */
 export default {
-  build: async () => {
+  build: async (options = {}) => {
+    /**
+     * Nota bene: `basePath` is relative to `main.js`, the CLI entry point.
+     */
+    const basePath = '../../packages/11ty'
+
     const paths = {
-      config: path.join('../../../../packages/11ty/.eleventy.js'),
-      input: path.join('../../../../packages/11ty/content/'),
-      output: '_site'
+      config: path.resolve(basePath, '.eleventy.js'),
+      input: path.resolve(basePath, 'content'),
+      output: path.resolve(basePath, '_site')
     }
+
+    // if (options.debug) {
+      // console.log(`basePath: ${basePath}`)
+      console.log(`paths: `, paths)
+    // }
 
     const eleventy = new Eleventy(paths.input, paths.output, {
       config: (eleventyConfig) => {
