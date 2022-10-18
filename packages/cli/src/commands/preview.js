b8582fa419251c4d8ca9db3b4238c74c4d6c60c3
mph <mhrudka@getty.edu>
2022-10-17 17:19:59 -0700

    Add quire-cli preview command to run dev server
    Update cli 11ty dependency to 2.0.0-canary.16

---
+++ packages/cli/src/commands/preview.js
@@ -0,0 +1,38 @@
+import Command from '#src/Command.js'
+import eleventy from '#lib/11ty/eleventy.js'
+
+/**
+ * Quire CLI `preview` Command
+ *
+ * @class      PreviewCommand
+ * @extends    {Command}
+ */
+export default class PreviewCommand extends Command {
+  static definition = {
+    name: 'preview',
+    description: 'Run the development server and watch on file changes',
+    summary: 'run the development server',
+    version: '1.0.0',
+    args: [
+      // [
+      //   '[formats]', 'output formats',
+      //   {
+      //     choices: ['html', 'pdf', 'epub'],
+      //     default: ['html', 'html only']
+      //   }
+      // ],
+    ],
+    options: [
+      [ '--port', 'serve files on localhost port', 8080 ],
+    ],
+  }
+
+  constructor() {
+    super(PreviewCommand.definition)
+  }
+
+  action(options = {}) {
+    console.error('Command \'%s\' called with options %o', this.name, options)
+    eleventy.serve(options)
+  }
+}
--- packages/cli/src/lib/11ty/eleventy.js
+++ packages/cli/src/lib/11ty/eleventy.js
@@ -5,6 +5,30 @@ import Eleventy from '@11ty/eleventy'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+const factory = (options) => {
+  /**
+   * Get paths for configuring Eleventy
+   * Nota bene: `basePath` is relative to `main.js`, the CLI entry point.
+   */
+  const basePath = '../../packages/11ty'
+
+  const paths = {
+    config: path.resolve(basePath, '.eleventy.js'),
+    input: path.resolve(basePath, 'content'),
+    output: path.resolve(basePath, '_site')
+  }
+
+  const { config, input, output } = paths
+
+  return new Eleventy(input, output, {
+    config: (eleventyConfig) => {
+      // custom configuration
+    },
+    configPath: options.config || config,
+    quiteMode: options.quite || false,
+  })
+}
+
 /**
  * A wrapper module for Eleventy
  * @see https://www.11ty.dev/docs/programmatic/
@@ -12,32 +36,17 @@ const __dirname = path.dirname(__filename)
  */
 export default {
   build: async (options = {}) => {
-    /**
-     * Nota bene: `basePath` is relative to `main.js`, the CLI entry point.
-     */
-    const basePath = '../../packages/11ty'
+    const eleventy = factory(options)
 
-    const paths = {
-      config: path.resolve(basePath, '.eleventy.js'),
-      input: path.resolve(basePath, 'content'),
-      output: path.resolve(basePath, '_site')
-    }
+    console.log('[CLI] running eleventy build')
 
-    // if (options.debug) {
-      // console.log(`basePath: ${basePath}`)
-      console.log(`paths: `, paths)
-    // }
+    await eleventy.executeBuild()
+  },
+  serve: async (options = {}) => {
+    const eleventy = factory(options)
 
-    const eleventy = new Eleventy(paths.input, paths.output, {
-      config: (eleventyConfig) => {
-        // custom configuration
-      },
-      configPath: paths.config,
-      quiteMode: false,
-    })
+    console.log('[CLI] running development server')
 
-    console.log(`running eleventy...`)
-
-    await eleventy.write()
+    await eleventy.serve()
   }
 }
