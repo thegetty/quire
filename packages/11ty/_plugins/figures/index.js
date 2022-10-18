ac34bbae6a6decae3c4048d237b11749e409eade
mph <mhrudka@getty.edu>
2022-10-18 13:19:44 -0700

    Update figures plugin module
    Condense passing iiif config as an arg to create the FigureFactory.
    
    Mutate global figures data by reference with result from calling the
    figure adapter method.

--- packages/11ty/_plugins/figures/index.js
+++ packages/11ty/_plugins/figures/index.js
@@ -1,6 +1,7 @@
 const chalkFactory = require('~lib/chalk')
 const FigureFactory = require('./figure/factory')
 const iiifConfig = require('./iiif/config')
+
 const { error, info } = chalkFactory('Figure Processing')
 
 /**
@@ -9,27 +10,31 @@ const { error, info } = chalkFactory('Figure Processing')
  * for all figures in `figures.yaml` and updates global data
  */
 module.exports = function (eleventyConfig, options = {}) {
-  const config = iiifConfig(eleventyConfig)
-  const figureFactory = new FigureFactory(config)
+  const figureFactory = new FigureFactory(iiifConfig(eleventyConfig))
 
   eleventyConfig.on('eleventy.before', async () => {
     let { figure_list: figureList } = eleventyConfig.globalData.figures
+
     figureList = await Promise.all(figureList.map((data) => {
       return figureFactory.create(data)
     }))
+
     const errors = figureList.filter(({ errors }) => !!errors.length)
 
     if (errors.length) {
-      error(`There were errors processing the following images:`)
+      error('There were errors processing the following images:')
       console.table(
         errors.map(({ errors, figure }) => {
-          return { id: figure.id, errors: errors.join(' ')}
+          return { id: figure.id, errors: errors.join(' ') }
         }),
         ['id', 'errors']
       )
     }
-    const figures = figureList.map(({ figure }) => figure.adapter())
-    Object.assign(eleventyConfig.globalData.figures.figure_list, figures)
+
+    /**
+     * Update global figures data to only have properties for Quire shortcodes
+     */
+    Object.assign(figureList, figureList.map(({ figure }) => figure.adapter()))
 
     info(`Done`)
   })
