eff546a80e1aae0411e8558505de5b5c44ec4a6c
mph <mhrudka@getty.edu>
2022-10-18 12:39:18 -0700

    Edit JSDoc for figure helper modules

--- packages/11ty/_plugins/figures/helpers/is-canvas.js
+++ packages/11ty/_plugins/figures/helpers/is-canvas.js
@@ -1,3 +1,10 @@
-module.exports = ({ annotations, id, canvasId, manifestId, iiifContent }) => {
+/**
+ * Test if a figure uses a canvas-panel
+ *
+ * @param  {Object} figure
+ * @return {Boolean}
+ */
+module.exports = (figure) => {
+  const { annotations, canvasId, id, iiifContent, manifestId } = figure
   return (!!canvasId && !!manifestId) || !!iiifContent || !!annotations
 }
