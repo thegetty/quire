87d9e0db0afda3c98b41c1c063802e9eda23070d
Anders Pollack <pollack.anders@gmail.com>
2022-10-27 16:49:18 -0700

    Implement basic `init-starter` module to copy starter and 11ty files

--- packages/cli/src/lib/quire/init-starter.js
+++ packages/cli/src/lib/quire/init-starter.js
@@ -1,3 +1,6 @@
+import fs from 'fs-extra'
+import path from 'path'
+import { cwd } from 'node:process'
 /**
  * Clone or copy a Quire starter project
  *
@@ -6,5 +9,21 @@
  * @return   {Promise}
  */
 export async function initStarter (starter, rootPath) {
+  const fullStarterPath = path.resolve(starter)
+  const fullRootPath = path.resolve(rootPath)
+  const starterFiles = fs.readdirSync(fullStarterPath)
+  // copies all files in `starters/default`, including `.gitignore`, `CHANGELOG,md`, `README.md`, `content`
+  starterFiles.forEach((filePath) => {
+    const fileToCopy = path.resolve(fullStarterPath, filePath)
+    fs.copySync(fileToCopy, path.join(fullRootPath, path.basename(filePath)))
+  })
 
+  const eleventyPath = path.resolve(cwd(), '../11ty')
+  const eleventyFiles = fs.readdirSync(eleventyPath)
+  // copies all files in `quire/packages/11ty`
+  eleventyFiles.forEach((filePath) => {
+    const fileToCopy = path.resolve(eleventyPath, filePath)
+    fs.copySync(fileToCopy, path.join(fullRootPath, path.basename(filePath)))
+  })
+  // @TODO don't copy node modules. skip them, then use `execa` to install
 }
