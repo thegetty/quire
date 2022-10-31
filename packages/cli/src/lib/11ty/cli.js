842abaef13459c6e389735aa07e8ff258620159f
Hannah Balenda <hbalenda@getty.edu>
2022-10-31 09:57:32 -0700

    Add preview command

--- packages/cli/src/lib/11ty/cli.js
+++ packages/cli/src/lib/11ty/cli.js
@@ -7,14 +7,13 @@ import paths from './paths.js'
  * A wrapper module for the Eleventy CLI
  * @see https://www.11ty.dev/docs/usage/#command-line-usage
  */
+const { config, input, output } = paths
+const projectRoot = path.resolve('../../packages/11ty')
+
 export default {
   build: async (options = {}) => {
-    const projectRoot = path.resolve('../../packages/11ty')
-
     console.info('[CLI:11ty] running eleventy build')
     console.info(`[CLI:11ty] projectRoot ${projectRoot}`)
-
-    const { config, input, output } = paths
 
     console.debug('[CLI:11ty] %o', paths)
 
@@ -28,6 +27,21 @@ export default {
 
     if (options.dryRun) eleventyOptions.push('--dryrun')
     if (options.quiet) eleventyOptions.push('--quiet')
+
+    await execa('npx', eleventyOptions, { cwd: projectRoot, stdout: 'pipe' })
+  },
+
+  serve: async (options = {}) => {
+    console.info('[CLI:11ty] running eleventy serve')
+
+    const eleventyOptions = [
+      `@11ty/eleventy`,
+      `--config=${config}`,
+      `--input=${input}`,
+      `--output=${output}`,
+      `--serve`,
+      '--incremental'
+    ]
 
     await execa('npx', eleventyOptions, { cwd: projectRoot, stdout: 'pipe' })
   }
