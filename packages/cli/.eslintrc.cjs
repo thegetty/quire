6f5a963e0ec618001848720d35f9f1d6239ee02e
mph <mhrudka@getty.edu>
2022-10-31 15:17:23 -0700

    Configure ESLint to allow template literals

--- packages/cli/.eslintrc.cjs
+++ packages/cli/.eslintrc.cjs
@@ -17,7 +17,7 @@ module.exports = {
   rules: {
     'indent': [ 'error', 2 ],
     'linebreak-style': [ 'error', 'unix' ],
-    'quotes': [ 'error', 'single' ],
+    'quotes': [ 'error', 'single', { 'allowTemplateLiterals': true } ],
     'no-console': 'off',
     'no-unused-vars': [
       'warn',
