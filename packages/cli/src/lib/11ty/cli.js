818714bdc3abafab38c32bfab923f3992c01177c
Hannah Balenda <hbalenda@getty.edu>
2022-10-31 10:56:49 -0700

    Extract factory function to configure eleventy command

--- packages/cli/src/lib/11ty/cli.js
+++ packages/cli/src/lib/11ty/cli.js
@@ -1,29 +1,40 @@
-import execa from 'execa'
+import { execa } from 'execa'
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 import paths from './paths.js'
+const projectRoot = path.resolve('../../packages/11ty')
+
+/**
+ * A factory function to configure an Eleventy CLI command
+ *
+ * @param  {Object}  options  Eleventy configuration options
+ * @return  {Array} Eleventy CLI options
+ */
+const factory = () => {
+  const { config, input, output } = paths
+
+  console.info(`[CLI:11ty] projectRoot ${projectRoot}`)
+
+  console.debug('[CLI:11ty] %o', paths)
+
+  return [
+    `@11ty/eleventy`,
+    `--config=${config}`,
+    `--input=${input}`,
+    `--output=${output}`,
+    '--incremental',
+  ]
+}
 
 /**
  * A wrapper module for the Eleventy CLI
  * @see https://www.11ty.dev/docs/usage/#command-line-usage
  */
-const { config, input, output } = paths
-const projectRoot = path.resolve('../../packages/11ty')
-
 export default {
   build: async (options = {}) => {
     console.info('[CLI:11ty] running eleventy build')
-    console.info(`[CLI:11ty] projectRoot ${projectRoot}`)
 
-    console.debug('[CLI:11ty] %o', paths)
-
-    const eleventyOptions = [
-      `@11ty/eleventy`,
-      `--config=${config}`,
-      `--input=${input}`,
-      `--output=${output}`,
-      '--incremental',
-    ]
+    const eleventyOptions = factory()
 
     if (options.dryRun) eleventyOptions.push('--dryrun')
     if (options.quiet) eleventyOptions.push('--quiet')
@@ -32,16 +43,15 @@ export default {
   },
 
   serve: async (options = {}) => {
-    console.info('[CLI:11ty] running eleventy serve')
+    console.info(`[CLI:11ty] running eleventy serve`)
 
-    const eleventyOptions = [
-      `@11ty/eleventy`,
-      `--config=${config}`,
-      `--input=${input}`,
-      `--output=${output}`,
-      `--serve`,
-      '--incremental'
-    ]
+    const eleventyOptions = factory()
+
+    eleventyOptions.push('--serve')
+
+    if (options.port) eleventyOptions.push(`--port=${options.port}`)
+    if (options.quiet) eleventyOptions.push('--quiet')
+    if (options.verbose) eleventyOptions.push('--verbose')
 
     await execa('npx', eleventyOptions, { cwd: projectRoot, stdout: 'pipe' })
   }
