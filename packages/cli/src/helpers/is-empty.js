66e9ba69cc20337daa12c1b0c6c2459d22a765f2
mph <mhrudka@getty.edu>
2022-10-31 12:18:00 -0700

    Add JSDoc to helpers/is-empty

--- packages/cli/src/helpers/is-empty.js
+++ packages/cli/src/helpers/is-empty.js
@@ -1,24 +1,27 @@
 import fs from 'node:fs'
+
+/**
+ * Ignored OS files when determing if a directory _is emtpy_
+ */
 const ignoreFiles = [
+  // MacOS
   '.DS_Store',
+  // Windows OS
+  'desktop.ini',
   'thumbs.db',
-  'desktop.ini'
 ]
 
 /**
  * Test is if a given `path` points to an existing empty directory.
- *
- * @todo refactor to ignore macOS `.DS_Store` files.
  *
  * @param  {String}  dirpath
  * @return  {Boolean}
  */
 export function isEmpty (dirpath) {
   try {
-    const files = fs.readdirSync(dirpath);
-    return !files.filter((file) => {
-      return !ignoreFiles.includes(file)
-    }).length
+    const files = fs.readdirSync(dirpath)
+      .filter((file) => !ignoreFiles.includes(file))
+    return !files.length
   } catch (error) {
     console.debug(error)
     return false
