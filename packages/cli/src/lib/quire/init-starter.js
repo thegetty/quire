6668733ad9a9c6cfc47b66b788a2b06e41784274
Anders Pollack <pollack.anders@gmail.com>
2022-11-01 19:09:33 -0700

    Add back 11ty file-copying logic

--- packages/cli/src/lib/quire/init-starter.js
+++ packages/cli/src/lib/quire/init-starter.js
@@ -1,5 +1,6 @@
 import fs from 'fs-extra'
 import path from 'node:path'
+import { cwd } from 'node:process'
 import git from '#src/lib/git/index.js'
 /**
  * Clone or copy a Quire starter project
@@ -11,10 +12,13 @@ import git from '#src/lib/git/index.js'
 export async function initStarter (starter, rootPath) {
   const remote = 'https://github.com/thegetty/quire-starter-default'
 
+  // Clone starter project repository
   await git.cwd(rootPath)
   await git
     .clone(remote, '.')
     .catch((error) => console.error('[CLI:error] ', error));
+
+  // Reinitialize project as a new git repository
   await fs.remove(path.join(rootPath, '.git'))
 
   const starterFiles = fs.readdirSync(rootPath)
@@ -22,5 +26,41 @@ export async function initStarter (starter, rootPath) {
   await git.init()
   await git.add(starterFiles)
   await git.commit('Initial Commit')
-  await git.log()
+
+  // Copy 11ty files
+  const fullRootPath = path.resolve(rootPath)
+  const eleventyPath = path.resolve(cwd(), '../11ty')
+  const eleventyFiles = fs
+    .readdirSync(eleventyPath)
+    .filter((filePath) => {
+      const ignoreFiles = ['content', 'node_modules', '_site']
+      return !ignoreFiles.includes(filePath)
+    })
+
+  /**
+   * Reads .gitignore and .eleventyignore to generate a list of file names to ignore
+   * @TODO handle `*` splat entries e.g. `**\/*\/.DS_Store'`
+   */
+  const getIgnoredFiles = () => {
+    return ['.gitignore', '.eleventyignore']
+      .flatMap((ignoreFile) => {
+        return fs
+          .readFileSync(path.join(eleventyPath, ignoreFile), 'UTF-8')
+          .split(/\r?\n/)
+          .filter((line) => !line.startsWith('#') && line.length > 0)
+      })
+  }
+
+  const eleventyFilesToIgnore = [
+    'content',
+    ...getIgnoredFiles()
+  ]
+
+  // copies all files in `quire/packages/11ty`, except for ignored files
+  eleventyFiles.forEach((filePath) => {
+    const fileToCopy = path.resolve(eleventyPath, filePath)
+    if (!eleventyFilesToIgnore.includes(filePath)) {
+      fs.copySync(fileToCopy, path.join(fullRootPath, path.basename(filePath)))
+    }
+  })
 }
