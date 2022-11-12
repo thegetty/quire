aec8ab6693a47f281332b7a8a43a84e19ba07f6e
mph <mhrudka@getty.edu>
2022-11-11 22:01:08 -0800

    Test for pagedjs and prince commands before use

--- packages/cli/src/lib/pdf/index.js
+++ packages/cli/src/lib/pdf/index.js
@@ -1,4 +1,18 @@
 import { execa } from 'execa'
+import which from 'which'
+
+/**
+ * Test if a shell command exists in the current shell PATH
+ *
+ * @param  {String}  command  The command name to test
+ */
+const commandExists = (command) => {
+  if (!which.sync(command, { nothrow: true })) {
+    console.error(`Unable to locate command '${command}'\n
+      Please make sure '${command}' is installed and is available in the shell PATH environment variable.
+    `)
+  }
+}
 
 /**
  * A wrapper module for PDF generation tools
@@ -8,12 +22,14 @@ export default {
   build(input, output, lib='pagedjs', options={}) {
     switch (lib) {
       case 'paged' || 'pagedjs' :
+        if (!commandExists('pagedjs-cli')) return
         const opts = [
           `--outline-tags 'h1'`,
           `--output ${output}`,
         ]
         const { stderror, stdout } = execa('pagedjs-cli', [...opts, input])
       case 'prince' :
+        if (!commandExists('prince')) return
         const opts = [
           `--outline-tags 'h1'`,
           `--output ${output}`,
