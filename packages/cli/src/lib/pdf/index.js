c470fb6b9c60a44636fb11e4ad9884a18fad8757
mph <mhrudka@getty.edu>
2022-11-16 14:31:18 -0800

    Call Paged.js programmatically
    Work-in-progress to use Paged.js programmatically rather than using
    the paged.js command-line interface.

--- packages/cli/src/lib/pdf/index.js
+++ packages/cli/src/lib/pdf/index.js
@@ -22,13 +22,30 @@ export default {
   build(input, output, lib='pagedjs', options={}) {
     switch (lib) {
       case 'paged' || 'pagedjs' :
-        if (!commandExists('pagedjs-cli')) return
-        const opts = [
-          `--outline-tags 'h1'`,
-          `--output ${output}`,
-        ]
+        // if (!commandExists('pagedjs-cli')) return
+        // const opts = [
+        //   `--outline-tags 'h1'`,
+        //   `--output ${output}`,
+        // ]
+        // const { stderror, stdout } = execa('pagedjs-cli', [...opts, input])
+
         console.info(`[CLI:lib/pdf] generating PDF using ${lib}`)
-        const { stderror, stdout } = execa('pagedjs-cli', [...opts, input])
+
+        const Printer = await import(pagedjs-cli)
+        /**
+         * Configure the Paged.js Printer options
+         * @see https://gitlab.coko.foundation/pagedjs/pagedjs-cli/-/blob/main/src/cli.js
+         */
+        const printerOptions = { debug: options.debug || false }
+        const printer = new Printer(printerOptions)
+        /**
+         * Configure the Paged.js PDF options
+         * @see
+         */
+        const pdfOptions = {}
+        const pdfFile = await printer.pdf(input, pdfOptions)
+        break
+
       case 'prince' :
         if (!commandExists('prince')) return
         const opts = [
@@ -37,8 +54,11 @@ export default {
         ]
         console.info(`[CLI:lib/pdf] generating PDF using ${lib}`)
         const { stderror, stdout } = execa('prince', [...opts, input])
+        break
+
       default:
         console.error('[CLI:lib/pdf] No PDF library specified')
+        break
     }
   }
 }
