4ef36c3a429e3f6d969ebc8dcf903223f6b8c63b
mph <mhrudka@getty.edu>
2022-11-20 17:16:18 -0800

    Output message not yet implemented for epub.js

--- packages/cli/src/lib/epub/epub.js
+++ packages/cli/src/lib/epub/epub.js
@@ -5,5 +5,5 @@ import epub from 'epubjs'
  * @see https://github.com/futurepress/epub.js
  */
 export default async (input, options) => {
-
+  console.info('[CLI:lib/epub] epub.js integration is not yet implemented.')
 }
