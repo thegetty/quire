2807cb7720df6728683622aefdc0c96323f60b1e
mph <mhrudka@getty.edu>
2022-11-21 12:41:34 -0800

    Fix pagination button text

--- packages/11ty/_includes/components/page-buttons.js
+++ packages/11ty/_includes/components/page-buttons.js
@@ -14,7 +14,7 @@ module.exports = function(eleventyConfig) {
   const icon = eleventyConfig.getFilter('icon')
   const {
     nextButtonText,
-    previousButtonText
+    prevButtonText
   } = eleventyConfig.globalData.config.navigation
 
   return function(params, options={}) {
@@ -25,7 +25,7 @@ module.exports = function(eleventyConfig) {
       if (!previousPage) return
       return html`
         <li class="quire-nav-button prev">
-          <a href="${previousPage.url}">${icon({ type: 'left-arrow', description: 'Go back a page'})}&nbsp;<span class="nav-title">${nextButtonText}</span></a>
+          <a href="${previousPage.url}">${icon({ type: 'left-arrow', description: 'Go back a page'})}&nbsp;<span class="nav-title">${prevButtonText}</span></a>
           <span class="visually-hidden">Previous Page (left keyboard arrow or swipe)</span>
         </li>
       `
@@ -35,7 +35,7 @@ module.exports = function(eleventyConfig) {
       if (!nextPage) return
       return html`
         <li class="quire-nav-button next">
-          <a href="${nextPage.url}"><span class="nav-title">${previousButtonText}</span>&nbsp;${icon({ type: 'right-arrow', description: 'Go back next page' })}</a>
+          <a href="${nextPage.url}"><span class="nav-title">${nextButtonText}</span>&nbsp;${icon({ type: 'right-arrow', description: 'Go back next page' })}</a>
             <span class="visually-hidden">Next Page (right keyboard arrow or swipe)</span>
         </li>
       `
