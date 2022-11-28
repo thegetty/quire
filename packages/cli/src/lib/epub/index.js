f84db52fa9a9128ae5055a09764d2818baac9dcc
mph <mhrudka@getty.edu>
2022-11-28 06:31:01 -0800

    Rewrite façade delegation module for epub and pdf
    Refactors quire-cli lib/epub and lib/pdf modules to export a method
    to configure and return the specified lib module.

--- packages/cli/src/lib/epub/index.js
+++ packages/cli/src/lib/epub/index.js
@@ -1,21 +1,30 @@
+import { dynamicImport } from '#helpers/os-utils.js'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 /**
  * A façade delegation module for EPUB libraries
  */
-export default {
-  build: async (input, output, lib='epubjs', options={}) => {
-    let epublib
+export default async (lib = 'epubjs', options = {}) => {
+  let libPath
 
-    switch (lib.toLowerCase()) {
-      case 'epubjs': {
-        epublib = await import('./epubjs')
-        break
-      }
-      default:
-        console.error('[CLI:lib/epub] Unrecognized EPUB library ${lib}')
-        return
+  switch (lib.toLowerCase()) {
+    case 'epubjs': {
+      libPath = path.join(__dirname, 'epub.js')
+      break
     }
+    default:
+      console.error('[CLI:lib/epub] Unrecognized EPUB library ${lib}')
+      return
+  }
 
+  const { default: epubLib } = await dynamicImport(libPath)
+
+  return async (input, output, options = {}) => {
     console.info(`[CLI:lib/epub] generating EPUB using ${lib}`)
-    return await epublib(input, options)
+    return await epubLib(input, options)
   }
 }
--- packages/cli/src/lib/pdf/index.js
+++ packages/cli/src/lib/pdf/index.js
@@ -1,27 +1,36 @@
+import { dynamicImport } from '#helpers/os-utils.js'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 /**
  * A façade delegation module for PDF generation libraries
  */
-export default {
-  build: async (input, output, lib='pagedjs', options={}) => {
-    let pdflib
+export default async (lib = 'pagedjs', options = {}) => {
+  let libPath
 
-    switch (lib.toLowerCase()) {
-      case 'paged':
-      case 'pagedjs': {
-        pdflib = await import('./paged.js')
-        break
-      }
-      case 'prince':
-      case 'princexml': {
-        pdflib = await import('./prince.js')
-        break
-      }
-      default:
-        console.error('[CLI:lib/pdf] Unrecognized PDF library ${lib}')
-        return
+  switch (lib.toLowerCase()) {
+    case 'paged':
+    case 'pagedjs': {
+      libPath = path.join(__dirname, 'paged.js')
+      break
     }
+    case 'prince':
+    case 'princexml': {
+      libPath = path.join(__dirname, 'prince.js')
+      break
+    }
+    default:
+      console.error('[CLI:lib/pdf] Unrecognized PDF library ${lib}')
+      return
+  }
 
+  const { default: pdfLib } = await dynamicImport(libPath)
+
+  return async (input, output, options = {}) => {
     console.info(`[CLI:lib/pdf] generating PDF using ${lib}`)
-    return await pdflib(input, options)
+    return await pdfLib(input, options)
   }
 }
