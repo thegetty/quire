e190aba9bdb5044ad81b0d9ce687ab7bbe37fedd
mph <mhrudka@getty.edu>
2022-11-30 16:15:25 -0800

    Update message when command not run in project dir

--- packages/cli/src/helpers/test-cwd.js
+++ packages/cli/src/helpers/test-cwd.js
@@ -6,7 +6,7 @@ import isQuire from '#helpers/is-quire.js'
  * @param  {Command}  command  the command from which testcwd was called
  */
 export default (command) => {
-  const message = `[CLI] ${ command ? command.name() : '' } command must be run from a Quire project directory`
+  const message = `[CLI] ${ command ? command.name() : '' } command must be run while in a Quire project directory. Use 'cd' to navigate to your project directory and re-run the 'quire ${ command ? command.name() : '' }' command.`
 
   if (!isQuire(process.cwd())) {
     console.error(message)
