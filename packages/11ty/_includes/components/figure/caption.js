f274212e71fdba16338289710ec3b206a17a32a1
Anders Pollack <pollack.anders@gmail.com>
2023-01-11 18:00:16 -0800

    Add markup for displaying a media embed URL in the figure caption

--- packages/11ty/_includes/components/figure/caption.js
+++ packages/11ty/_includes/components/figure/caption.js
@@ -12,9 +12,19 @@ const { oneLine } = require('~lib/common-tags')
 module.exports = function(eleventyConfig) {
   const markdownify = eleventyConfig.getFilter('markdownify')
 
-  return function({ caption, credit, content='' }) {
+  return function({ caption, credit, content='', mediaId, mediaType }) {
+    const getEmbeddedMediaSrc = (({ mediaId, mediaType }) => {
+      // @TODO implement utility for generating correct media embed URLs
+      // for a given hosting service: youtube, soundcloud, vimeo
+      return null;
+    })
+    const embeddedMediaSrc = getEmbeddedMediaSrc({ mediaId, mediaType })
+    const embedLink = embeddedMediaSrc
+      ? `<span class="q-figure__caption-embed-link"><a href="${embeddedMediaSrc}"><em>${embeddedMediaSrc}</em></a></span>`
+      : ''
     return oneLine`
       <figcaption class="q-figure__caption">
+        ${embedLink}
         ${markdownify(content)}
         <span class="q-figure__caption-content">${markdownify(caption || '')}</span>
         <span class="q-figure__credit">${markdownify(credit || '')}</span>
