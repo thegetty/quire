5b4a47ede66e2815bd101b0ab1511668eaf1a0ac
mph <mhrudka@getty.edu>
2023-01-18 17:36:19 -0800

    Update cli/lib/conf migrations

--- packages/cli/src/lib/config/migrations.js
+++ packages/cli/src/lib/config/migrations.js
@@ -3,10 +3,6 @@
  * @see https://github.com/sindresorhus/conf#migrations
  */
 const migrations = {
-  '1.0.0-rc.1': (store) => {
-    store.set('quire11tyPath', './11ty')
-    store.set('versionFile', '.quire-version')
-  },
   '1.0.0': (store) => {
     store.set('updateChannels', ['latest'])
   }
