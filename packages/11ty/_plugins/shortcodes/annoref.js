05afc68c0d6ee7aecfc9670bab7588d3f9caa633
Hannah Balenda <hbalenda@getty.edu>
2023-02-08 15:59:56 -0800

    Add on-scroll attribute to annoref shortcode

--- packages/11ty/_plugins/shortcodes/annoref.js
+++ packages/11ty/_plugins/shortcodes/annoref.js
@@ -16,18 +16,26 @@ const logger = chalkFactory(`Shortcodes:Annoref`)
 module.exports = function (eleventyConfig) {
   const getFigure = eleventyConfig.getFilter('getFigure')
   const markdownify = eleventyConfig.getFilter('markdownify')
-  return ({ anno='', fig, region='', text='' }) => {
+  return ({ anno='', fig, region='', text='', onscroll }) => {
     const figure = getFigure(fig)
     if (!figure) {
       console.error(`[annoref shortcode] "fig" parameter doesn't correspond to a valid figure id in "figures.yaml". Fig: ${fig}`)
     }
     const annoIds = anno.split(',').map((id) => id.trim())
+
+    if (onscroll) {
+      console.warn(
+        `onscroll attribute applied to annoref. This feature is under development. Params:`,
+        { anno, fig, region, text, onscroll }
+      )
+    }
     return oneLine`
       <a 
         class="annoref"
         data-annotation-ids="${annoIds.join(',')}"
         data-figure-id="${fig}"
         data-region="${region}"
+        on-scroll="${onscroll}"
       >${markdownify(text)}</a>
     `
   }
