0c1542152c962384273cadaa710cb18a45ee5bc0
mph <mhrudka@getty.edu>
2023-03-01 16:18:26 -0800

    Update prerelease version
    Configure the quire-cli update notifier to use dist-tag "latest"

--- packages/cli/bin/cli.js
+++ packages/cli/bin/cli.js
@@ -20,7 +20,7 @@ const INTERVAL = Object.freeze({
  * @todo user configuration of choosen update interval
  */
 const notifier = updateNotifier({
-  distTag: 'rc',
+  distTag: 'latest',
   pkg: packageConfig,
   updateCheckInterval: INTERVAL.DAILY,
 })
