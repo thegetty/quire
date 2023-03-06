963282d7953a3c496d66e4f028caa9460b39dfa4
mph <mhrudka@getty.edu>
2023-03-06 12:55:25 -0800

    Correct epub lib switch statment condition

--- packages/cli/src/lib/epub/index.js
+++ packages/cli/src/lib/epub/index.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename)
 export default async (name = 'epubjs', options = {}) => {
   const lib = { name, options, path }
 
-  switch (lib.toLowerCase()) {
+  switch (name.toLowerCase()) {
     case 'epubjs': {
       lib.name = 'Epub.js'
       lib.options = {}
