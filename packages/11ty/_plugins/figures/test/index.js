54cebe7b5217ffe324a8ffbd72b36e7e005061c5
Anders Pollack <pollack.anders@gmail.com>
2023-03-08 17:38:15 -0800

    Add extra skipped tests for ajv validation

--- packages/11ty/_plugins/figures/test/index.js
+++ packages/11ty/_plugins/figures/test/index.js
@@ -1,10 +1,15 @@
 require('module-alias/register')
 const { mock, test } = require('node:test')
+const Ajv = require('ajv')
 const assert = require('assert/strict')
 const Figure = require('../figure')
 const figureFixtures = require('./__fixtures__/figures/index.js')
 const iiifConfig = require('./__fixtures__/iiif-config.json')
 const Manifest = require('../iiif/manifest')
+const manifestSchema = require('../iiif/manifest/schema.json')
+
+const ajv = new Ajv({allErrors: true})
+const validate = ajv.compile(manifestSchema)
 
 const createManifestFromFigureFixture = async (figureFixtureName) => {
   const {
@@ -29,15 +34,29 @@ test('figure with checkbox annotations creates a valid manifest', async () => {
   assert.deepStrictEqual(manifestFixture, manifestJson)
 })
 
+test('figure with checkbox annotations creates a valid manifest', { skip: true }, async () => {
+  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('annotationsCheckbox')
+  assert.equal(validate(manifestJson), true)
+})
+
 test('figure with radio annotations creates a valid manifest', async () => {
   const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('annotationsRadio')
   assert.deepStrictEqual(manifestFixture, manifestJson)
 })
 
-// TODO fix `calcCanvasDimensions` to work with sequences -- sequence figure fixture data does not have calculated dimensions
+test('figure with radio annotations creates a valid manifest', { skip: true }, async () => {
+  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('annotationsRadio')
+  assert.equal(validate(manifestJson), true)
+})
+
 test('sequence figure creates a valid manifest', async () => {
   const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('sequence')
   assert.deepStrictEqual(manifestFixture, manifestJson)
+})
+
+test('sequence figure creates a valid manifest', { skip: true }, async () => {
+  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('sequence')
+  assert.equal(validate(manifestJson), true)
 })
 
 test('sequence figure with annotations creates a valid manifest', async () => {
@@ -45,7 +64,17 @@ test('sequence figure with annotations creates a valid manifest', async () => {
   assert.deepStrictEqual(manifestFixture, manifestJson)
 })
 
+test('sequence figure with annotations creates a valid manifest', { skip: true }, async () => {
+  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('sequenceWithAnnotations')
+  assert.equal(validate(manifestJson), true)
+})
+
 test('zoomable figure creates a valid manifest', async () => {
   const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('zoomable')
   assert.deepStrictEqual(manifestFixture, manifestJson)
+})
+
+test('zoomable figure creates a valid manifest', { skip: true }, async () => {
+  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('zoomable')
+  assert.equal(validate(manifestJson), true)
 })
