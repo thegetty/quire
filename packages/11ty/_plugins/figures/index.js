dc6926af0156bd9ca49254dce537b15f63681fa4
Hannah Balenda <hbalenda@getty.edu>
2023-03-08 16:40:06 -0800

    Run figures plugin in 11ty before

--- packages/11ty/_plugins/figures/index.js
+++ packages/11ty/_plugins/figures/index.js
@@ -9,40 +9,42 @@ const logger = chalkFactory('Figures', 'DEBUG')
  * Uses the FigureFactory to create Figure instances
  * for all figures in `figures.yaml` and updates global data
  */
-module.exports = async function (eleventyConfig, options = {}) {
-  const config = iiifConfig(eleventyConfig)
-  const figureFactory = new FigureFactory(config)
+module.exports = function (eleventyConfig, options = {}) {
+  eleventyConfig.on('eleventy.before', async () => {
+    const config = iiifConfig(eleventyConfig)
+    const figureFactory = new FigureFactory(config)
 
-  const { figure_list: figureList } = eleventyConfig.globalData.figures
+    const { figure_list: figureList } = eleventyConfig.globalData.figures
 
-  const figures = await Promise.all(
-    figureList.map((data) => {
-      return figureFactory.create(data)
-    })
-  )
-  const errors = figureList.filter(({ errors }) => errors && !!errors.length)
-
-  if (errors.length) {
-    logger.error('There were errors processing the following images:')
-    console.table(
-      errors.map(({ errors, figure }) => {
-        return { id: figure.id, errors: errors.join(' ') }
-      }),
-      ['id', 'errors']
+    const figures = await Promise.all(
+      figureList.map((data) => {
+        return figureFactory.create(data)
+      })
     )
-  }
+    const errors = figureList.filter(({ errors }) => errors && !!errors.length)
 
-  /**
-   * Add IIIFConfig to global data
-   */
-  eleventyConfig.globalData.iiifConfig = config
+    if (errors.length) {
+      logger.error('There were errors processing the following images:')
+      console.table(
+        errors.map(({ errors, figure }) => {
+          return { id: figure.id, errors: errors.join(' ') }
+        }),
+        ['id', 'errors']
+      )
+    }
 
-  /**
-   * Update global figures data to only have properties for Quire shortcodes
-   */
-  Object.assign(
-    figureList,
-    figures.map(({ figure }) => figure.adapter()),
-  )
-  logger.info('Processing complete')
+    /**
+     * Add IIIFConfig to global data
+     */
+    eleventyConfig.globalData.iiifConfig = config
+
+    /**
+     * Update global figures data to only have properties for Quire shortcodes
+     */
+    Object.assign(
+      figureList,
+      figures.map(({ figure }) => figure.adapter()),
+    )
+    logger.info('Processing complete')
+  })
 }
