8ec2749e51a9b8f806e118701b5008d53d6185d7
Anders Pollack <pollack.anders@gmail.com>
2023-03-22 13:55:25 -0700

    Implement custom `image-sequence.webc` component

--- packages/11ty/_includes/components/figure/image/sequence.js
+++ packages/11ty/_includes/components/figure/image/sequence.js
@@ -1,25 +1,24 @@
-const { html } = require('~lib/common-tags')
 const path = require('path')
 
 module.exports = function(eleventyConfig) {
   const renderFile = eleventyConfig.getFilter('renderFile')
   const renderTemplate = eleventyConfig.getFilter('renderTemplate')
   const { assetDir } = eleventyConfig.globalData.config.figures
-  return async function(data) {
+  return async function({ id, sequences }) {
     const { WebC } = await import('@11ty/webc')
+    const page = new WebC()
     const fileName = 'image-sequence.webc'
     const filePath = path.join(eleventyConfig.dir.input, eleventyConfig.dir.includes, 'components', 'figure', 'image', fileName)
-    let page = new WebC();
+    const itemUris = sequences[0].items.map(({ uri }) => uri).join(',')
+    page.setContent(`<image-sequence sequence-id="${id}" items="${itemUris}" webc:import="${filePath}"></image-sequence>`)
+    const { html, css, js, components } = await page.compile()
+    return html
 
-    // This enables aggregation of CSS and JS
-    // As of 0.4.0+ this is disabled by default
-    page.setBundlerMode(true);
+    // using renderTemplate with markdown works
+    // return await renderTemplate('# Heading', 'md')
 
-    page.setInputPath(filePath);
-
-    const { html: markup, css, js, components } = await page.compile();
-    console.warn('GOODS', { markup, css, js, components })
-
-    return html(markup)
+    // using renderTemplate/renderFile with webc does not work
+    // return await renderTemplate('<image-sequence></image-sequence>', 'webc')
+    // return await renderFile(filePath)
   }
 }
