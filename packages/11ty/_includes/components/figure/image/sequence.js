df161d131398943f05ad04d0a623d72f9b33fbca
Anders Pollack <pollack.anders@gmail.com>
2023-03-22 16:18:07 -0700

    Update `figure/image/sequence` shortcode to use `renderWebcComponent`

--- packages/11ty/_includes/components/figure/image/sequence.js
+++ packages/11ty/_includes/components/figure/image/sequence.js
@@ -1,24 +1,18 @@
 const path = require('path')
 
 module.exports = function(eleventyConfig) {
-  const renderFile = eleventyConfig.getFilter('renderFile')
-  const renderTemplate = eleventyConfig.getFilter('renderTemplate')
+  const renderWebcComponent = eleventyConfig.getFilter('renderWebcComponent')
   const { assetDir } = eleventyConfig.globalData.config.figures
   return async function({ id, sequences }) {
-    const { WebC } = await import('@11ty/webc')
-    const page = new WebC()
-    const fileName = 'image-sequence.webc'
-    const filePath = path.join(eleventyConfig.dir.input, eleventyConfig.dir.includes, 'components', 'figure', 'image', fileName)
     const itemUris = sequences[0].items.map(({ uri }) => uri).join(',')
-    page.setContent(`<image-sequence sequence-id="${id}" items="${itemUris}" webc:import="${filePath}"></image-sequence>`)
-    const { html, css, js, components } = await page.compile()
-    return html
-
-    // using renderTemplate with markdown works
-    // return await renderTemplate('# Heading', 'md')
-
-    // using renderTemplate/renderFile with webc does not work
-    // return await renderTemplate('<image-sequence></image-sequence>', 'webc')
-    // return await renderFile(filePath)
+    const fileName = 'image-sequence.webc'
+    const includesDir = path.join(eleventyConfig.dir.input, eleventyConfig.dir.includes)
+    const componentPathname = __dirname.split(includesDir)[1]
+    const filePath = path.join(includesDir, componentPathname, fileName)
+    // const filePath = path.join('_includes', 'components', 'figure', 'image', 'image-sequence.webc')
+    return await renderWebcComponent(filePath, {
+      'sequence-id': id,
+      'items': itemUris
+    })
   }
 }
