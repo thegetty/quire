bbd7f5d0f1a6936cf8c5b5e2027585829800856f
Anders Pollack <pollack.anders@gmail.com>
2023-03-28 15:17:57 -0700

    Add `index` arg to `annoref` shortcode

--- packages/11ty/_plugins/shortcodes/annoref.js
+++ packages/11ty/_plugins/shortcodes/annoref.js
@@ -10,24 +10,25 @@ const logger = chalkFactory(`Shortcodes:Annoref`)
  * @property   {String} fig Figure ID
  * @property   {String} region Comma-separated, with format "x,y,width,height"
  * @property   {String} text Link text
- * 
+ *
  * @return     {String}  Anchor tag with link text annotation and region data attributes
  */
 module.exports = function (eleventyConfig) {
   const getFigure = eleventyConfig.getFilter('getFigure')
   const markdownify = eleventyConfig.getFilter('markdownify')
-  return ({ anno='', fig, region='', text='' }) => {
+  return ({ anno='', fig, index, region='', text='' }) => {
     const figure = getFigure(fig)
     if (!figure) {
       console.error(`[annoref shortcode] "fig" parameter doesn't correspond to a valid figure id in "figures.yaml". Fig: ${fig}`)
     }
     const annoIds = anno.split(',').map((id) => id.trim())
     return oneLine`
-      <a 
+      <a
         class="annoref"
         data-annotation-ids="${annoIds.join(',')}"
         data-figure-id="${fig}"
         data-region="${region}"
+        data-index="${index}"
       >${markdownify(text)}</a>
     `
   }
