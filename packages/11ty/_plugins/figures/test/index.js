8bb08b2416ac023505a21eb25a66587fc0b6989c
Anders Pollack <pollack.anders@gmail.com>
2023-03-30 16:22:15 -0700

    Refactor figure plugin test suite

--- packages/11ty/_plugins/figures/test/index.js
+++ packages/11ty/_plugins/figures/test/index.js
@@ -1,5 +1,5 @@
 require('module-alias/register')
-const { mock, test } = require('node:test')
+const { describe, mock, test } = require('node:test')
 const Ajv = require('ajv')
 const assert = require('assert/strict')
 const Figure = require('../figure')
@@ -7,9 +7,6 @@ const figureFixtures = require('./__fixtures__/figures/index.js')
 const iiifConfig = require('./__fixtures__/iiif-config.json')
 const Manifest = require('../iiif/manifest')
 const manifestSchema = require('../iiif/manifest/schema.json')
-
-const ajv = new Ajv({allErrors: true})
-const validate = ajv.compile(manifestSchema)
 
 const createManifestFromFigureFixture = async (figureFixtureName) => {
   const {
@@ -29,63 +26,72 @@ const createManifestFromFigureFixture = async (figureFixtureName) => {
   }
 }
 
-test('figure with checkbox annotations creates a valid manifest', async () => {
-  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('annotationsCheckbox')
-  assert.deepStrictEqual(manifestFixture, manifestJson)
+describe('validating manifests via deepStrictEqual', () => {
+  test('figure with checkbox annotations creates a valid manifest', async () => {
+    const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('annotationsCheckbox')
+    assert.deepStrictEqual(manifestFixture, manifestJson)
+  })
+
+  test('figure with radio annotations creates a valid manifest', async () => {
+    const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('annotationsRadio')
+    assert.deepStrictEqual(manifestFixture, manifestJson)
+  })
+
+  test('sequence figure creates a valid manifest', async () => {
+    const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('sequence')
+    assert.deepStrictEqual(manifestFixture, manifestJson)
+  })
+
+  test('sequence figure with annotations creates a valid manifest', async () => {
+    const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('sequenceWithAnnotations')
+    assert.deepStrictEqual(manifestFixture, manifestJson)
+  })
+
+  test('zoomable figure creates a valid manifest', async () => {
+    const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('zoomable')
+    assert.deepStrictEqual(manifestFixture, manifestJson)
+  })
+
+  /* Skipping this test, since we are currently overriding zoom on all sequence manifests */
+  test('zoomable sequence figure creates a valid manifest', { skip: true }, async () => {
+    const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('zoomableSequence')
+    assert.deepStrictEqual(manifestFixture, manifestJson)
+  })
 })
 
-test('figure with checkbox annotations creates a valid manifest', { skip: true }, async () => {
-  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('annotationsCheckbox')
-  assert.equal(validate(manifestJson), true)
-})
+/* Skipping all ajv validation tests. The IIIF 3.0 schema is not ready for prime-time */
+describe('validating manifests via ajv', { skip: true }, () => {
+  let ajv = new Ajv({ allErrors: true })
+  let validate = ajv.compile(manifestSchema)
 
-test('figure with radio annotations creates a valid manifest', async () => {
-  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('annotationsRadio')
-  assert.deepStrictEqual(manifestFixture, manifestJson)
-})
+  test('figure with checkbox annotations creates a valid manifest', { skip: true }, async () => {
+    const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('annotationsCheckbox')
+    assert.equal(validate(manifestJson), true)
+  })
 
-test('figure with radio annotations creates a valid manifest', { skip: true }, async () => {
-  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('annotationsRadio')
-  assert.equal(validate(manifestJson), true)
-})
+  test('figure with radio annotations creates a valid manifest', { skip: true }, async () => {
+    const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('annotationsRadio')
+    assert.equal(validate(manifestJson), true)
+  })
 
-test('sequence figure creates a valid manifest', async () => {
-  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('sequence')
-  assert.deepStrictEqual(manifestFixture, manifestJson)
-})
+  test('sequence figure creates a valid manifest', { skip: true }, async () => {
+    const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('sequence')
+    assert.equal(validate(manifestJson), true)
+  })
 
-test('sequence figure creates a valid manifest', { skip: true }, async () => {
-  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('sequence')
-  assert.equal(validate(manifestJson), true)
-})
 
-test('sequence figure with annotations creates a valid manifest', async () => {
-  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('sequenceWithAnnotations')
-  assert.deepStrictEqual(manifestFixture, manifestJson)
-})
+  test('sequence figure with annotations creates a valid manifest', { skip: true }, async () => {
+    const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('sequenceWithAnnotations')
+    assert.equal(validate(manifestJson), true)
+  })
 
-test('sequence figure with annotations creates a valid manifest', { skip: true }, async () => {
-  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('sequenceWithAnnotations')
-  assert.equal(validate(manifestJson), true)
-})
+  test('zoomable figure creates a valid manifest', { skip: true }, async () => {
+    const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('zoomable')
+    assert.equal(validate(manifestJson), true)
+  })
 
-test('zoomable figure creates a valid manifest', async () => {
-  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('zoomable')
-  assert.deepStrictEqual(manifestFixture, manifestJson)
-})
-
-test('zoomable figure creates a valid manifest', { skip: true }, async () => {
-  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('zoomable')
-  assert.equal(validate(manifestJson), true)
-})
-
-/* Skipping the following two tests, since we are currently overriding zoom on all sequence manifests */
-test('zoomable sequence figure creates a valid manifest', { skip: true }, async () => {
-  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('zoomableSequence')
-  assert.deepStrictEqual(manifestFixture, manifestJson)
-})
-
-test('zoomable sequence figure creates a valid manifest', { skip: true }, async () => {
-  const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('zoomableSequence')
-  assert.equal(validate(manifestJson), true)
+  test('zoomable sequence figure creates a valid manifest', { skip: true }, async () => {
+    const { manifestFixture, manifestJson } = await createManifestFromFigureFixture('zoomableSequence')
+    assert.equal(validate(manifestJson), true)
+  })
 })
