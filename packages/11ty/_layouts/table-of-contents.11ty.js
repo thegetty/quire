88337c0f9b82a23690f10f69ce998891515d7a7b
Anders Pollack <pollack.anders@gmail.com>
2023-03-31 15:52:31 -0700

    Ensure table of contents renders

--- packages/11ty/_layouts/table-of-contents.11ty.js
+++ packages/11ty/_layouts/table-of-contents.11ty.js
@@ -14,7 +14,7 @@ module.exports = class TableOfContents {
     }
   }
 
-  render(data) {
+  async render(data) {
     const {
       collections,
       content,
@@ -49,7 +49,7 @@ module.exports = class TableOfContents {
         ${contentElement}
         <div class="container ${containerClass}">
           <div class="quire-contents-list ${presentation}">
-            ${this.tableOfContents({ collections, currentPageUrl: page.url, key, presentation })}
+            ${await this.tableOfContents({ collections, currentPageUrl: page.url, key, presentation })}
             <div class="content">
               {% bibliography pageReferences %}
             </div>
