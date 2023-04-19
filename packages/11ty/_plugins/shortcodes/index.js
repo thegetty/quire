84d320069011b799ac878933f6a57c708f09104c
Hannah Balenda <hbalenda@gmail.com>
2023-04-18 18:52:45 -0700

    Include section paired shortcode

--- packages/11ty/_plugins/shortcodes/index.js
+++ packages/11ty/_plugins/shortcodes/index.js
@@ -7,6 +7,7 @@ const contributors = require('./contributors')
 const figure = require('./figure')
 const figureGroup = require('./figureGroup')
 const ref = require('./figureRef')
+const section = require('./section.js')
 const shortcodeFactory = require('../../_plugins/components/addShortcode')
 const title = require('./title')
 const tombstone = require('./tombstone')
@@ -17,6 +18,9 @@ module.exports = function(eleventyConfig, collections, options) {
   addComponentTag(eleventyConfig, 'annoref', annoref)
   eleventyConfig.addPairedShortcode('backmatter', function(content, ...args) {
     return backmatter(eleventyConfig)(content, ...args)
+  })
+  eleventyConfig.addPairedShortcode('section', function(content, ...args) {
+    return section(eleventyConfig)(content, ...args)
   })
   addShortcode('bibliography', bibliography)
   addShortcode('cite', cite)
