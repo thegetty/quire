abe33470d0adc507a2283509a8bb0ee1bd1764a6
Hannah Balenda <hbalenda@gmail.com>
2023-04-18 19:02:03 -0700

    Add accordion section component

---
+++ packages/11ty/_plugins/shortcodes/section.js
@@ -0,0 +1,32 @@
+const { oneLine } = require('~lib/common-tags')
+/**
+ * Accordion component
+ *
+ * @param      {String}  content  content to render in the accordion body
+ * @param      {String}  heading  content to render in the accordion heading
+ * @param      {String}  open  accordion initial state
+ *
+ * @return     {String}  An HTML <details> element with <summary> and <section>
+ */
+module.exports = function (eleventyConfig) {
+    const markdownify = eleventyConfig.getFilter('markdownify')
+    const slugify = eleventyConfig.getFilter('slugify')
+    const { button, controls } = eleventyConfig.globalData.config.accordion
+    const controlsClass = `accordion-section__controls--${controls}`
+    
+    return (content, heading, open=undefined) => {
+      const id = `section-${slugify(heading)}`
+      return oneLine`
+        <details class="accordion-section" id="${id}" ${open}>
+            <summary class="accordion-section__heading ${controlsClass}">
+              <a href="#${id}" class="accordion-section__heading-link">
+                <span class="accordion-section__heading-link-content">${button}</span>
+              </a>
+              ${markdownify(heading, { inline: false })}
+            </summary>
+            <section class="accordion-section__body">${markdownify(content, { inline: false })}</section>
+        </details>
+      `
+    }
+  }
+  
