957a646c19ef7da939acd32cd5ff352b745c0656
Hannah Balenda <hbalenda@gmail.com>
2023-04-26 11:08:49 -0700

    include intersection observer for scroll interactions

--- packages/11ty/content/_assets/javascript/application/canvas-panel.js
+++ packages/11ty/content/_assets/javascript/application/canvas-panel.js
@@ -1,5 +1,6 @@
 import poll from './poll'
 import scrollToHash from './scroll-to-hash'
+import { intersectionObserverFactory } from './intersection-observer-factory'
 
 /**
  * Get annotation data from annotaitons UI input element
@@ -54,11 +55,17 @@ const getTarget = (region) => {
 /**
  * Scroll to a figure, or go to figure slide in lightbox
  * Select annotations and/or region, and update the URL
- * @param  {String} figureId    The id of the figure in figures.yaml
- * @param  {Array} annotationIds  The IIIF ids of the annotations to select
- * @param  {String} region      The canvas region
+ * @param  {Array}  annotationIds The IIIF ids of the annotations to select
+ * @param  {String} figureId The id of the figure in figures.yaml
+ * @param  {String} historyBehavior replace||push Whether the window history should push to or replace the state
+ * @param  {String} region The canvas region
  */
-const goToFigureState = function ({ annotationIds=[], figureId, index, region }) {
+const goToFigureState = function ({
+  annotationIds = [],
+  figureId,
+  historyBehavior = 'push',
+  region,
+}) {
   if (!figureId) {
     console.error(`goToFigureState called without an undefined figureId`)
     return
@@ -86,7 +93,7 @@ const goToFigureState = function ({ annotationIds=[], figureId, index, region })
   /**
    * Update figure state
    */
-  update(serviceId, { annotations, index, region })
+  update(serviceId, { annotations, index, region: region || 'reset' })
 
   /**
    * Build URL
@@ -101,7 +108,12 @@ const goToFigureState = function ({ annotationIds=[], figureId, index, region })
   const paramsString = params.toString()
   const urlParts = [url.pathname]
   if (paramsString) urlParts.push(paramsString)
-  window.history.pushState({}, '', `${urlParts.join('?')}${url.hash}`)
+  const historyArgs = [{}, '', `${urlParts.join('?')}${url.hash}`]
+  if (historyBehavior === 'replace') {
+    window.history.replaceState(...historyArgs)
+  } else {
+    window.history.pushState(...historyArgs)
+  }
 }
 
 /**
@@ -160,7 +172,7 @@ const selectAnnotation = (canvasPanel, annotation) => {
     case 'choice':
       /**
        * `canvasPanel.makeChoice` is defined asynchronously on the web component,
-       * and while the event model emits a 'choice' event when a choice is selected, it is noisy,
+       * and while the event model emits a 'choice' event when a choice is selected, it is noisy, 
        * and since there is no 'done' event to indicate when this method is available,
        * we will use polling
        */
@@ -207,12 +219,29 @@ const setUpUIEventHandlers = () => {
     annoRef.addEventListener('click', ({ target }) => {
       goToFigureState({ annotationIds, figureId, index, region })
     })
+
+    const onscroll = annoRef.getAttribute('data-on-scroll')
+    if (onscroll === 'true') {
+      const callback = () =>
+        goToFigureState({
+          annotationIds,
+          figureId,
+          historyBehavior: 'replace',
+          index,
+          region,
+        })
+      intersectionObserverFactory(annoRef, callback)
+    } else {
+      annoRef.addEventListener('click', ({ target }) =>
+        goToFigureState({ annotationIds, figureId, index, region })
+      )
+    }
   }
 
   /**
    * Add click handlers to UI inputs
    */
-  const inputs = document.querySelectorAll('.annotations-ui__input')
+  const inputs = document.querySelectorAll('.annotations-ui__input')  
   for (const input of inputs) {
     handleSelect(input)
     input.addEventListener('click', ({ target }) => handleSelect(target))
@@ -221,7 +250,7 @@ const setUpUIEventHandlers = () => {
 
 /**
  * Update canvas panel or image-service properties
- *
+ * 
  * @param  {String} id Canvas ID or path to image-service info.json
  * @param  {Object} data
  * @property {String} region comma-separated, @example "x,y,width,height"
