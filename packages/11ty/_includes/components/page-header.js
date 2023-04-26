79c89bf8fe68ad19d19f69e6bb098276ee329e84
Hannah Balenda <hbalenda@gmail.com>
2023-04-26 08:36:36 -0700

    Include global accordion controls in page header

--- packages/11ty/_includes/components/page-header.js
+++ packages/11ty/_includes/components/page-header.js
@@ -8,15 +8,16 @@ const path = require('path')
  */
 module.exports = function(eleventyConfig) {
   const contributors = eleventyConfig.getFilter('contributors')
-  const markdownify = eleventyConfig.getFilter('markdownify')
   const pageTitle = eleventyConfig.getFilter('pageTitle')
   const slugify = eleventyConfig.getFilter('slugify')
 
+  const { accordion: accordionConfig } = eleventyConfig.globalData.config
   const { labelDivider } = eleventyConfig.globalData.config.pageTitle
   const { imageDir } = eleventyConfig.globalData.config.figures
 
   return function (params) {
     const {
+      accordion: accordionPageConfig={},
       byline_format: bylineFormat,
       image,
       label,
@@ -24,6 +25,11 @@ module.exports = function(eleventyConfig) {
       subtitle,
       title
     } = params
+
+    /**
+     * Merge page and global config for accordion
+     */
+    const accordionControls = Object.assign({...accordionConfig.globalControls}, {...accordionPageConfig.globalControls|| {}})
 
     const classes = ['quire-page__header', 'hero']
 
@@ -53,6 +59,14 @@ module.exports = function(eleventyConfig) {
         `
       : ''
 
+    const globalAccordionControls = accordionControls.enable
+      ? html`
+        <div class="global-accordion-controls">
+          <button class="global-accordion-expand-all">${accordionControls.expand}</button>
+          <button class="global-accordion-collapse-all">${accordionControls.collapse}</button>
+        </div>`
+      : ''
+
     return html`
       <section class="${classes}">
         <div class="hero-body">
@@ -62,6 +76,7 @@ module.exports = function(eleventyConfig) {
           </h1>
           ${contributorsElement}
         </div>
+        ${globalAccordionControls}
       </section>
       ${imageElement}
     `
