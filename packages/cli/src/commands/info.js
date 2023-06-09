889804d4403227a3819503dc82278c4c7da4f3e4
Hannah Balenda <hbalenda@gmail.com>
2023-06-09 12:23:45 -0700

    Add os, 11ty, cli, and node version numbers to output

--- packages/cli/src/commands/info.js
+++ packages/cli/src/commands/info.js
@@ -1,4 +1,7 @@
 import Command from '#src/Command.js'
+import { execaCommand } from 'execa'
+import fs from 'node:fs'
+import os from 'node:os'
 import testcwd from '#helpers/test-cwd.js'
 
 /**
@@ -28,8 +31,60 @@ export default class InfoCommand extends Command {
   action(options, command) {
     if (options.debug) {
       console.debug('[CLI] Command \'%s\' called with options %o', this.name(), options)
+    } else {
+      console.debug('[CLI] Command \'%s\' called', this.name())
     }
-    console.debug('[CLI] Command \'%s\' called', this.name())
+
+    const versions = [
+      {
+        name: 'quire-cli',
+        get: async () => {
+          const { stdout } = await execaCommand('quire --version')
+          return stdout
+        }
+      },
+      {
+        name: 'quire-11ty',
+        get: () => {
+          const { version } = JSON.parse(fs.readFileSync('./package.json'))
+          return version
+        }
+      },
+      // {
+      //   name: 'starter',
+      //   get: () => {
+      //     let name, version
+      //     return `${name}, ${version}`
+      //   }
+      // },
+      {
+        debug: true,
+        name: 'os',
+        get: () => `${os.type()} ${os.release()}`,
+      },
+      {
+        debug: true,
+        name: 'node',
+        get: () => process.version
+      },
+      {
+        debug: true,
+        name: 'npm',
+        get: async () => {
+          const { stdout } = await execaCommand('npm --version')
+          return stdout
+        }
+      }
+    ]
+    const listVersions = async () => {
+      for (let i = 0; i < versions.length; i++) {
+        const { debug, get, name } = versions[i]
+        if (!debug || options.debug && debug) {
+          console.info(`${name} ${await get()}`)
+        }
+      }
+    }
+    listVersions()
   }
 
   preAction(command) {
