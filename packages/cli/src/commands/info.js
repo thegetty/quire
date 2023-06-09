9bc4d03399fbae9da9fea13972400d107a660610
Hannah Balenda <hbalenda@gmail.com>
2023-06-09 11:32:18 -0700

    Stub info command

---
+++ packages/cli/src/commands/info.js
@@ -0,0 +1,33 @@
+import Command from '#src/Command.js'
+
+/**
+ * Quire CLI `info` Command
+ *
+ * Runs the Eleventy `info` to generate static output.
+ *
+ * @class      InfoCommand
+ * @extends    {Command}
+ */
+export default class InfoCommand extends Command {
+  static definition = {
+    name: 'info',
+    description: 'List Quire cli, quire-11ty, and node versions',
+    summary: 'list info',
+    version: '1.0.0',
+    args: [],
+    options: [
+      [ '--debug', 'include os versions in output' ],
+    ],
+  }
+
+  constructor() {
+    super(InfoCommand.definition)
+  }
+
+  action(options, command) {
+    if (options.debug) {
+      console.debug('[CLI] Command \'%s\' called with options %o', this.name(), options)
+    }
+    console.debug('[CLI] Command \'%s\' called', this.name())
+  }
+}
