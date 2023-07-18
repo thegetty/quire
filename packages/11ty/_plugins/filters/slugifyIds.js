902ffc8e2160c211a2aba5c7de1cc82990a7669c
Anders Pollack <pollack.anders@gmail.com>
2023-07-18 16:19:33 -0500

    Refactor `slugifyIds` filter to NOT instantiate a new `JSDom`

--- packages/11ty/_plugins/filters/slugifyIds.js
+++ packages/11ty/_plugins/filters/slugifyIds.js
@@ -1,23 +1,15 @@
-const jsdom = require('jsdom')
-
-const { JSDOM } = jsdom
-
 /**
  * Slugify all ids in a DOM
  *
- * @param   {Object}   element HTML element
+ * @param   {Object}   HTML Document element
  * @param   {Function} slugifyFn Function to slugify ids strings
  */
-module.exports = (content, eleventyConfig) => {
-  const dom = new JSDOM(content)
-  const { document } = dom.window
+module.exports = (documentElement, eleventyConfig) => {
   const slugify = eleventyConfig.getFilter('slugify')
 
   Array
-    .from(document.querySelectorAll('[id]'))
+    .from(documentElement.querySelectorAll('[id]'))
     .forEach((element) => {
       element.id = slugify(element.id)
     })
-
-  return dom.serialize()
 }
