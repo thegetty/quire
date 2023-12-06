582cbb84196889626cc7de17eee016795e995855
Anders Pollack <pollack.anders@gmail.com>
2023-12-05 20:04:12 -0800

    Fix audio print output

--- packages/11ty/_includes/components/figure/audio/print.js
+++ packages/11ty/_includes/components/figure/audio/print.js
@@ -2,35 +2,30 @@ const { html } = require('~lib/common-tags')
 const chalkFactory = require('~lib/chalk')
 const path = require('path')
 
-const logger = chalkFactory('Figure Video')
-
 /**
- * Renders an image fallback for a video player in print output
+ * Renders an iframe element with the SoundCloud audio player
  *
  * @param      {Object}  eleventyConfig  eleventy configuration
- * @param      {Object}  figure          The figure object
+ * @param      {Object}  figure          The figure
  *
- * @return     {String}  HTML containing a fallback image and a caption
+ * @return     {String}  An embedded SoundCloud player and a caption
  */
 module.exports = function(eleventyConfig) {
   const { imageDir } = eleventyConfig.globalData.config.figures
 
-  return function({
-    aspect_ratio: aspectRatio,
-    poster=''
-  }) {
-    if (!poster) {
-      logger.warn(`Figure '${id}' does not have a 'poster' property. Print media will not render a fallback image for id: ${id}`)
-    }
-
+  return function({ poster='' }) {
     const posterSrc = poster.startsWith('http')
       ? poster
       : path.join(imageDir, poster)
 
+    const imageElement = poster
+      ? `<div class="q-figure__media-wrapper">
+          <img src="${posterSrc}" />
+        </div>`
+      : ''
+
     return html`
-      <div class="q-figure__media-wrapper--${ aspectRatio || 'widescreen' }">
-        <img src="${posterSrc}" />
-      </div>
+      ${imageElement}
     `
   }
 }
