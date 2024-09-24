52280f2c0c0528ed47dc2781a9703b106b2983ca
charlie butcosk <cbutcosk@users.noreply.github.com>
2024-09-24 11:07:38 -0400

    Update packages/11ty/_includes/components/figure/image/sequence.js
    Co-authored-by: matthew p hrudka <mphstudios@users.noreply.github.com>

--- packages/11ty/_includes/components/figure/image/sequence.js
+++ packages/11ty/_includes/components/figure/image/sequence.js
@@ -8,14 +8,16 @@ module.exports = function(eleventyConfig) {
     const itemUris = sequences[0].items.map(({ uri }) => uri).join(',')
     const reverse = sequences[0].viewingDirection === 'right-to-left' 
 
-    // TODO: Why is `html` not serializing boolean values to their string equivs?
-    return html `<q-image-sequence 
-                    continuous="${continuous ? 'true' : 'false'}"
-                    index="${startCanvasIndex}"
-                    items="${itemUris}"
-                    interactive="${ interactive ? 'true' : 'false' }"
-                    sequence-id="${id}"
-                    reverse="${reverse ? 'true' : 'false' }"                    
-                 ></q-image-sequence>`    
+    // @todo Why does `html` not serialize boolean values to their string equivalent?
+    return html `
+      <q-image-sequence 
+        continuous="${continuous ? 'true' : 'false'}"
+         index="${startCanvasIndex}"
+         interactive="${interactive ? 'true' : 'false'}"
+         items="${itemUris}"
+         reverse="${reverse ? 'true' : 'false'}"
+         sequence-id="${id}"
+       ></q-image-sequence>
+     `   
   }
 }
