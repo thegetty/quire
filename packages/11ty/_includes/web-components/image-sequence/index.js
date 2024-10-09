7c0bbc784687461ffac5864266634fd101569950
Charlie Butcosk <incharge@weichbild.com>
2024-10-09 11:57:41 -0400

    Refine slot and canvas usage, initial load-at-visibility handling

--- packages/11ty/_includes/web-components/image-sequence/index.js
+++ packages/11ty/_includes/web-components/image-sequence/index.js
@@ -14,9 +14,12 @@ class ImageSequence extends LitElement {
         max-width: 100vw;
       }
 
-      .image-sequence.interactive {
+      .image-sequence {
         height: 100%;
         width: 100%;
+      }
+
+      .image-sequence.interactive {
         cursor: grab;
       }
 
@@ -28,6 +31,7 @@ class ImageSequence extends LitElement {
         top: 0;
         left: 0;
         height: 100%;
+        width: 100%;
         background: rgba(0,0,0,0);
         color: white;
         transition: all 0.25s linear;
@@ -76,30 +80,23 @@ class ImageSequence extends LitElement {
         opacity: 1;
       }
 
-      slot[name='images'] canvas {
-        display: block;
+      canvas {
+        display: none;
         pointer-events: none;
         user-select: none;
+        object-fit: contain;
         width: 100%;
         height: 100%;
-      }
-
-      slot[name='images'] canvas:not(.placeholder) {
         position: absolute;
         top: 0;
         left: 0;
       }
 
-      slot[name='images'] canvas.visible {
-        opacity: 1;
-        object-fit: contain;
+      canvas.visible {
+        display: block;
       }
 
-      slot[name='images'] canvas:not(.visible) {
-        opacity: 0;
-      }
-
-      slot[name='images'] canvas.fade-in {
+      canvas.fade-in {
         animation: fade-in 0.25s 1 linear;          
       }
 
@@ -110,6 +107,15 @@ class ImageSequence extends LitElement {
         to {
           opacity: 1;
         }
+      }
+
+      slot[name='placeholder-image'] img {
+        display: block;
+        pointer-events: none;
+        opacity: 0;
+        height: 100%;
+        transition: opacity 0.25s linear;
+        object-fit: cover;
       }
 
       slot[name='placeholder-image'] img.loading {
@@ -125,16 +131,6 @@ class ImageSequence extends LitElement {
         to {
           filter: brightness(0.2);
         }
-      }
-
-      slot[name='placeholder-image'] img {
-        height: 100%;
-        transition: opacity 0.25s linear;
-        object-fit: cover;
-      }
-
-      slot[name='placeholder-image'] img.interacted {
-        opacity: 0;
       }
       `
 
@@ -172,6 +168,10 @@ class ImageSequence extends LitElement {
     },
     transition: {
       type: Number,
+    },
+    visible: {
+      type: Boolean,
+      state: true,
     }
   }
 
@@ -188,24 +188,24 @@ class ImageSequence extends LitElement {
    * 
    * Fetches `url`, converts it into a blob and stores the image data
    **/
-  _fetchImage(url,seqIndex) {
+  _fetchImage(url,seqIndex,interactive) {
     // TODO: These should be moved to a worker
     fetch(url)
     .then( (resp) => resp.blob() )
-    .then( (blob) => {
-      const url = URL.createObjectURL(blob)
-      let img = new Image()
-      img.src = url
-
+    .then( (blob) => window.createImageBitmap(blob) )
+    .then( (bmp) => {
       if (this.intrinsicHeight === 0) {
-        window.createImageBitmap(blob).then( bmp => {
-          this.intrinsicHeight = bmp.height
-          this.intrinsicWidth = bmp.width          
-        })
+        this.intrinsicHeight = bmp.height
+        this.intrinsicWidth = bmp.width   
       }
 
-      this.images[seqIndex] = img
-      this.imagesLoaded++
+      this.images[seqIndex] = bmp
+
+      if (interactive) {
+        this.imagesLoaded++
+      } else {
+        this.imagesLoaded = this.images.length
+      }
 
       if (this.allImagesLoaded) {
         this.requestUpdate()
@@ -221,9 +221,26 @@ class ImageSequence extends LitElement {
 
   // Fires when this component is visible
   _loadImages() {
-    this.imageUrls.forEach( (imgUrl,i) => {
-      this._fetchImage(imgUrl,i)
+    this.imageUrls.slice(0, this.isInteractive ? this.imageUrls.length : 1 ).forEach( (imgUrl,i) => {
+      this._fetchImage(imgUrl,i,this.isInteractive)
     })
+
+  }
+
+  connectedCallback() {
+    super.connectedCallback()
+
+    // TODO: Setup observer and run isVisible() when we're at least one screen away 
+    const io = new IntersectionObserver( (entries,observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting && !this.visible) {
+          this.visible = true
+        }
+      })
+    },{ root: document.documentElement, rootMargin: '-50% 0% -50% 0%',
+        threshold: 0 })
+
+    io.observe(this)
   }
 
   constructor() {
@@ -252,27 +269,10 @@ class ImageSequence extends LitElement {
     this.imagesLoaded = 0
     this.totalCanvases = this.imageUrls.length
 
-    // this.context = this.canvasRef.value.getContext('2d')
-
-    // this.canvasRef.value.classList.add('fade-in')
-
-    // Begin loading images
-    this._loadImages()
+    // TODO: Replace this with the visibility handler
+    // this._loadImages()
 
     // Set up observable and mouse events
-
-    // TODO: Setup observer and run isVisible() when we're at least one screen away 
-    // const io = new IntersectionObserver( (entries,observer) => {
-    //   entries.forEach(entry => {
-    //     if (entry.intersectionRatio > 0) {
-    //       this.visible = true
-    //       this._isObservable()
-    //     }
-    //   })
-    // })
-
-    // io.observe(this)
-
     if (this.isInteractive) {
       this.addEventListener('mousemove', this.handleMouseMove.bind(this))
     }
@@ -388,6 +388,12 @@ class ImageSequence extends LitElement {
       this.context = this.canvasRef.value.getContext('2d')
     }
 
+    if (!this.images[this.index]) {
+      // Not loaded yet
+      // TODO: Use a callback to load the next N
+      return
+    }
+
     this.context.drawImage(this.images[this.index],0,0)
   }
 
@@ -401,6 +407,12 @@ class ImageSequence extends LitElement {
       this._updateCanvas()
     }
 
+    if (changedProperties.has('visible') && !this.visible ) {
+      console.log(this.posterImageSrc,'is visible')
+
+      this._loadImages()
+
+    }
   }
 
   /**
@@ -462,10 +474,8 @@ class ImageSequence extends LitElement {
 
   render() {
 
-    const loadingOverlayElement = html`<div slot='loading-overlay' class='${ this.allImagesLoaded ? '' : 'visible'} loading overlay'>Loading Image Sequence...</div>` 
-
     const descriptionOverlay = this.isInteractive ? 
-      html`<div slot='overlay' class="overlay ${ this.oldX === null ? 'visible' : '' }"><span class="description">
+      html`<div slot='overlay' class="overlay ${ this.didInteract === false ? 'visible' : '' }"><span class="description">
             <svg class="description__icon">
               <symbol id="rotation-icon" viewBox="0 0 24 24">
                 <path d="M6.07426 4.68451L6.89234 4.68451L6.89234 6.34444C7.68861 5.65993 8.56396 5.09807 9.5184 4.65884C10.4728 4.21961 11.4791 4 12.5371 4C13.5952 4 14.6014 4.21106 15.5559 4.63317C16.5103 5.05528 17.3856 5.6086 18.1819 6.29311L18.1819 4.68451L19 4.68451L19 7.93593L15.8913 7.93593L15.8913 7.08029L17.7565 7.08029C16.9821 6.39578 16.1694 5.85388 15.3186 5.45458C14.4678 5.05528 13.5406 4.85564 12.5371 4.85564C11.5336 4.85564 10.6092 5.05528 9.76382 5.45458C8.91847 5.85388 8.10311 6.39578 7.31775 7.08029L9.18298 7.08029L9.18298 7.93593L6.07426 7.93593L6.07426 4.68451ZM6.09062 14.0794L7.8086 9.42473L9.05209 9.90389C9.24843 9.98375 9.42023 10.0978 9.56748 10.2461C9.71474 10.3945 9.81018 10.577 9.85381 10.7938L10.1156 12.1457L14.975 9.86966C15.3895 9.67572 15.8067 9.66431 16.2267 9.83544C16.6466 10.0066 16.9493 10.3089 17.1348 10.7424C17.3202 11.1759 17.3338 11.6123 17.1757 12.0515C17.0175 12.4908 16.7312 12.8073 16.3167 13.0013L13.7479 14.1992L13.9115 14.6099C13.9333 14.6555 13.9442 14.7069 13.9442 14.7639L13.9442 14.935L13.8461 17.7415C13.8352 18.0381 13.737 18.3005 13.5516 18.5287C13.3661 18.7569 13.1316 18.9109 12.848 18.9907L9.3466 19.9491C9.03027 20.0403 8.73031 20.009 8.4467 19.8549C8.1631 19.7009 7.95585 19.4699 7.82496 19.1619L6.12334 15.1575C6.04699 14.9864 6.00608 14.8067 6.00063 14.6184C5.99518 14.4302 6.02517 14.2505 6.09062 14.0794ZM6.97415 14.6099L8.83938 19.0079L12.848 17.9469L12.9953 14.4559L12.6189 13.6002L15.9076 12.0601C16.0931 11.9688 16.2185 11.8433 16.284 11.6836C16.3494 11.5239 16.3385 11.3471 16.2512 11.1531C16.164 10.9592 16.044 10.8308 15.8913 10.7681C15.7386 10.7053 15.5695 10.7196 15.3841 10.8109L9.41204 13.5831L8.88847 10.9478L8.38126 10.7595L6.97415 14.6099Z" />
@@ -480,14 +490,12 @@ class ImageSequence extends LitElement {
 
     return html`<div class="image-sequence ${ this.allImagesLoaded ? '' : 'loading' } ${ this.isInteractive ? 'interactive' : '' }">
                   <slot name="placeholder-image">
-                    <img slot="placeholder-image" class="${ this.didInteract ? 'interacted' : '' } ${ this.allImagesLoaded ? '' : 'loading' } placeholder" src="${ this.posterImageSrc }" >
+                    <img slot="placeholder-image" class="${ this.allImagesLoaded ? '' : 'loading' } placeholder" src="${ this.posterImageSrc }" >
                   </slot>
                   <slot name="loading-overlay">
-                    ${ loadingOverlayElement }
+                    <div slot="loading-overlay" class='${ this.allImagesLoaded ? '' : 'visible'} loading overlay'>Loading Image Sequence...</div>
                   </slot>
-                  <slot name="images">
-                    <canvas ${ref(this.canvasRef)} height="${this.intrinsicHeight}" width="${this.intrinsicWidth}" class="${ this.allImagesLoaded ? 'visible' : '' } ${ this.didInteract ? '' : 'fade-in' }" slot="images"></canvas>                
-                  </slot>                  
+                  <canvas ${ref(this.canvasRef)} height="${this.intrinsicHeight}" width="${this.intrinsicWidth}" class="${ this.allImagesLoaded ? 'visible' : '' } ${ this.didInteract ? '' : 'fade-in' }" slot="images"></canvas>                
                   <slot name="overlay">
                     ${ descriptionOverlay }
                   </slot>
