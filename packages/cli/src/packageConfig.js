04ccc108af7ecedd491f0ab4826d9d00d54a9f6b
mph <mhrudka@getty.edu>
2024-12-16 17:27:33 -0800

    Update comments for reading package config

--- packages/cli/src/packageConfig.js
+++ packages/cli/src/packageConfig.js
@@ -6,10 +6,9 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 /**
- * Nota bene:
- * working directory will be the path from which quire-cli commands are run;
- * readPackageUpSync must be called with the change working directory option
- * set to the path of this module to find the quire-cli package config file.
+ * Nota bene: current working directory will be that from which cli commands
+ * are being, readPackageUpSync is passed the directory of this module as the
+ * starting path to search for the quire-cli package config file.
  */
 const { packageJson } = readPackageUpSync({ cwd: __dirname, normalize: true })
 
