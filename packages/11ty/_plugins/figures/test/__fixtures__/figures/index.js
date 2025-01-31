ec479d3753420ebbc4a9c6634539fdbdf7e157a3
matthew p hrudka <mphstudios@users.noreply.github.com>
2025-01-30 16:52:17 -0800

    Use a local function to read fixtures json files

--- packages/11ty/_plugins/figures/test/__fixtures__/figures/index.js
+++ packages/11ty/_plugins/figures/test/__fixtures__/figures/index.js
@@ -1,41 +1,44 @@
-import fs from 'node:fs'
-import path from 'node:path'
-/**
- * Export all figure JSON
- */
+import { readFileSync } from 'node:fs'
+import { join } from 'node:path'
 
+function resolver = (path) => join(import.meta.dirname, path)
+function readJson = (path) => JSON.parse(readFileSync(resolver(path)))
+
+/**
+ * Export all figures fixture data
+ */
 export default {
   annotationsCheckbox: {
     dimensions: { height: 1455, width: 1200 },
-    figure: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './annotations-checkbox/figure.json'))),
-    manifest: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './annotations-checkbox/manifest.json')))
+    figure: readJson('./annotations-checkbox/figure.json'),
+    manifest: readJson('./annotations-checkbox/manifest.json')
   },
   annotationsRadio: {
     dimensions: { height: 2868, width: 2082 },
-    figure: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './annotations-radio/figure.json'))),
-    manifest: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './annotations-radio/manifest.json')))
+    figure: readJson('./annotations-radio/figure.json'),
+    manifest: readJson('./annotations-radio/manifest.json')
   },
   sequence: {
     dimensions: { height: 2160, width: 1827 },
-    figure: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './sequence/figure.json'))),
-    files: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './sequence/files.json'))),
-    manifest: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './sequence/manifest.json')))
+    figure: readJson('./sequence/figure.json'),
+    files: readJson('./sequence/files.json'),
+    manifest: readJson('./sequence/manifest.json')
   },
   sequenceWithAnnotations: {
     dimensions: { height: 2048, width: 1536 },
-    figure: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './sequence-with-annotations/figure.json'))),
-    files: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './sequence-with-annotations/files.json'))),
-    manifest: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './sequence-with-annotations/manifest.json')))
+    figure: readJson('./sequence-with-annotations/figure.json'),
+    files: readJson('./sequence-with-annotations/files.json'),
+    manifest: readJson('./sequence-with-annotations/manifest.json')
   },
   zoomable: {
     dimensions: { height: 3221, width: 4096 },
-    figure: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './zoomable/figure.json'))),
-    manifest: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './zoomable/manifest.json')))
+    figure: readJson('./zoomable/figure.json'),
+    manifest: readJson('./zoomable/manifest.json')
   },
   zoomableSequence: {
     dimensions: { height: 2160, width: 1827 },
-    figure: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './zoomable-sequence/figure.json'))),
-    files: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './zoomable-sequence/files.json'))),
-    manifest: JSON.parse(fs.readFileSync(path.join(import.meta.dirname, './zoomable-sequence/manifest.json')))
+    figure: readJson('./zoomable-sequence/figure.json'),
+    files: readJson('./zoomable-sequence/files.json'),
+    manifest: readJson('./zoomable-sequence/manifest.json')
   }
 }
