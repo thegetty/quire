07b75914d192076c2276ab897a783555afbff4a0
Charlie Butcosk <incharge@weichbild.com>
2025-03-19 18:06:38 -0400

    Fix import directory resolution in figure components

--- packages/11ty/_includes/components/figure/table/index.js
+++ packages/11ty/_includes/components/figure/table/index.js
@@ -1,9 +1,11 @@
+import path from 'node:path'
+
 /**
  * Render all `table` outputs
  */
 export default function (eleventyConfig) {
   const renderOutputs = eleventyConfig.getFilter('renderOutputs')
   return function (params) {
-    return renderOutputs(import.meta.dirname, params)
+    return renderOutputs(path.dirname(import.meta.url), params)
   }
 }
