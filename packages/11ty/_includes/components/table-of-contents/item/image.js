9f7bb4f47b1e4c4e34dcb23351b9102f2a22a1a5
Charlie Butcosk <incharge@weichbild.com>
2025-03-19 17:15:11 -0400

    Fix lint errs

--- packages/11ty/_includes/components/table-of-contents/item/image.js
+++ packages/11ty/_includes/components/table-of-contents/item/image.js
@@ -1,10 +1,11 @@
+import { html } from '#lib/common-tags/index.js'
 import path from 'node:path'
 
 /**
  * Renders a TOC item image
  *
  * @param      {Object} eleventyConfig eleventy configuration
- * 
+ *
  * @param      {Object} params `figure` data from `figures.yaml`
  * @property   {String} alt The alt text for the image
  * @property   {String} src The src path for the image
@@ -13,7 +14,7 @@ import path from 'node:path'
  */
 export default function (eleventyConfig) {
   const { imageDir } = eleventyConfig.globalData.config.figures
-  return function({ alt='', src='', isStatic=false }) {
+  return function ({ alt = '', src = '', isStatic = false }) {
     if (!imageDir || !src) return ''
     const imgPath = src.startsWith('http') || isStatic ? src : path.join(imageDir, src)
     return html`
