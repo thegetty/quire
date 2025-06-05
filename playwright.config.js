c1620d61f387cbacd1eb6cb91f4646441bf8af95
Charlie Butcosk <incharge@weichbild.com>
2025-06-05 11:28:11 -0400

    Refine pathing for test reports

--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['junit', {printSteps: true, outputFile: 'test-results-publication-browser.xml'}]
+    ['junit', {printSteps: true, outputFile: 'reports/publication-browser.xml'}]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
