d07e8d8d7774c611910ad715fd6315d2e8ed4ad7
Charlie Butcosk <incharge@weichbild.com>
2025-06-05 11:08:48 -0400

    Refine test result output filenames and globs, playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -2,17 +2,10 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
+  expect: { toHaveScreenshot: { maxDiffPixels: 100 } },
   testDir: './_tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -23,7 +16,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['junit', {printSteps: true, outputFile: 'test-results-publication-browser.xml'}]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
