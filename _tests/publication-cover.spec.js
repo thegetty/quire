4b4b032e87ee6a86b97faed5728f4a82763f3a75
Charlie Butcosk <incharge@weichbild.com>
2025-06-16 14:45:17 -0400

    Remove screenshot based tests

--- _tests/publication-cover.spec.js
+++ _tests/publication-cover.spec.js
@@ -14,25 +14,3 @@ test.beforeEach(async ({ page }) => {
 test('page title', async ({ page }) => {
   await expect(page).toHaveTitle(/Cover \| New Deal Photography/)
 })
-
-test('screenshot', async ({ page }) => {
-  await expect(page).toHaveScreenshot('cover.png')
-})
-
-test('toc menu', async ({ page }) => {
-  const tocToggle = await page.getByRole('button', { name: 'Table of Contents' })
-  await tocToggle.click()
-  await tocToggle.hover()
-  await expect(page).toHaveScreenshot('cover-toc-open.png')
-  await tocToggle.click()
-  await expect(page).toHaveScreenshot('cover-toc-closed.png')
-})
-
-test('search window', async ({ page }) => {
-  const searchButton = await page.getByRole('button', { name: 'Search' })
-  await searchButton.click()
-  await expect(page).toHaveScreenshot('search-open.png')
-  const closeButton = await page.getByRole('button', { name: 'Close search window' })
-  await closeButton.click()
-  await expect(page).toHaveScreenshot('search-closed.png')
-})
