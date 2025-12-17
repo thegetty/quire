a2e88063cd621712be85d1fcec1901056428dd6d
Andy De Leon <66084673+andy-jdl@users.noreply.github.com>
2025-12-17 15:48:49 -0800

    feature: quire validate command

---
+++ packages/cli/src/commands/validate.js
@@ -0,0 +1,64 @@
+import Command from '#src/Command.js'
+import { YAMLException } from 'js-yaml'
+import YamlValidationError from '../errors/validation/yaml-validation-error.js'
+import fs from 'fs-extra'
+import path from 'node:path'
+import { projectRoot  } from '#lib/11ty/index.js'
+import testcwd from '../helpers/test-cwd.js'
+import yamlValidation from '../validators/validate-yaml.js'
+
+
+/**
+ * Quire CLI `validate` Command
+ *  
+ * @class     ValidateCommand
+ * @extends   {Command}
+ */
+export default class ValidateCommand extends Command {
+  static definition = {
+    name: 'validate',
+    description: 'Validate configuration files',
+    summary: 'run validation',
+    version: '1.0.0',
+    options: [
+      [ '--debug', 'run validate with debug output to console' ],
+    ],
+  }
+
+  constructor() {
+    super(ValidateCommand.definition)
+  }
+
+  action(options, command){
+    if(options.debug) {
+      console.debug('[CLI] Command \'%s\' called with options %o', this.name(), options)
+    }
+    
+    const dataPath = path.join(projectRoot, 'content', '_data')
+    const files = fs.readdirSync(dataPath)
+      .filter(file => file.endsWith('.yaml') || file.endsWith('.yml'))
+      .map(file => path.join(dataPath, file)
+      )
+
+    let errorList = []
+    console.log('Validating YAML files..')
+
+    for (const file of files) {
+      try {
+        yamlValidation(file)
+      } catch (error){
+        errorList.push(error)
+      }
+    }
+
+    if(errorList.length > 0) {
+      errorList.forEach(err => { console.error(`${err.reason}`) })
+    } else {
+      console.log('Validation complete.')
+    }
+  }
+
+  preAction(options, command) {
+    testcwd(command)
+  }
+}
