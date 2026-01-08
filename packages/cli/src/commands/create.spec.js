e1b97ce7cdd58adfc058515b2b8119fc1a230696
matthew p hrudka <mphstudios@users.noreply.github.com>
2026-01-08 12:34:02 -0800

    feature: quire new clean cache option

--- packages/cli/src/commands/create.spec.js
+++ packages/cli/src/commands/create.spec.js
@@ -53,13 +53,25 @@ test('command should have correct options defined', (t) => {
   const { command } = t.context
 
   t.true(Array.isArray(command.options))
+
   const path = command.options.find((opt) => opt[0].includes('--quire-path'))
   const version = command.options.find((opt) => opt[0].includes('--quire-version'))
   const debug = command.options.find((opt) => opt[0] === '--debug')
 
+  // Nota bene: this is correct but shows that the unit tests are fragile;
+  // the current spec implementation tests the intermediate definition format,
+  // not what users actually interact with, which is the public API.
+  // Specs should verify the structure and configuration of the command definition,
+  // as well as the command's public contract/API, *without testing behavior*.
+  // @TODO refactor the command specs to test that the registered Commander.js
+  // command is actually a contract/interface, the integration between each
+  // Command class → Commander creates Command, Argument, and Option instances.
+  const cleanCache = command.options.find((opt) => opt.flags === '--clean-cache')
+
   t.truthy(path)
   t.truthy(version)
   t.truthy(debug)
+  t.truthy(cleanCache)
 })
 
 test('action method should be defined and async', (t) => {
