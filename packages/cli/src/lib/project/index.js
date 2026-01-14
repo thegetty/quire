c31c1dd1eff2faa75ee178286258cc6979f1fe07
matthew p hrudka <mphstudios@users.noreply.github.com>
2026-01-14 13:35:41 -0800

    Implement cli lib module to encapsulate a project
    Paths are an overlapping concern of both Eleventy and a Quire project.
    Implementing a Project module to encapsulate this concern addresses the
    leaky abstraction of handling paths in either place or scattered across
    different modules.

---
+++ packages/cli/src/lib/project/index.js
@@ -0,0 +1,8 @@
+/**
+ * Project module
+ *
+ * Provides project-level concerns including paths, detection, and configuration.
+ */
+import paths, { Paths } from './paths.js'
+
+export { Paths, paths as default }
