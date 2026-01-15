f6f854e66fdeb180f7e0177f3a066f59a88e8b6b
matthew p hrudka <mphstudios@users.noreply.github.com>
2026-01-15 00:21:36 -0800

    Export version methods from lib/project

--- packages/cli/src/lib/project/index.js
+++ packages/cli/src/lib/project/index.js
@@ -1,10 +1,28 @@
 /**
  * Project module
  *
- * Provides project-level concerns including paths, detection, and configuration.
+ * Provides project-level concerns including paths, detection, configuration,
+ * and version management.
  */
 import paths, { Paths } from './paths.js'
 import detect from './detect.js'
 import { loadProjectConfig } from './config.js'
+import {
+  getVersion,
+  setVersion,
+  getVersionsFromStarter,
+  readVersionFile,
+  writeVersionFile,
+} from './version.js'
 
-export { detect, loadProjectConfig, Paths, paths as default }
+export {
+  detect,
+  loadProjectConfig,
+  paths as default,
+  Paths,
+  getVersion,
+  setVersion,
+  getVersionsFromStarter,
+  readVersionFile,
+  writeVersionFile,
+}
