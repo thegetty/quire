0e4a793187cf935abc62adc934f34def1867b146
matthew p hrudka <mphstudios@users.noreply.github.com>
2026-01-18 01:20:00 -0800

    Move utils for command definition to lib/commander

--- packages/cli/src/main.js
+++ packages/cli/src/main.js
@@ -1,4 +1,5 @@
-import { Argument, Command, Option } from 'commander'
+import { Command, Argument, Option } from 'commander'
+import { arrayToArgument, arrayToOption } from '#lib/commander/index.js'
 import commands from '#src/commands/index.js'
 import config from '#lib/conf/config.js'
 import { handleError } from '#lib/error/handler.js'
@@ -101,22 +102,6 @@ commands.forEach((command) => {
   }
 
   /**
-   * Convert a command arg defined using array-syntax to an Argument object
-   *
-   * @param {Array} entry - Argument definition array [name, description, { choices, default }]
-   * @returns {Argument} Commander Argument object
-   * @see https://github.com/tj/commander.js?tab=readme-ov-file#command-arguments
-   * @see https://github.com/tj/commander.js#more-configuration-1
-   */
-  const arrayToArgument = (entry) => {
-    const [name, description, config = {}] = entry
-    const argument = new Argument(name, description)
-    if (config.choices) argument.choices(config.choices)
-    if (config.default) argument.default(config.default)
-    return argument
-  }
-
-  /**
    * Register arguments with the subcommand
    */
   if (Array.isArray(args)) {
@@ -124,49 +109,6 @@ commands.forEach((command) => {
       const argument = entry instanceof Argument ? entry : arrayToArgument(entry)
       subCommand.addArgument(argument)
     })
-  }
-
-  /**
-   * Convert a command option defined using array-syntax to an Option object
-   *
-   * Supports two array formats:
-   * - Separate flags: ['-s', '--long <value>', 'description', default, { choices, default }]
-   * - Combined flags: ['--long <value>', 'description', default, { choices, default }]
-   *
-   * @param {Array} entry - Option definition array
-   * @returns {Option} Commander Option object
-   * @see https://github.com/tj/commander.js/#options
-   * @see https://github.com/tj/commander.js/#more-configuration
-   */
-  const arrayToOption = (entry) => {
-    const lastElement = entry[entry.length - 1]
-    const configObj = typeof lastElement === 'object' && lastElement !== null ? lastElement : null
-
-    let flags, description, defaultValue
-    if (entry[0]?.startsWith('-') && entry[1]?.startsWith('--')) {
-      // Array has separate elements for short and long option flags
-      // @example ['-s', '--long <value>', 'desc', ...]
-      flags = `${entry[0]}, ${entry[1]}`
-      description = entry[2]
-      defaultValue = entry[3]
-    } else {
-      // Array has a single element with option flags
-      // @example ['--long <value>', 'desc', ...]
-      flags = entry[0]
-      description = entry[1]
-      defaultValue = entry[2]
-    }
-
-    const option = new Option(flags, description)
-
-    if (configObj) {
-      if (configObj.choices) option.choices(configObj.choices)
-      if (configObj.default !== undefined) option.default(configObj.default)
-    } else if (defaultValue !== undefined) {
-      option.default(defaultValue)
-    }
-
-    return option
   }
 
   /**
