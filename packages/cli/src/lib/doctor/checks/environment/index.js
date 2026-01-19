9d1577cb86a99814560c6e01add4d167ec3d4dea
matthew p hrudka <mphstudios@users.noreply.github.com>
2026-01-19 12:57:12 -0800

    Add check for Quire CLI version updates

--- packages/cli/src/lib/doctor/checks/environment/index.js
+++ packages/cli/src/lib/doctor/checks/environment/index.js
@@ -5,6 +5,7 @@
  *
  * @module lib/doctor/checks/environment
  */
+export { checkCliVersion } from './cli-version.js'
 export { checkNodeVersion } from './node-version.js'
 export { checkNpmAvailable } from './npm-available.js'
 export { checkGitAvailable } from './git-available.js'
