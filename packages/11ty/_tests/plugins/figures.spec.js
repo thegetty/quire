a87d308d269bea76d8fd0e2152027a7e38694210
Charlie Butcosk <incharge@weichbild.com>
2026-04-24 12:28:49 -0400

    Add test that all images both get transformations applied

--- packages/11ty/_tests/plugins/figures.spec.js
+++ packages/11ty/_tests/plugins/figures.spec.js
@@ -4,26 +4,98 @@
  * Tests for the `figures` plugin
  *
  **/
-// import FigureFactory from '../../_plugins/figures/figure/factory.js'
+import esmock from 'esmock'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import path from 'path'
+import sinon from 'sinon'
 import test from 'ava'
-// import fs from 'fs'
-// import { fileURLToPath } from 'url'
-// import { initEleventyEnvironment } from '../helpers/index.js'
-// import { JSDOM } from 'jsdom'
-// import path from 'path'
 
-// Initialize a minimal FigureFactory
+const iiifConfigPath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../../_plugins/figures/test/__fixtures__/iiif-config.json')
+
+/**
+ * @function MockFigureFactory
+ *
+ * @param {Object} iiifConfig
+ * @param {Object.prototype} figureClass
+ * @param {function} processStub
+ *
+ * Creates a FigureFactory with `processStub` as mock processor
+ *
+ **/
+async function MockFigureFactory (sandbox, iiifConfig, processStub) {
+  const Figure = await esmock('#plugins/figures/figure/index.js', {
+    sharp: sandbox.stub().returns({
+      metadata: sandbox.stub().returns({ height: 100, width: 100 })
+    })
+  })
+
+  const FigureFactory = await esmock('#plugins/figures/figure/factory.js', {
+    '#plugins/figures/image/processor.js': sandbox.stub().returns({
+      processImage: sandbox.stub().callsFake(processStub)
+    }),
+    '#plugins/figures/figure/index.js': Figure
+  })
+
+  const factory = new FigureFactory(iiifConfig)
+
+  return factory
+}
+
+// Test fixtures and Figure class that avoids real disk calls to sharp
 test.before('', async (t) => {
-  // const factory = new FigureFactory()
+  const iiifConfig = JSON.parse(fs.readFileSync(iiifConfigPath))
+  const sandbox = sinon.createSandbox()
 
-  // t.context.factory = factory
+  t.context.iiifConfig = iiifConfig
+  t.context.sandbox = sandbox
 })
 
-test('FigureFactory should create a staticInlineFigureImage for all figures', async (t) => {
-  // TODO: Mock sharp().metadata() to return height: 100, width: 100
-  // TODO: Mock sharp().extract().resize().withMetadata().toFile(x)
+test('FigureFactory should create a staticInlineFigureImage for zoomable figures', async (t) => {
+  const { iiifConfig, sandbox } = t.context
 
-  // TODO: Given a figure with zoom: true, `staticInlineFigureImage` should be populated and the mock for sharp should be called
-  // TODO: Given a figure without zoom: true, `staticInlineFigureImage` should be populated and the mock for sharp should be called
-  t.fail('Spec staticInlineFigureImage test')
+  const figure = {
+    id: 'iiif-figure',
+    src: 'iiif-figure.jpg',
+    zoom: true
+  }
+
+  let passed = false
+  const isTransformedZoomedAndIIIF = async (path, _, options) => {
+    passed = options.transformations?.length > 0 && options.tile && options.iiifEndpoint
+    return { errors: [] }
+  }
+
+  const factory = await MockFigureFactory(sandbox, iiifConfig, isTransformedZoomedAndIIIF)
+  await factory.create(figure)
+
+  if (passed) {
+    t.pass()
+  } else {
+    t.fail()
+  }
+})
+
+test('FigureFactory should create a staticInlineFigureImage for static figures', async (t) => {
+  const { iiifConfig, sandbox } = t.context
+
+  const figure = {
+    id: 'static-figure',
+    src: 'static-figure.jpg'
+  }
+
+  let passed = false
+  const isTransformedOnly = async (path, _, options) => {
+    passed = options.transformations?.length > 0 && !options.tile && !options.iiifEndpoint
+    return { errors: [] }
+  }
+
+  const factory = await MockFigureFactory(sandbox, iiifConfig, isTransformedOnly)
+  await factory.create(figure)
+
+  if (passed) {
+    t.pass()
+  } else {
+    t.fail()
+  }
 })
