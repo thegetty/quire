d8a7376ff0bb957cec6ea91e54dff95923eb75b0
Charlie Butcosk <incharge@weichbild.com>
2026-05-27 11:40:53 -0400

    Stub test of TOC grid item images

---
+++ packages/11ty/_tests/components/tocGridItem.spec.js
@@ -0,0 +1,16 @@
+/**
+ * figures.spec.js
+ *
+ * Tests for the `figures` plugin
+ *
+ **/
+// import esmock from 'esmock'
+// import { fileURLToPath } from 'url'
+// import fs from 'fs'
+// import path from 'path'
+// import sinon from 'sinon'
+import test from 'ava'
+
+test('TOC Grid Item should have valid thumbnails when using image from files and external IIIF endpoints', (t) => {
+  t.fail()
+})
