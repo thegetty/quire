00cc060eadede22df0edfbce8d41769472562808
Charlie Butcosk <incharge@weichbild.com>
2026-06-10 10:58:49 -0400

    Add failing test for tocGrid image usage

--- packages/11ty/_tests/components/tocGridItem.spec.js
+++ packages/11ty/_tests/components/tocGridItem.spec.js
@@ -13,5 +13,27 @@ import test from 'ava'
 import tocGridItemImage from '../../_includes/components/table-of-contents/item/image.js'
 
 test('Table of Contents Grid Item Image should use thumbnails metadata if available', (t) => {
+  const imageComponent = tocGridItemImage({})
+
+  // Mock figureMedia data
+  const figureMedia = {
+    derivatives: {
+      thumbnail: {
+        paths: {
+          internal: '/_assets/test.jpg',
+          absolute: '/pathname/_assets/test.jpg',
+          uri: 'https://example.org/test.jpg'
+        },
+        dimensions: {
+          height: 250,
+          width: 250
+        }
+      }
+    }
+  }
+  const rendered = imageComponent(figureMedia)
+  console.log(rendered)
+  // TODO: JSDOM the rendered image @src should === figureMedia.derivatives.thumbnail.paths.internal and metadata should match too
+
   t.fail()
 })
