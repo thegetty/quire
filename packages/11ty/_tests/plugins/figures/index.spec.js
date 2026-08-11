61ed01ba3d83cc4aee53c2f321202aed79fb4ecb
Charlie Butcosk <incharge@weichbild.com>
2026-08-11 09:48:49 -0400

    Failing test that annex figures were processed

--- packages/11ty/_tests/plugins/figures/index.spec.js
+++ packages/11ty/_tests/plugins/figures/index.spec.js
@@ -17,20 +17,30 @@ test.before('', async (t) => {
 test('Annex publication images (logos, avatars, etc) should be added to figuresMedia globalData', async (t) => {
   const { sandbox } = t.context
 
-  // addGlobalData() and create() functions for testing whether the factory was passed the figure
-  const addGlobalData = sandbox.fake.returns({})
-  const create = sandbox.fake.returns({
-    media: () => { return {} }
+  // fake functions for tracking calls on the factory mock and global data
+  const create = sandbox.fake(({ id }) => {
+    return {
+      id,
+      figure: {
+        id,
+        media: () => { return { id } }        
+      }
+    }
   })
+  const addGlobalData = sandbox.fake()
 
   // Initialize the plugin against a config that auto-runs the plugin's event hook
   const pluginInit = await esmock('#plugins/figures/index.js', {
-    '#plugins/figures/figureMedia/factory.js': { create }
+    '#plugins/figures/figureMedia/factory.js': sandbox.stub().returns({ create })
   })
-  // TODO: Stub globalData with our test figures
+
+  // Stub globalData with our test figures
   const eleventyConfig = {
     addGlobalData,
     globalData: {
+      directoryConfig: {
+        inputDir: 'content', publicDir: 'public'
+      },
       epub: {
         defaultCoverImage: 'test-default-cover.jpg'
       },
@@ -38,27 +48,56 @@ test('Annex publication images (logos, avatars, etc) should be added to figuresM
         contributor: [
           { id: 'test-contributor', image: 'test-contributor-avatar.jpg' }
         ],
+        promo_image: 'test-promo-image.jpg',
         publisher: [
-          { id: 'test-publisher', logo: 'test-publisher-logo.jpg' }
-        ]
+          { name: 'Test Publisher', logo: 'test-publisher-logo.jpg' }
+        ],
+        url: new URL('http://localhost:8080')
       },
       figures: {
         figure_list: []
       }
     },
-    on: (eventKey, hook) => hook()
+    on: async (eventKey, hook) => {
+      await hook()
+    },
+    serverOptions: {
+      port: 8080
+    }
   }
+
   pluginInit(eleventyConfig, {})
+  console.log('suppers', addGlobalData.notCalled)
+  t.true(create.calledWithMatch(sinon.match({ id: 'promo-image', src: 'test-promo-image.jpg' })),
+    'Figures plugin should process promo image for derivatives')
+  // TODO: Test that addGlobalData was called for this figure
+  // t.true(
+  //   addGlobalData.calledWithMatch(
+  //     sinon.match('figureMedia'),
+  //     sinon.match.has('id', 'promo-image')
+  //   ),
+  //   'globalData should be added for promo image')
 
-  t.true('Figures plugin should process promo image for derivatives', create.calledWithMatch(sinon.match({ id: '', src: '' })))
-  t.true('globalData should be added for promo image', addGlobalData.calledWithMatch(sinon.match({ id: '' })))
+  t.true(create.calledWithMatch(sinon.match({ id: 'epub-default', src: 'test-default-cover.jpg' })),
+    'Figures plugin should process epub default cover for derivatives')
+  // TODO: Test that addGlobalData was called for this figure
+  // t.true(
+  //   addGlobalData.calledWithMatch(
+  //     sinon.match('figureMedia'),
+  //     sinon.match.has('id', 'promo-image')
+  //   ),
+  //   'globalData should be added for epub default cover'
+  // )
 
-  t.true('Figures plugin should process epub default cover for derivatives', create.calledWithMatch(sinon.match({ id: '', src: '' })))
-  t.true('globalData should be added for epub default cover', addGlobalData.calledWithMatch(sinon.match({ id: '' })))
+  t.true(create.calledWithMatch(sinon.match({ id: 'logo-test-publisher', src: 'test-publisher-logo.jpg' })),
+    'Figures plugin should process logos for derivatives')
+  // TODO
+  // t.true(addGlobalData.calledWithMatch(sinon.match({ id: '' })),
+  //   'globalData should be added for logos')
 
-  t.true('Figures plugin should process logos for derivatives', create.calledWithMatch(sinon.match({ id: '', src: '' })))
-  t.true('globalData should be added for logos', addGlobalData.calledWithMatch(sinon.match({ id: '' })))
-
-  t.true('Figures plugin should process contributor avatars for derivatives', create.calledWithMatch(sinon.match({ id: '', src: '' })))
-  t.true('globalData should be added for contributor avatars', addGlobalData.calledWithMatch(sinon.match({ id: '' })))
+  t.true(create.calledWithMatch(sinon.match({ id: 'contributor-test-contributor', src: 'test-contributor-avatar.jpg' })),
+    'Figures plugin should process contributor avatars for derivatives')
+  // TODO
+  // t.true(addGlobalData.calledWithMatch(sinon.match({ id: '' })),
+  //   'globalData should be added for contributor avatars')
 })
