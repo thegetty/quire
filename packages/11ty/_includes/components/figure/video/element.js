553c8e3eb8b9306f33be45b5da13a2a7b4a4d303
Charlie Butcosk <incharge@weichbild.com>
2026-08-31 10:15:18 -0400

    Remove figureMediaEmbedUrl component and logic from components

--- packages/11ty/_includes/components/figure/video/element.js
+++ packages/11ty/_includes/components/figure/video/element.js
@@ -21,7 +21,7 @@ const logger = chalkFactory('Figure Video')
 export default function (eleventyConfig) {
   const { pathname } = eleventyConfig.globalData.publication
   const { imageDir } = eleventyConfig.globalData.config.figures
-  const figureMediaEmbedUrl = eleventyConfig.getFilter('figureMediaEmbedUrl')
+
   const videoElements = {
     video ({ derivatives, id }) {
       const { media, staticInlineFigureImage } = derivatives
@@ -46,13 +46,8 @@ export default function (eleventyConfig) {
         </video>
       `
     },
-    vimeo ({ id, mediaId, mediaType, lazyLoading }) {
-      if (!mediaId) {
-        logger.error(`Cannot render Vimeo embed without 'media_id'. Check that figures data for id: ${id} has a valid 'media_id'`)
-        return ''
-      }
-
-      const { embedUrl } = figureMediaEmbedUrl({ mediaId, mediaType })
+    vimeo ({ id, derivatives, lazyLoading }) {
+      const { embedUrl } = derivatives.embed
 
       return html`
         <iframe
@@ -65,13 +60,8 @@ export default function (eleventyConfig) {
         ></iframe>
       `
     },
-    youtube ({ id, mediaId, mediaType, lazyLoading }) {
-      if (!mediaId) {
-        logger.error(`Cannot render Youtube component without 'media_id'. Check that figures data for id: ${id} has a valid 'media_id'`)
-        return ''
-      }
-
-      const { embedUrl } = figureMediaEmbedUrl({ mediaId, mediaType })
+    youtube ({ id, derivatives, lazyLoading }) {
+      const { embedUrl } = derivatives.embed
 
       return html`
         <iframe
