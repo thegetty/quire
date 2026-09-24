20571b694015fe8870acb4ccc692420d3338cba2
Charlie Butcosk <incharge@weichbild.com>
2026-09-24 09:26:31 -0400

    Add basic tests of processor logic for compositing

--- packages/11ty/_tests/plugins/figures/processor.spec.js
+++ packages/11ty/_tests/plugins/figures/processor.spec.js
@@ -1,17 +1,60 @@
+import esmock from 'esmock'
+import { fileURLToPath } from 'node:url'
+import fs from 'node:fs'
+import path from 'node:path'
+import sinon from 'sinon'
 import test from 'ava'
 
-// TODO: Create some test fixture images at various sizes (100x100, 200x100, and so on)
+const iiifConfigPath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../../../_plugins/figures/test/__fixtures__/iiif-config.json')
 
-test('Processor should correctly handle composite overlays', async (t) => {
-  // TODO: Test that the canvas is always at the size of the base image
-  // TODO: Test that 3 same-sized images get overlaid without error
+test.before(async (t) => {
+  const sandbox = sinon.createSandbox()
+  const iiifConfig = JSON.parse(fs.readFileSync(iiifConfigPath))
 
-  t.fail()
+  t.context.iiifConfig = iiifConfig
+  t.context.sandbox = sandbox
 })
 
-test('Processor should correctly handle composite grids', async (t) => {
-  // TODO: Test that the canvas matches expected dimensions
-  // TODO: Test that a grid is successfully created no matter the order of the images
+test('processor should call compositor when passed a non-zero `composite` options key', async (t) => {
+  const { iiifConfig, sandbox } = t.context
 
-  t.fail()
+  const composite = sandbox.fake()
+  const MockProcessor = await esmock('../../../_plugins/figures/image/processor.js', {
+    '../../../_plugins/figures/image/compositor.js': sandbox.stub().returns({
+      composite: sandbox.stub().callsFake(composite)
+    })
+  })
+
+  // Mock the processor and ensure that the compositor gets called correctly for each configuration
+  const processor = new MockProcessor(iiifConfig)
+
+  processor.processImages(['test.jpg'], 'destination', { composite: 'grid' })
+  t.truthy(
+    composite.calledWith(
+      sinon.match(['test.jpg']),
+      sinon.match('grid'),
+      sinon.match.any
+    )
+  )
+
+  processor.processImages(['test.jpg'], 'destination', { composite: 'overlay' })
+  t.truthy(
+    composite.calledWith(
+      sinon.match(['test.jpg']),
+      sinon.match('overlay'),
+      sinon.match.any
+    )
+  )
+
+  // Reset the fake and ensure it is *not* called for composite: false
+  composite.resetHistory()
+
+  processor.processImages(['test.jpg'], 'destination', { composite: false })
+  t.truthy(
+    composite.neverCalledWith(
+      sinon.match(['test.jpg']),
+      sinon.match.any,
+      sinon.match.any
+    )
+  )
 })
